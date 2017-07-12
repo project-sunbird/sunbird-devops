09adc0c9fd87f94b06f4e91d7a6e27fec11dc437
Shashank Teotia <shashankteotia@gmail.com>
2017-07-12 14:27:16 +0530

    Issue #23 feat: Dockerizing AdminUtils

---
+++ images/adminutil/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Build script
+# set -o errexit
+e () {
+    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
+}
+m=$(cat $METADATA_FILE)
+
+org=$(e "${m}" "org")
+name=$(e "${m}" "name")
+version=$(e "${m}" "version")
+
+artifactLabel=${ARTIFACT_LABEL:-bronze}
+env=${ENV:-null}
+
+echo "artifactLabel:  ${artifactLabel}"
+echo "env:            ${env}"
+echo "org:            ${org}"
+echo "name:           ${name}"
+echo "version:        ${version}"
+echo "ANSIBLE_PATH:   $ANSIBLE_PATH"
+
+ansible-playbook --version
+ansible-playbook -i ansible/inventory/dev ansible/deploy.yml --tags "adminutil" --extra-vars "hub_org=${org} image_name=${name} image_tag=${version}-${artifactLabel}" --vault-password-file /run/secrets/vault-pass
+# ENV=${env} ORG=${org} NAME=${name} \
+# TAG=${version}-${artifactLabel} \
+# docker stack deploy -c ./images/proxy/docker-compose.yml ${name}-${env}
