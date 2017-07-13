f47bdd37577f487f5998003f6222f28b1c44e95c
gauravsinghania <grvsinghania@gmail.com>
2017-07-13 13:57:08 +0530

    Issue #24 chore : Creating network in bootstrap and adding to monitoring

--- pipelines/monitor/deploy.sh
+++ pipelines/monitor/deploy.sh
@@ -3,4 +3,5 @@
 # set -o errexit
 
 ansible-playbook --version
+ansible-playbook -i ansible/inventory/dev ansible/bootstrap.yml --tags "create_network" --vault-password-file /run/secrets/vault-pass
 ansible-playbook -i ansible/inventory/dev ansible/deploy.yml --tags "stack-monitor" --vault-password-file /run/secrets/vault-pass
