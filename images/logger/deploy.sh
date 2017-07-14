1d45ca8c2e9d179503489dea15fc26f557c7277a
gauravsinghania <grvsinghania@gmail.com>
2017-07-14 18:21:12 +0530

    Issue #28 chore : adding logger for stagging

--- images/logger/deploy.sh
+++ images/logger/deploy.sh
@@ -4,4 +4,4 @@
 
 ansible-playbook --version
 
-ansible-playbook -i ansible/inventory/dev ansible/deploy.yml --tags "stack-logger" --vault-password-file  /run/secrets/vault-pass
+ansible-playbook -i ansible/inventory/${ENV} ansible/deploy.yml --tags "stack-logger" --vault-password-file  /run/secrets/vault-pass
