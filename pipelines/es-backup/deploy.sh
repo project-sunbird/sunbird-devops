646c5c4af8f12185703b9b503ccf2d994e75477b
Kiran G <kirang20@gmail.com>
2017-07-24 09:25:31 +0530

    Issue #50 feat: verbose logging for ansible

--- pipelines/es-backup/deploy.sh
+++ pipelines/es-backup/deploy.sh
@@ -3,4 +3,4 @@
 # set -o errexit
 
 ansible-playbook --version
-ansible-playbook -i ansible/inventory/$ENV ansible/es-backup.yml --tags "es_backup" --vault-password-file /run/secrets/vault-pass
+ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/es-backup.yml --tags "es_backup" -v --vault-password-file /run/secrets/vault-pass
