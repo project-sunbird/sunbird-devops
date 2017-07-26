ec92707d489d2d33873bf44041b90464913458e0
Kiran G <kirang20@gmail.com>
2017-07-26 10:53:29 +0530

    Issue #50 feat: debug msg

--- pipelines/log-es-restore/deploy.sh
+++ pipelines/log-es-restore/deploy.sh
@@ -3,4 +3,5 @@
 # set -o errexit
 
 ansible-playbook --version
+cat $SNAPSHOT_NUMBER
 ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/es.yml --tags "log_es_restore" --extra-vars "snapshot_number=$SNAPSHOT_NUMBER" -v --vault-password-file /run/secrets/vault-pass
