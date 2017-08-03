f732b4a52a3722b23c11cc290b476fd59ddca274
Kiran G <kirang20@gmail.com>
2017-08-03 16:05:20 +0530

    Issue #88 feat: Remove old logger files

--- images/kibana/metadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# return version
-echo '{"name":"kibana","version":"0.0.1","org":"sunbird","hubuser":"purplesunbird"}'
--- images/logstash/metadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# return version
-echo '{"name":"logstash","version":"0.0.1","org":"sunbird","hubuser":"purplesunbird"}'
