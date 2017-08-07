69101937eef99a4b0cb9fe2eab62469e84c860b0
Kiran G <kirang20@gmail.com>
2017-08-07 15:16:42 +0530

    Issue #88 feat: Fix jmx config

--- ansible/roles/cassandra/templates/run_jmx_httpserver.sh
+++ ansible/roles/cassandra/templates/run_jmx_httpserver.sh
@@ -3,4 +3,4 @@
 
 # Note: You can use localhost:5556 instead of 5556 for configuring socket hostname.
 
-java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=7199 -jar /usr/share/cassandra/lib/jmx_prometheus_httpserver-0.11.jar 7199 /etc/cassandra/cassandra_jmx.yaml
+java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555 -jar /usr/share/cassandra/lib/jmx_prometheus_httpserver-0.11.jar 5556 /etc/cassandra/jmx_httpserver.yml
