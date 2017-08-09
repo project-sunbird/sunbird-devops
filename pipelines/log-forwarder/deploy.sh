3c23a981293a2a4f358fa72b51e9b7361b2ac0a5
Shashank Teotia <shashankteotia@gmail.com>
2017-08-09 18:16:00 +0530

    Issue #91 feat: loading all VMs

--- pipelines/log-forwarder/deploy.sh
+++ pipelines/log-forwarder/deploy.sh
@@ -3,4 +3,4 @@
 # set -o errexit
 
 ansible-playbook --version
-ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/provision.yml --tags "log-forwarder" -v --vault-password-file /run/secrets/vault-pass
+ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/ops.yml --tags "log-forwarder" -v --vault-password-file /run/secrets/vault-pass
