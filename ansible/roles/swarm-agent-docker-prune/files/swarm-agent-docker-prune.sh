c29a1c5bb222e7ce0c601934406857a7afc3326e
Deepak N <endeep123@gmail.com>
2017-08-16 21:31:11 +0530

    Issue #105 feat : swarm-agent-docker-prune

---
+++ ansible/roles/swarm-agent-docker-prune/files/swarm-agent-docker-prune.sh
@@ -0,0 +1,7 @@
+master_node_ip=$1
+agent_nodes=$(ssh -i /run/secrets/ops-private-key ops@$master_node_ip "docker node ls -f role=worker --format {{.Hostname}}")
+for agent_node in $agent_nodes; do
+    echo ""
+    echo  "Cleaning node: $agent_node"
+    eval `ssh-agent -s` && ssh-add /run/secrets/ops-private-key && ssh -A  ops@$master_node_ip "ssh $agent_node docker system prune -f"
+done;
