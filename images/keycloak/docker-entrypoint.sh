af14d50d8536056d1f259d6a18032293f05786cf
manoj v <manojv@ilimi.in>
2017-08-30 12:25:30 +0530

    Issue #131 fix : secrets added to the service create command

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export HOSTNAME_IP=$(hostname -i)
 echo "hostname -i returned: $HOSTNAME_IP"
-echo "export TCPPING_INITIAL_HOSTS=`cat /run/secrets/postgresql_keycloak_pass`" >> ~/.bashrc
+echo "export POSTGRES_PASSWORD=`cat /run/secrets/postgresql_keycloak_pass`" >> ~/.bashrc
 source ~/.bashrc
 exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
 exit $?
