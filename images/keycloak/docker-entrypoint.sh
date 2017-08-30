25d1083b28fd9c48d84015dda1180f4f5b92350b
manoj v <manojv@ilimi.in>
2017-08-30 12:11:30 +0530

    Issue #131 fix : environment file is updated

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export HOSTNAME_IP=$(hostname -i)
 echo "hostname -i returned: $HOSTNAME_IP"
-export TCPPING_INITIAL_HOSTS=`cat /run/secrets/postgresql_keycloak_pass`
+echo "export TCPPING_INITIAL_HOSTS=`cat /run/secrets/postgresql_keycloak_pass`" >> ~/.bashrc
+source ~/.bashrc
 exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
 exit $?
