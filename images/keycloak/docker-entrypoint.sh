954fc51152a576b7cd5e88c90a739717e4f2fd2b
manoj v <manojv@ilimi.in>
2017-08-30 11:47:48 +0530

    Issue #131 fix : updated keycloak config

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 export HOSTNAME_IP=$(hostname -i)
 echo "hostname -i returned: $HOSTNAME_IP"
-
+export TCPPING_INITIAL_HOSTS=`cat /run/secrets/postgresql_keycloak_pass`
 exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
 exit $?
