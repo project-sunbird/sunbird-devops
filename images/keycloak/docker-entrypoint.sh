36006225bc305448d9e6a339c840e2ac4b6eb844
Shailesh <kochhar@users.noreply.github.com>
2017-09-01 23:23:27 +0530

    Issue #0: fix: bind to private address.

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -6,5 +6,5 @@ echo "export POSTGRES_PASSWORD=`cat /run/secrets/postgresql_keycloak_pass`" >> ~
 # ./add-user-keycloak.sh -u admin -p 3KSt3p1707
 source ~/.bashrc
 echo "TCPPING_INITIAL_HOSTS: $TCPPING_INITIAL_HOSTS"
-exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
+exec /opt/jboss/keycloak/bin/standalone.sh -b $HOSTNAME_IP -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
 exit $?
