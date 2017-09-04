010112806a315bfff33ce22308a003b7d8586a45
Kiran G <kirang20@gmail.com>
2017-09-04 12:38:05 +0530

    Issue #162 feat: env vars android slave service

--- images/android-jenkins-swarm-agent/custom-jenkins-slave.sh
+++ images/android-jenkins-swarm-agent/custom-jenkins-slave.sh
@@ -6,14 +6,13 @@ cd $HOME
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
 
   # Jenkins swarm slave
-  JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
+  #JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
+  JAR=/home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar
   PARAMS=""
   # if -master is not provided and using --link jenkins:jenkins
   if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_MASTER_PORT" ]; then
     PARAMS="$PARAMS -master http://$JENKINS_MASTER_HOST:$JENKINS_MASTER_PORT/jenkins"
   fi
-  USERNAME=sudo cat $JENKINS_SLAVE_USERNAME
-  PASSWORD=sudo cat $JENKINS_SLAVE_PASSWORD
   CMD="java $JAVA_OPTS -jar $JAR -fsroot $HOME -username `cat $JENKINS_SLAVE_USERNAME` -password `cat $JENKINS_SLAVE_PASSWORD` -mode $JENKINS_SLAVE_MODE -name $JENKINS_SLAVE_NAME -executors $JENKINS_SLAVE_EXECUTORS $PARAMS $@"
   echo Running $CMD
   exec $CMD
