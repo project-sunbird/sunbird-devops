1c63d8ded5b78ae4e444635bd3e5253f880c4972
Kiran G <kirang20@gmail.com>
2017-09-04 10:39:58 +0530

    Issue #162 feat: env vars android slave service

---
+++ images/android-jenkins-swarm-agent/custom-jenkins-slave.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $HOME
+
+# If `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
+
+  # Jenkins swarm slave
+  JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
+  PARAMS=""
+  # if -master is not provided and using --link jenkins:jenkins
+  if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_MASTER_PORT" ]; then
+    PARAMS="$PARAMS -master http://$JENKINS_MASTER_HOST:$JENKINS_MASTER_PORT/jenkins"
+  fi
+
+  CMD="java $JAVA_OPTS -jar $JAR -fsroot $HOME -username $JENKINS_SLAVE_USERNAME -password $JENKINS_SLAVE_PASSWORD -mode $JENKINS_SLAVE_MODE -name $JENKINS_SLAVE_NAME -executors $JENKINS_SLAVE_EXECUTORS $PARAMS $@"
+  echo Running $CMD
+  exec $CMD
+fi
+
+# Argument passed was not jenkins, so we assume the user wants to run their own process
+exec "$@"
