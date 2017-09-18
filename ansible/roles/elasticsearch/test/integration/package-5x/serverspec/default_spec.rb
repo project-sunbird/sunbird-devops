6abcabf54263f3415f3bc9c8ea43ff882baf2692
Deepak N <endeep123@gmail.com>
2017-09-18 11:01:54 +0530

    Issue #190 feat: remove code copied to public

--- ansible/roles/elasticsearch/test/integration/package-5x/serverspec/default_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'package_spec'
-
-
-describe 'Package Tests v 5.x' do
-  include_examples 'package::init', "5.2.2", ["ingest-attachment","ingest-geoip"]
-end
--- ansible/roles/swarm-agent-docker-prune/files/swarm-agent-docker-prune.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-master_node_ip=$1
-agent_nodes=$(ssh -i /run/secrets/ops-private-key ops@$master_node_ip "docker node ls -f role=worker --format {{.Hostname}}")
-for agent_node in $agent_nodes; do
-    echo ""
-    echo  "Cleaning node: $agent_node"
-    eval `ssh-agent -s` && ssh-add /run/secrets/ops-private-key && ssh -A -o StrictHostKeyChecking=no ops@$master_node_ip "ssh -o StrictHostKeyChecking=no $agent_node 'docker container prune -f && docker image prune -f'"
-done;
