7a51b7234dd9d77d0cd80dbe6f704825344a3d1d
Deepak N <endeep123@gmail.com>
2017-09-18 09:59:47 +0530

    Issue #190 feat: checkout devops code inside script

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
+
 # Temp line to remove folder till it is removed from code here
 ls -al
 rm -rf ansible/roles ansible/*.yml ansible/static-files
