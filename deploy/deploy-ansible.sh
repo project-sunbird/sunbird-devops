aed846f0f7fe9aabbecc033fb64258ac3f53cd7a
Kiran G <kirang20@gmail.com>
2017-09-21 16:11:04 +0530

    Issue ekstep/sunbird-devops#202 feat: deploy player service

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -5,6 +5,7 @@
 ENV=staging
 ORG=sunbird
 ACTOR_SERVICE_VERSION=0.0.1-gold
+PLAYER_VERSION=0.0.6-gold
 
 mkdir -p ../ansible/secrets
 touch "../ansible/secrets/$ENV.yml"
@@ -17,12 +18,10 @@ ansible-playbook -i ../ansible/inventory/$ENV ../ansible/bootstrap.yml  --extra-
 echo "@@@@@@@@@ Redeploy actor service"
 ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-sunbird" --extra-vars "hub_org=${ORG} image_name=actor-service image_tag=$ACTOR_SERVICE_VERSION service_name=actor-service deploy_actor=True"
 
+# Re-deploy Player service
+echo "@@@@@@@@@ Redeploy player service"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-sunbird" --extra-vars "hub_org=${ORG} image_name=player image_tag=${PLAYER_VERSION} service_name=player deploy_stack=True"
 
-## Re-deploy Player service
-#echo "Redeploy player service"
-#ensure_service_is_killed player
-#docker service create -p 3000:3000 --name player --reserve-memory $PLAYER_RESERVED_MEMORY --limit-memory $PLAYER_MEMORY_LIMIT --network $APPLICATION_NETWORK --env-file ./configs/sunbird_player.env sunbird/player:$PLAYER_VERSION
-#
 ## Re-deploy Content service
 #echo "Redeploy content service"
 #ensure_service_is_killed content_service
