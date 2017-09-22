01cf4a9da3195c1ff3aacf8911a3966f6179a358
Kiran G <kirang20@gmail.com>
2017-09-22 14:27:12 +0530

    Issue ekstep/sunbird-devops#202 feat: delete public inventory and group_var during ntp deploy

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -7,5 +7,6 @@ git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbi
 # Temp line to remove folder till it is removed from code here
 ls -al
 rm -rf ansible/roles ansible/*.yml ansible/static-files
+rm -rf sunbird-devops/ansible/inventory sunbird-devops/ansible/group_vars
 
 cp -r sunbird-devops/ansible/* ansible/
