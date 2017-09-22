620709933877b4bedd823ac55b69465944fe8db7
manoj v <manojv@ilimi.in>
2017-09-22 14:42:49 +0530

    Issue #0 fix : fixed mege conflicts

---
+++ deploy/deploy-keycloak.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Build script
+# set -o errexit
+
+ENV=staging
+ORG=sunbird
+KEYCLOAK_VERSION=3.2.1.Final-bronze
+
+mkdir -p ../ansible/secrets
+touch "../ansible/secrets/$ENV.yml"
+
+# Create application network
+echo "@@@@@@@@@ Keycloak deploy"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-keycloak" --extra-vars "deploy_keycloak1=true hub_org=${ORG} image_name=keycloak_image image_tag=$KEYCLOAK_VERSION service_name=keycloak1"
