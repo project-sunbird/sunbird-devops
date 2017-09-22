7ce9f0b1250a5fb4e2d7dd5b0f3b1bc6bf40cbcc
manoj v <manojv@ilimi.in>
2017-09-22 10:31:34 +0530

    Issue #0 fix : updated inventory and group_vars

---
+++ deploy/install-dbs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Build script
+# set -o errexit
+
+ENV=staging
+ORG=sunbird
+
+mkdir -p ../ansible/secrets
+touch "../ansible/secrets/$ENV.yml"
+
+#Elasticsearch installation
+echo "@@@@@@@@@ Elasticsearch installation"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/provision.yml --tags es
+
+# Cassandra installation
+echo "@@@@@@@@@ Cassandra installation"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags cassandra
+
+# Postgresql-master installation
+echo "@@@@@@@@@ Postgresql-master installation"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags  postgresql-master
