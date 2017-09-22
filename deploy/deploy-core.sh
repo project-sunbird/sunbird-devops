3ca512ab44acb8181c27285da63fb9d2eb57f506
Kiran G <kirang20@gmail.com>
2017-09-22 09:27:00 +0530

    Issue ekstep/sunbird-devops#202 feat: deploy api manager and echo server - 1

--- deploy/deploy-core.sh
+++ deploy/deploy-core.sh
@@ -31,13 +31,3 @@ ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stac
 # Re-deploy Learner service
 echo "Redeploy learner service"
 ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-sunbird" --extra-vars "hub_org=${ORG} image_name=learner_service image_tag=${LEARNER_SERVICE_VERSION} service_name=learner-service deploy_learner=True"
-
-## Re-deploy Content service
-#echo "Redeploy content service"
-#ensure_service_is_killed content_service
-#docker service create -p 5000:5000 --name content_service --reserve-memory $CONTENT_SERVICE_RESERVED_MEMORY --limit-memory $CONTENT_SERVICE_MEMORY_LIMIT --network $APPLICATION_NETWORK --env-file ./configs/sunbird_content_service.env sunbird/content_service:$CONTENT_SERVICE_VERSION
-#
-## Re-deploy Learner service
-#echo "Redeploy learner service"
-#ensure_service_is_killed learner_service
-#docker service create -p 9000:9000 --name learner_service --reserve-memory $LEARNER_SERVICE_RESERVED_MEMORY --limit-memory $LEARNER_SERVICE_MEMORY_LIMIT --network $APPLICATION_NETWORK --env-file ./configs/sunbird_learner_service.env sunbird/learner_service:$LEARNER_SERVICE_VERSION
