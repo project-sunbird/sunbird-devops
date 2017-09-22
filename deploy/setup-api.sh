499b033e223760fefcabcb42b9c1d99f4b9a8c3a
Kiran G <kirang20@gmail.com>
2017-09-22 10:18:55 +0530

    Issue ekstep/sunbird-devops#202 feat: deploy admin utils

--- deploy/setup-api.sh
+++ deploy/setup-api.sh
@@ -8,7 +8,12 @@ touch "../ansible/secrets/$ENV.yml"
 ENV=staging
 ORG=sunbird
 ECHO_SERVER_VERSION=0.0.2-silver
+ADMIN_UTILS_VERSION=0.0.1-SNAPSHOT
 
 # Deploy API Manager
 echo "@@@@@@@@@ Deploy API Manager"
 ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-api-manager" --extra-vars "hub_org=${ORG} echo_server_image_name=echo-server echo_server_image_tag=${ECHO_SERVER_VERSION}"
+
+# Deploy Admin Utils API
+echo "@@@@@@@@@ Deploy Admin Utils API"
+ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-adminutil" --extra-vars "hub_org=${ORG} image_name=adminutil image_tag=${ADMIN_UTILS_VERSION}"
