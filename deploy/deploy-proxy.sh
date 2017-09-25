e0f4ede28ef57d3e3f9355ac2aa0ce183d62a41f
Deepak N <endeep123@gmail.com>
2017-09-25 16:29:04 +0530

    Issue ekstep/sunbird-devops#202 feat: deploy proxy using inventory path

--- deploy/deploy-proxy.sh
+++ deploy/deploy-proxy.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-# Build script
-# set -o errexit
+set -e
 
-ENV=staging
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: Illegal number of parameters"
+    echo "Usage: $0 <inventory-path>"
+    exit 1
+fi
+
+INVENTORY_PATH=$1
+
 ORG=sunbird
 PROXY_VERSION=0.0.1-gold
 
-mkdir -p ../ansible/secrets
-touch "../ansible/secrets/$ENV.yml"
-
 # Bootstrap swarm
 echo "@@@@@@@@@ Bootstrap swarm"
-ansible-playbook -i ../ansible/inventory/$ENV ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm
+ansible-playbook -i $INVENTORY_PATH ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm
 
 # Re-deploy Proxy
 echo "Redeploy Proxy"
-ansible-playbook -i ../ansible/inventory/$ENV ../ansible/deploy.yml --tags "stack-proxy" --extra-vars "hub_org=${ORG} image_name=proxy image_tag=${PROXY_VERSION}"
+ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-proxy" --extra-vars "hub_org=${ORG} image_name=proxy image_tag=${PROXY_VERSION}"
