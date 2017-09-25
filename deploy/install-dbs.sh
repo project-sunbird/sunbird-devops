f628aa404c2f93a4c23a3c41998c920f4247f8ba
manoj v <manojv@ilimi.in>
2017-09-25 12:12:41 +0530

    Issue #0 fix : 0.0.6-gold version is updated for the player

--- deploy/install-dbs.sh
+++ deploy/install-dbs.sh
@@ -25,3 +25,9 @@ ansible-playbook -i ../ansible/inventory/$ENV ../ansible/provision.yml --tags ca
 # Postgresql-master installation
 echo "@@@@@@@@@ Postgresql-master installation"
 ansible-playbook -i ../ansible/inventory/$ENV ../ansible/provision.yml --tags  provision_postgres
+
+#mongo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org*
