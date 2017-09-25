deedf18fed2825f5a5fb21ef08a4c40973c5c902
Shashank Teotia <shashankteotia@gmail.com>
2017-09-25 16:58:23 +0530

    Issue ekstep/sunbird-devops#202 feat: added ARM templates for Azure setup

---
+++ cloud/azure/arm/swarm/acs-engine/production.sample/env.sh
@@ -0,0 +1,8 @@
+# Mention the Azure Subscription ID
+export AZURE_SUBSCRIPTION=""
+# Name of Azure Resource Group
+export AZURE_RG_NAME=
+# Azure resource group location
+export AZURE_RG_LOCATION=
+# A string to identify your deployment in Azure portal
+export AZURE_DEPLOYMENT_NAME=
