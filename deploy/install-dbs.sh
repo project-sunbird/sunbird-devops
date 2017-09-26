594266b970ac2705927bfa6f47dcd05647a4c0bf
manoj v <manojv@ilimi.in>
2017-09-26 14:34:13 +0530

    Issue #0 fix : mongodb role added

--- deploy/install-dbs.sh
+++ deploy/install-dbs.sh
@@ -28,9 +28,3 @@ ansible-playbook -i $INVENTORY_PATH ../ansible/provision.yml --tags cassandra
 echo "@@@@@@@@@ Postgresql-master installation"
 ansible-playbook -i $INVENTORY_PATH ../ansible/provision.yml --tags  postgresql-master
 
-#mongo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-apt-get update
-apt-get install -y mongodb-org*
-service mongod start
