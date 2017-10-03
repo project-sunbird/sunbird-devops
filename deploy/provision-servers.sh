5c7cf64c68044df872d921ef7c92f6ee2deb9f40
Shashank Teotia <shashankteotia@gmail.com>
2017-10-03 17:32:26 +0530

    Issue ekstep/sunbird-devops#212 feat: managing ssh for github

--- deploy/provision-servers.sh
+++ deploy/provision-servers.sh
@@ -12,7 +12,7 @@ fi
 
 AZURE_DEPLOY_SCRIPT=`pwd`/deploy-azure.sh
 repourl=git@github.com:Azure/acs-engine.git
-
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 if [ -d acs-engine ]; then 
     (cd acs-engine && git pull); 
 else 
