093f0969070d7dc67bb2144bdb83783b38565ba3
Shashank Teotia <shashankteotia@gmail.com>
2017-10-03 20:21:36 +0530

    Issue ekstep/sunbird-devops#212 feat: choosing between Alpine and Linux

--- ansible/installDeps.sh
+++ ansible/installDeps.sh
@@ -1,2 +1,11 @@
-apk -v --update --no-cache add jq
-apk -v add ansible=2.3.0.0-r1
+if grep -q Alpine /etc/os-release; then
+    apk -v --update --no-cache add jq
+    apk -v add ansible=2.3.0.0-r1
+else
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible
+fi
+
