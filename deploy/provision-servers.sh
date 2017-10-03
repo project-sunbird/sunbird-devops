2de5bba448438fff286714a81a6f3e1b6135ab50
Shashank Teotia <shashankteotia@gmail.com>
2017-10-03 20:45:09 +0530

    Issue ekstep/sunbird-devops#212 feat: modifying docker run command

--- deploy/provision-servers.sh
+++ deploy/provision-servers.sh
@@ -24,17 +24,18 @@ rm -rf ./scripts/deploy-azure.sh
 rm -rf ./deployments
 
 docker build --pull -t acs-engine .
-
-docker run --rm \
+docker version
+docker run \
+	--rm \
 	--privileged \
 	--security-opt seccomp:unconfined \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v `pwd`:/gopath/src/github.com/Azure/acs-engine \
-    -v ${APP_DEPLOYMENT_JSON_PATH}:/gopath/src/github.com/Azure/acs-engine/deployments/deployment/app \
-    -v ${DB_DEPLOYMENT_JSON_PATH}:/gopath/src/github.com/Azure/acs-engine/deployments/deployment/db \
-    -v ${AZURE_DEPLOY_SCRIPT}:/gopath/src/github.com/Azure/acs-engine/scripts/deploy-azure.sh \
+	-v ${APP_DEPLOYMENT_JSON_PATH}:/gopath/src/github.com/Azure/acs-engine/deployments/deployment/app \
+	-v ${DB_DEPLOYMENT_JSON_PATH}:/gopath/src/github.com/Azure/acs-engine/deployments/deployment/db \
+	-v ${AZURE_DEPLOY_SCRIPT}:/gopath/src/github.com/Azure/acs-engine/scripts/deploy-azure.sh \
 	-v ~/.azure:/root/.azure \
 	-w /gopath/src/github.com/Azure/acs-engine \
-		acs-engine /bin/bash ./scripts/deploy-azure.sh
+	acs-engine /bin/bash ./scripts/deploy-azure.sh
 
 chown -R "$(logname):$(id -gn $(logname))" . ~/.azure
