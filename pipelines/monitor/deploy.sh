a5f157df5405cf07147ae3a7b39afc238f80b906
Kiran G <kirang20@gmail.com>
2017-10-04 12:02:54 +0530

    Issue ekstep/sunbird-devops#205 feat: move monitoring job to use new structure variables

--- pipelines/monitor/deploy.sh
+++ pipelines/monitor/deploy.sh
@@ -3,4 +3,4 @@
 # set -o errexit
 
 ansible-playbook --version
-ansible-playbook -i ansible/inventory/${TARGET_ENV} ansible/monitoring.yml -v --vault-password-file /run/secrets/vault-pass
+ansible-playbook -i ansible/inventories/${TARGET_ENV} ansible/monitoring.yml -v --vault-password-file /run/secrets/vault-pass
