95b98ad19d4997675371de602da15a318911851f
Kiran G <kirang20@gmail.com>
2017-10-05 11:31:18 +0530

    Issue ekstep/sunbird-devops#205 feat: use new-structure branch of public sunbird-devops

--- pipelines/setupDevopsCodeNewStructure.sh
+++ pipelines/setupDevopsCodeNewStructure.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
+git -C sunbird-devops pull || git clone -b new-structure https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
 
 # Temp line to remove folder till it is removed from code here
 ls -al
