3561b70ee2c7a7730a94605c764d753cae550e7d
Deepak N <endeep123@gmail.com>
2017-10-05 09:31:17 +0530

    Issue ekstep/sunbird-devops#206 feat: add -y for apt

--- ansible/installDeps.sh
+++ ansible/installDeps.sh
@@ -2,10 +2,9 @@ if grep -q Alpine /etc/os-release; then
     apk -v --update --no-cache add jq
     apk -v add ansible=2.3.0.0-r1
 else
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get -y install software-properties-common
-    sudo apt-add-repository ppa:ansible/ansible
-    sudo apt-get update
+    sudo apt-add-repository ppa:ansible/ansible -y
+    sudo apt-get update -y
     sudo apt-get -y install ansible
 fi
-
