8324341962f9ac5078d82775208c55ca784c16bf
Deepak N <endeep123@gmail.com>
2017-10-06 13:16:51 +0530

    Issue ekstep/sunbird-devops#216 feat: use logging playbook

--- pipelines/logger/deploy.sh
+++ pipelines/logger/deploy.sh
@@ -4,4 +4,4 @@
 
 ansible-playbook --version
 
-ansible-playbook -i ansible/inventories/${ENV} sunbird-devops/ansible/deploy.yml --tags "stack-logger" --vault-password-file  /run/secrets/vault-pass
+ansible-playbook -i ansible/inventories/${ENV} sunbird-devops/ansible/logging.yml --vault-password-file  /run/secrets/vault-pass
