e3bedcf45fbea784f88feca9cc60d856da6d79ed
Kiran G <kirang20@gmail.com>
2017-10-06 11:35:27 +0530

    Issue ekstep/sunbird-devops#205 feat: use new playbook

--- pipelines/monitor/deployOldStructure.sh
+++ pipelines/monitor/deployOldStructure.sh
@@ -3,4 +3,4 @@
 # set -o errexit
 
 ansible-playbook --version
-ansible-playbook -i ansible/inventory/${TARGET_ENV} ansible/monitoring.yml -vv --tags "stack-monitor" --vault-password-file /run/secrets/vault-pass
+ansible-playbook -i ansible/inventory/${TARGET_ENV} ansible/monitoring.yml --vault-password-file /run/secrets/vault-pass
