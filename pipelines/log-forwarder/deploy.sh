773e9c1e3bfd68214a463e4622610e646ea1be98
Deepak N <endeep123@gmail.com>
2017-10-06 13:15:39 +0530

    Issue ekstep/sunbird-devops#216 feat: Separate logging stack

--- pipelines/log-forwarder/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-
-ansible-playbook --version
-ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/ops.yml --limit '!localhost' --tags "log-forwarder" -v --vault-password-file /run/secrets/vault-pass
