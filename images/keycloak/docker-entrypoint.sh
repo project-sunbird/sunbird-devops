eaeb16636f095901689141fd0326451224483927
manoj v <manojv@ilimi.in>
2017-10-09 15:24:16 +0530

    Issue #00 fix : Parameterise keycloak

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -3,7 +3,7 @@ export HOSTNAME_IP=$(hostname -i)
 echo "hostname -i returned: $HOSTNAME_IP"
 echo "export POSTGRES_PASSWORD=`cat /run/secrets/postgresql_keycloak_pass`" >> ~/.bashrc
 cd keycloak/bin
-./add-user-keycloak.sh -u $1 -p $2
+./add-user-keycloak.sh -u $KEYCLOAK_ADMIN_USERNAME -p $KEYCLOAK_ADMIN_PASSWORD
 source ~/.bashrc
 echo "TCPPING_INITIAL_HOSTS: $TCPPING_INITIAL_HOSTS"
 exec /opt/jboss/keycloak/bin/standalone.sh -b $HOSTNAME_IP -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
--- images/keycloak/dockerPushToRepo.sh
+++ images/keycloak/dockerPushToRepo.sh
@@ -13,7 +13,6 @@ name=$(e "${m}" "name")
 version=$(e "${m}" "version")
 artifactLabel=${ARTIFACT_LABEL:-bronze}
 
-
 docker login -u "purplesunbird" -p`cat /home/ops/vault_pass`
 docker push ${org}/${name}:${version}-${artifactLabel}
 docker logout
