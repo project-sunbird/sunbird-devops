1bd3a3a283eca1a8dede1a22444abdfe45d3527e
Kiran G <kirang20@gmail.com>
2017-10-09 12:27:26 +0530

    Issue ekstep/sunbird-devops#205 feat: update setup devops code

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-rm -rf sunbird-devops
 git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
 
 # Temp line to remove folder till it is removed from code here
