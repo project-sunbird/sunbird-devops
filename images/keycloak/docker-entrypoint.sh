e489c35bbd0c63c3a23ec89d26fc5cd99d82e508
manoj v <manojv@ilimi.in>
2017-10-12 15:30:21 +0530

    Issue ekstep/sunbird-devops#217 fix : Syntax error  of keycloak password variable

--- images/keycloak/docker-entrypoint.sh
+++ images/keycloak/docker-entrypoint.sh
@@ -3,7 +3,7 @@ export HOSTNAME_IP=$(hostname -i)
 echo "hostname -i returned: $HOSTNAME_IP"
 echo "export POSTGRES_PASSWORD=`cat /run/secrets/postgresql_keycloak_pass`" >> ~/.bashrc
 cd keycloak/bin
-./add-user-keycloak.sh -u $KEYCLOAK_ADMIN_USERNAME -p $KEYCLOAK_ADMIN_PASSWORD
+./add-user-keycloak.sh -u $KEYCLOAK_ADMIN_USERNAME -p $KEYCLOAK_ADMIN_INITIAL_PASSWORD
 source ~/.bashrc
 echo "TCPPING_INITIAL_HOSTS: $TCPPING_INITIAL_HOSTS"
 exec /opt/jboss/keycloak/bin/standalone.sh -b $HOSTNAME_IP -Djboss.bind.address.private=$HOSTNAME_IP -Djboss.jgroups.tcpping.initial_hosts=$TCPPING_INITIAL_HOSTS $@
