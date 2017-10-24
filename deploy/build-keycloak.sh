872eeb0efb07bac285ddde5e72ed03047f88adff
manoj v <manojv@ilimi.in>
2017-10-24 16:20:58 +0530

    Issue #00 fix : keycloak scripts for nile

--- deploy/build-keycloak.sh
+++ deploy/build-keycloak.sh
@@ -3,4 +3,4 @@ wget https://downloads.jboss.org/keycloak/3.2.0.Final/keycloak-3.2.0.Final.tar.g
 tar -xvf keycloak-3.2.0.Final.tar.gz
 mv keycloak-3.2.0.Final keycloak
 tar -czvf keycloak.tar.gz  keycloak
-cp keycloak.tar.gz ../ansible/
+mv keycloak.tar.gz $1
