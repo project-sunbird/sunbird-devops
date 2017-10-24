94e5e56dfae05df8f9c7000bead42c80e7fb279b
Deepak N <endeep123@gmail.com>
2017-10-24 12:25:26 +0530

    Issue ekstep/sunbird-devops#233 feat: remove temp lines from setup devops script

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -3,8 +3,3 @@
 set -ex
 
 git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
-
-# Temp line to remove folder till it is removed from code here
-ls -al
-rm -rf ansible/roles ansible/*.yml ansible/static-files ansible/group_vars ansible/inventory ansible/secrets
-ls -al
