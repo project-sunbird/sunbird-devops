395725ee6c845f154504a46c0e46cc8475002d2f
Deepak N <endeep123@gmail.com>
2017-10-24 12:04:32 +0530

    Issue ekstep/sunbird-devops#233 feat: remove unused deploy scripts

--- pipelines/log-es-restore/deployOldStructure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-
-ansible-playbook --version
-ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/es.yml --tags "log_es_restore" --extra-vars "snapshot_number=$SNAPSHOT_NUMBER" -v --vault-password-file /run/secrets/vault-pass
--- pipelines/logger/deployOldStructure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-
-ansible-playbook --version
-
-ansible-playbook -i ansible/inventory/${ENV} ansible/logging.yml --vault-password-file  /run/secrets/vault-pass
