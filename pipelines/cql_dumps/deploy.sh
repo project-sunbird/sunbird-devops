81a3cd8261df57a86f3e2376cff89a8bdfec06ed
manoj v <manojv@ilimi.in>
2017-10-26 11:55:49 +0530

    Issue #00 fix : vault pass added to slave

--- pipelines/cql_dumps/deploy.sh
+++ pipelines/cql_dumps/deploy.sh
@@ -9,4 +9,4 @@ set -e
 env=${ENV:-null}
 
 ansible-playbook --version
-ansible-playbook -i ansible/inventories/$ENV sunbird-devops/ansible/reporting.yml --vault-password-file /home/ops/vault-pass
+ansible-playbook -i ansible/inventories/$ENV sunbird-devops/ansible/reporting.yml --vault-password-file "/home/ops/vault-pass"
