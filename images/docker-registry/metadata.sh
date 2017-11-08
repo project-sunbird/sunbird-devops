0651110e6b2a6d4f35368b0e002c38b8607d0eb4
manoj v <manojv@ilimi.in>
2017-11-08 16:39:57 +0530

    Issue sunbird-devops#292 fix : org in the metadata.sh updated

--- images/docker-registry/metadata.sh
+++ images/docker-registry/metadata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # return version
-echo '{"name":"registry_image","version":"2.0","org":"ntp","hubuser":"purplesunbird"}'
+echo '{"name":"registry_image","version":"2.0","org":"sunbird","hubuser":"purplesunbird"}'
 
