ebf032f07eefa26a66e2f583f4ff276257210cfb
Deepak N <endeep123@gmail.com>
2017-11-08 19:11:00 +0530

    Issue ekstep/sunbird-devops#288 feat: Deleted dashboard should not reappear due to old directory

--- ansible/roles/grafana-dashboards-export/templates/export-dashboards-to-git-repo.sh
+++ ansible/roles/grafana-dashboards-export/templates/export-dashboards-to-git-repo.sh
@@ -5,8 +5,12 @@ set -e
 GIT_REPO_URL={{ grafana_dashboards_git_repo_url_with_credentails }}
 rm -rf grafana-dashboards
 git clone $GIT_REPO_URL grafana-dashboards
+
+# Remove old dashboards
+rm -rf dashboards
 rm -rf grafana-dashboards/dashboards/*
 
+# Import and copy dashboards
 wizzy import dashboards
 cp dashboards/* grafana-dashboards/dashboards/
 
