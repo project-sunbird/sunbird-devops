64a1b223801cd7025e97cbba98f2018ad19257c9
Deepak N <endeep123@gmail.com>
2017-11-17 10:58:14 +0530

    Issue ekstep/sunbird-devops#139 feat: echo commands from post install script for swarm nodes

--- deploy/docker-swam-node-post-install.sh
+++ deploy/docker-swam-node-post-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 # This is to ensure we don't get below error
 # Failed to start container manager: inotify_init: too many open files
 # Reference: https://github.com/moby/moby/issues/1044
