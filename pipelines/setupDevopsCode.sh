e769938d1e6d5740d819d0cdc7e4f7a42a8a1280
Deepak N <endeep123@gmail.com>
2017-11-24 16:10:45 +0530

    Issue project-sunbird/sunbird-commons#341 feat: xargs option to ignore empty

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -5,12 +5,17 @@ set -ex
 COMMIT_REF=${1:-master}
 SUNBIRD_DEVOPS_DIR_NAME=sunbird-devops
 
+XARGS_OPTION_TO_IGNORE_EMPTY=''
+if [[ `uname` == 'Linux' ]]; then
+   XARGS_OPTION_TO_IGNORE_EMPTY='--no-run-if-empty'
+fi
+
 if [ -d $SUNBIRD_DEVOPS_DIR_NAME ]; then
     cd $SUNBIRD_DEVOPS_DIR_NAME
     git reset --hard && git clean -fd
     git checkout master
     git pull --all
-    git branch | grep -v "master" | xargs git branch -D
+    git branch | grep -v "master" | xargs $XARGS_OPTION_TO_IGNORE_EMPTY git branch -D
 else
     git clone https://github.com/project-sunbird/sunbird-devops.git $SUNBIRD_DEVOPS_DIR_NAME
     cd $SUNBIRD_DEVOPS_DIR_NAME
