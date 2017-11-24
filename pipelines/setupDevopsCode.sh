53efda88ff85b0171257400c6169a389aae70a7b
Deepak N <endeep123@gmail.com>
2017-11-24 15:23:27 +0530

    Issue project-sunbird/sunbird-commons#341 feat: Ability to checkout a branch, tag or commit ref

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,4 +2,18 @@
 
 set -ex
 
-git -C sunbird-devops pull || git clone https://github.com/project-sunbird/sunbird-devops.git sunbird-devops
+COMMIT_REF=${1:-master}
+SUNBIRD_DEVOPS_DIR_NAME=sunbird-devops
+
+if [ -d $SUNBIRD_DEVOPS_DIR_NAME ]; then
+    cd $SUNBIRD_DEVOPS_DIR_NAME
+    git reset --hard && git clean -fd
+    git checkout master
+    git pull --all
+    git branch | grep -v "master" | xargs git branch -D
+else
+    git clone https://github.com/project-sunbird/sunbird-devops.git $SUNBIRD_DEVOPS_DIR_NAME
+    cd $SUNBIRD_DEVOPS_DIR_NAME
+fi
+
+git checkout $COMMIT_REF
