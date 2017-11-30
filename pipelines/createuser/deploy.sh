1e4e291aa31b6245ba287391cd90097180aef01d
manoj v <manojv@ilimi.in>
2017-11-30 16:44:37 +0530

    Issue #345 chore: changed to release-1.3

--- pipelines/createuser/deploy.sh
+++ pipelines/createuser/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 pass=`mkpasswd -m sha-512 $3`
 ansible-playbook --version
+ansible-playbook --limit 172.16.0.5 -i ansible/inventories/$ENV sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars="remote=172.16.0.5 group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
 ansible-playbook --limit $1 -i ansible/inventories/$ENV sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars="remote=$1 group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-COMMIT_REF=${1:-master}
+COMMIT_REF=${1:-release-1.3}
 SUNBIRD_DEVOPS_DIR_NAME=sunbird-devops
 
 XARGS_OPTION_TO_IGNORE_EMPTY=''
@@ -13,9 +13,9 @@ fi
 if [ -d $SUNBIRD_DEVOPS_DIR_NAME ]; then
     cd $SUNBIRD_DEVOPS_DIR_NAME
     git reset --hard && git clean -fd
-    git checkout master
+    git checkout release-1.3
     git pull --all
-    git branch --list | grep -v "master" | xargs $XARGS_OPTION_TO_IGNORE_EMPTY git branch -D
+    git branch --list | grep -v "release-1.3" | xargs $XARGS_OPTION_TO_IGNORE_EMPTY git branch -D
 else
     git clone https://github.com/project-sunbird/sunbird-devops.git $SUNBIRD_DEVOPS_DIR_NAME
     cd $SUNBIRD_DEVOPS_DIR_NAME
