2b534da4db9c927adf4cef73a95e201ba5c6e2ce
manoj v <manojv@ilimi.in>
2017-12-01 14:13:50 +0530

    Issue #345 chore: updated bastion-host of admin inventory

--- pipelines/createuser/deploy.sh
+++ pipelines/createuser/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pass=`mkpasswd -m sha-512 $3`
 ansible-playbook --version
-ansible-playbook -i ansible/inventories/admin sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars="remote=bastion-host group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
-ansible-playbook -i ansible/inventories/$ENV sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars="remote=$1 group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
+ansible-playbook -i ansible/inventories/admin sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars "remote=bastion-host group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
+ansible-playbook -i ansible/inventories/$ENV sunbird-devops/ansible/createuser.yml --tags "add-user" --extra-vars "remote=$1 group=$2 user=$2 password=$pass public_key=$4" --vault-password-file /run/secrets/vault-pass
