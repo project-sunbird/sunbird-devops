9089b75e4c6b04e593b392378e2f53be4752b81e
manoj v <manojv@ilimi.in>
2017-12-01 14:06:37 +0530

    Issue #345 chore: updated bastion-host of admin inventory

--- pipelines/createuser/installDeps.sh
+++ pipelines/createuser/installDeps.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 # Build script
 # set -o errexit
-# apk -v --update --no-cache add jq
-# apk -v --update --no-cache add ansible=2.3.0.0-r1
-# apk -v --update --no-cache add rsync
-# apk -v --update --no-cache add whois
-apt-get update
-apt-get install -y whois
-apt-get install -y jq
-
+apk -v --update --no-cache add jq
+apk -v --update --no-cache add ansible=2.3.0.0-r1
+apk -v --update --no-cache add rsync
+apk -v --update --no-cache add whois
