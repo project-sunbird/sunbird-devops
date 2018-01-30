1018f7b209ebe8d8b83924a2b449bef4acb105a9
rjshrjndrn <rjshrjndrn@gmail.com>
2018-01-30 12:52:19 +0530

    Issue #23 feat: restore cassandra to a fresh installation

---
+++ deploy/complete_restore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##############################################
+# This scipt is designed to restore data to  #
+# fresh installation of cassandra            #
+##############################################
+
+if [ $# -ne 1 ];then
+    echo -e "$0 <path to cassandra backup root dir>"
+    exit 1
+fi
+
+CASS_DATA_PATH=/var/lib/cassandra/data
+CASS_ROOT_PATH=$1
+
+echo 'Restoring schemas'
+for schema in $CASS_ROOT_PATH/schemas/*.schema; do
+    cqlsh $CASS_IP -e "source '$schema'"
+done
+
+echo 'Restoring KeySpaces'
+
+for keyspace in $CASS_ROOT_PATH/keyspace_backup/*;do
+    for table in $keyspace/*; do
+        sstableloader -d localhost $table
+    done
+done
