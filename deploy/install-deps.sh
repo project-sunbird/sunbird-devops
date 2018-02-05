c86bfb4e8439648aab68daf6a0a03dcfd981ad5a
rjshrjndrn <rjshrjndrn@gmail.com>
2018-02-05 11:17:13 +0530

    Issue #22 feat: idempotency

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -7,43 +7,47 @@ ANSIBLE_VERSION=2.4.1.0
 SWARM_MASTER_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 echo "MasterIP: $SWARM_MASTER_IP"
 
-# Remove other versions of docker
-if [ $(which docker) ];
-apt-get -y remove docker docker-engine docker.io
+# Check for docker
+if [ ! $(which docker) ];then
+    # Remove other versions of docker
+    apt-get -y remove docker docker-engine docker.io
+
+    # Install extra packages needed for docker
+    apt-get -y update && \
+      apt-get -y install \
+        linux-image-extra-$(uname -r) \
+        linux-image-extra-virtual
+
+    # Setup docker repository
+    apt-get -y install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    apt-key fingerprint 0EBFCD88
+    add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    apt-get -y update
+
+    # Install Docker engine
+    apt-get -y install docker-ce=$DOCKER_VERSION
+
+    # Setup docker to system service
+    systemctl enable docker
+    systemctl restart docker
+
+    # Initialise Docker Swarm, with current machine as Master (which is active)
+    docker swarm init --advertise-addr $SWARM_MASTER_IP
+
+    docker node ls
 fi
 
-# Install extra packages needed for docker
-apt-get -y update && \
-  apt-get -y install \
-    linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual
-
-# Setup docker repository
-apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get -y update
-
-# Install Docker engine
-apt-get -y install docker-ce=$DOCKER_VERSION
-
+# Checking for ansible
+if [ ! $(which ansible) ];then
 # Install Ansible
-apt install -y python-pip
-pip install ansible==$ANSIBLE_VERSION
-
-# Setup docker to system service
-systemctl enable docker
-systemctl restart docker
-
-# Initialise Docker Swarm, with current machine as Master (which is active)
-docker swarm init --advertise-addr $SWARM_MASTER_IP
-
-docker node ls
+    apt install -y python-pip
+    pip install ansible==$ANSIBLE_VERSION
+fi
