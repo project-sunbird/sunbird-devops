7ee30fcd8525cdf72ee25075da585dde0f50bd0d
rjshrjndrn <rjshrjndrn@gmail.com>
2018-02-11 10:39:25 +0530

    Issue #39 feat: Install sunbird_deps

--- deploy/sunbird_install.sh
+++ deploy/sunbird_install.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-usage() { echo "Usage: $0 [ -s {config|dbs|apis|proxy|keycloak} ]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [ -s {config|dbs|apis|proxy|keycloak|core} ]" &>/dev/null; exit 0; }
 
 # Reading environment and implimentation name
 implimentation_name=$(awk '/implementation_name: / {print $2}' config)
@@ -10,7 +10,6 @@ env_name=$(awk '/env: / {print $2}' config)
 app_host=$(awk '/application_host: / {print $2}' config)
 db_host=$(awk '/database_host: / {print $2}' config)
 ssh_ansible_user=$(awk '/ssh_ansible_user: / {print $2}' config)
-ssh_ansible_file=$(awk '/ssh_ansible_file: / {print $2}' config)
 ansible_private_key_path=$(awk '/ansible_private_key_path: / {print $2}' config)
 ansible_variable_path=$implimentation_name-devops/ansible/inventories/$env_name
 
@@ -23,14 +22,13 @@ if [ ! -d logs ];then mkdir logs &> /dev/null;fi
 # Installing dependencies
 deps() { sudo ./install-deps.sh; }
 
+
 # Generating configs
 config() { 
     time ./generate-config.sh $implimentation_name $env_name core; 
     # Creating inventory
     sed -i s#\"{{database_host}}\"#$db_host#g $ansible_variable_path/hosts
     sed -i s#\"{{application_host}}\"#$app_host#g $ansible_variable_path/hosts
-    sed -i s#\"{{ssh_ansible_user}}\"#$ssh_ansible_user#g $ansible_variable_path/hosts
-    sed -i s#\"{{ssh_ansible_file}}\"#$ssh_ansible_file#g $ansible_variable_path/hosts
     sed -i s#\"{{ansible_private_key_path}}\"#$ansible_private_key_path#g $ansible_variable_path/hosts
 }
 
@@ -46,6 +44,9 @@ proxy() { ./deploy-proxy.sh $ansible_variable_path; }
 
 # Keycloak
 keycloak() { ./provision-keycloak.sh $ansible_variable_path; ./deploy-keycloak-vm.sh $ansible_variable_path; }
+
+# Core
+core() { ./deploy-core.sh $ansible_variable_path; }
 
 while getopts "s:h" o;do
     case "${o}" in
@@ -73,6 +74,8 @@ while getopts "s:h" o;do
                     echo -e "\n$(date)\n">>keycloak.log; keycloak 2>&1 | tee -a logs/keycloak.log
                     exit 0
                     ;;
+                core)
+                    echo -e "\n$(date)\n">>core.log; core2>&1 | tee -a logs/core.log
                 *)
                     usage
                     exit 0
@@ -89,6 +92,8 @@ done
 # Default action: install and configure from scratch
 echo -e \n$(date)\n >> deps.log; deps 2>&1 | tee -a logs/deps.log
 echo -e \n$(date)\n >> config.log; config 2>&1 | tee -a logs/config.log
+# Installing sunbird_ansible prerequisites
+ansible-playbook -i $ansible_variable_path/$env_name ../ansible/sunbird_prerequisites.yml --extra-vars ansible_ssh_user=$ansible_ssh_user
 echo -e \n$(date)\n >> dbs.log; dbs 2>&1 | tee -a logs/dbs.log
 echo -e \n$(date)\n >> apis.log; apis 2>&1 | tee -a logs/apis.log
 echo -e \n$(date)\n >> proxies.log; proxy 2>&1 | tee -a logs/proxies.log
