8162e4ae78dadb6de99b3179860381c6452ca987
manoj v <manojv@ilimi.in>
2018-02-14 16:23:01 +0530

    Issue #00 chore: Add --extra-vars @config

--- deploy/sunbird_install.sh
+++ deploy/sunbird_install.sh
@@ -76,6 +76,7 @@ while getopts "s:h" o;do
                     ;;
                 core)
                     echo -e "\n$(date)\n">>core.log; core2>&1 | tee -a logs/core.log
+                    exit 0
                     ;;
                 *)
                     usage
