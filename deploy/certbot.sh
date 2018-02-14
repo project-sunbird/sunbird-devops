c3d326d35867e1e79fd3c03a93df4a901a9bcdc1
rjshrjndrn <rjshrjndrn@gmail.com>
2018-02-14 17:27:45 +0530

    Issue #47 fix: full chain needs for complete ssl verification

--- deploy/certbot.sh
+++ deploy/certbot.sh
@@ -28,13 +28,13 @@ fi
 sudo certbot certonly --standalone -d $dns_name
 
 sudo cp -r $certbot_home/cert1.pem $certbot_home/privkey1.pem /home/$ssh_ansible_user/
-sudo chown -R $ssh_ansible_user:$ssh_ansible_user /home/$ssh_ansible_user/cert1.pem /home/$ssh_ansible_user/privkey1.pem
-sudo chmod 775 /home/$ssh_ansible_user/cert1.pem /home/$ssh_ansible_user/privkey1.pem
+sudo chown -R $ssh_ansible_user:$ssh_ansible_user /home/$ssh_ansible_user/fullchain1.pem /home/$ssh_ansible_user/site.crt
+sudo chmod 775 /home/$ssh_ansible_user/cert1.pem /home/$ssh_ansible_user/site.key
 
 
 echo -e "Please take a note of these, and fill it up in config file: \
     \n\n    dns_name: $dns_name \n
-    cert_path: /home/$ssh_ansible_user/cert1.pem \n
-    key_path: /home/$ssh_ansible_user/privkey1.pem\n
+    cert_path: /home/$ssh_ansible_user/site.crt \n
+    key_path: /home/$ssh_ansible_user/site.key\n
 
 please remove after the installation process. or keep these in a safe place."
