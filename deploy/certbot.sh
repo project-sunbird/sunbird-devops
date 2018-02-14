c5c270a8fb3665c8e948c85a22d023c8afb06993
rjshrjndrn <rjshrjndrn@gmail.com>
2018-02-14 17:45:24 +0530

    Issue #47 fix: stdout formatting

--- deploy/certbot.sh
+++ deploy/certbot.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 # This script won't work for aws, as it's black listed
 
-echo -e "This script won't work for aws, as it's black listed in letsencrypt\n so if youre running on aws please press ctrl+c with in 5 seconds"
+echo -e "This script won't work for aws, as it's black listed in letsencrypt\nso if youre running on aws please press ctrl+c with in 5 seconds"
 
 sleep 5
 
@@ -37,5 +37,4 @@ echo -e "Please take a note of these, and fill it up in config file: \
     \n\n    dns_name: $dns_name \n
     cert_path: /home/$ssh_ansible_user/site.crt \n
     key_path: /home/$ssh_ansible_user/site.key\n
-
-please remove after the installation process. or keep these in a safe place."
+!!! please remove certs after the installation process. or keep it in a safe place."
