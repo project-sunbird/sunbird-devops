c34c44c4c9b2f79f9c12c2123f8e8d3639abf0e6
Ubuntu <ubuntu@ip-20-0-0-85.ap-south-1.compute.internal>
2018-02-27 09:34:42 +0000

    Issue #56 feat: onboarding apis using docker ansible container

---
+++ deploy/onboard-apis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: Illegal number of parameters"
+    echo "Usage: $0 <inventory-path>"
+    exit 1
+fi
+
+inventory_path="deploy/$1"
+ansible_mount_dir=$(readlink -f ../)
+
+# Creating container to onboard apis and consumers
+
+echo "@@@@@@@@ Creating ansible service"
+
+# Creating ansible service
+sudo docker service create --name ansible_container --mount source=$ansible_mount_dir,target=/ansible,type=bind,readonly --network api-manager_default sunbird/ansible:latest
+
+# Waiting for service to start
+sleep 5
+
+# Getting container id
+ansible_container=$(sudo docker ps | grep sunbird/ansible | awk '{print $1}')
+
+# Onboard APIs
+echo "@@@@@@@@@ Onboard APIs"
+sudo docker exec $ansible_container ansible-playbook -i $inventory_path ansible/api-manager.yml --tags kong-api  --extra-vars=@deploy/config --connection local
+
+# Onboard Consumers
+echo "@@@@@@@@@ Onboard Consumers"
+sudo docker exec $ansible_container ansible-playbook -v -i $inventory_path ansible/api-manager.yml --tags kong-consumer --extra-vars=@deploy/config --connection local
+
+# Removing service as it's not needed anymore
+sudo docker service rm ansible_container
+sleep 5
+sudo docker rmi sunbird/ansible:latest
+sudo docker image prune -f
