cbd4df194ba8c609280efc1e0f314f4347733196
rjshrjndrn <rjshrjndrn@gmail.com>
2018-03-26 17:51:31 +0530

    Issue #56 fix: Dynamically interpretting the kong ip

--- deploy/deploy-apis.sh
+++ deploy/deploy-apis.sh
@@ -13,14 +13,25 @@ ORG=sunbird
 ECHO_SERVER_VERSION=0.0.2-silver
 ADMIN_UTILS_VERSION=0.0.1-SNAPSHOT-gold
 
+# Saving kong api url
+kong_admin_api_url=$(docker service ps api-manager_kong | grep Runn | head -n1 | awk 'print $4')
+
 # Bootstrap swarm
 echo "@@@@@@@@@ Bootstrap swarm"
-ansible-playbook -i $INVENTORY_PATH ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm --extra-vars=@config --extra-vars=@advanced
+ansible-playbook -i $INVENTORY_PATH ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm
 
 # Deploy API Manager
 echo "@@@@@@@@@ Deploy API Manager"
-ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-api-manager" --extra-vars "hub_org=${ORG} echo_server_image_name=echo-server echo_server_image_tag=${ECHO_SERVER_VERSION}" --extra-vars=@config --extra-vars=@advanced
+ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-api-manager" --extra-vars "hub_org=${ORG} echo_server_image_name=echo-server echo_server_image_tag=${ECHO_SERVER_VERSION}"
 
 # Deploy Admin Utils API
 echo "@@@@@@@@@ Deploy Admin Utils API"
-ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-adminutil" --extra-vars "hub_org=${ORG} image_name=adminutil image_tag=${ADMIN_UTILS_VERSION}" --extra-vars=@config --extra-vars=@advanced
+ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-adminutil" --extra-vars "hub_org=${ORG} image_name=adminutil image_tag=${ADMIN_UTILS_VERSION}"
+
+# Onboard APIs
+echo "@@@@@@@@@ Onboard APIs"
+ansible-playbook -i $INVENTORY_PATH ../ansible/api-manager.yml --tags kong-api --extra-vars kong_admin_api_url=http://$kong_admin_api_url:8001
+
+# Onboard Consumers
+echo "@@@@@@@@@ Onboard Consumers"
+ansible-playbook -v -i $INVENTORY_PATH ../ansible/api-manager.yml --tags kong-consumer --extra-vars kong_admin_api_url=http://$kong_admin_api_url:8001
