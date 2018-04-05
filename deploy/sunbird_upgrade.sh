86e59e72eaa05e154a37d91699d2a15ca6a85168
manoj v <manojv@ilimi.in>
2018-04-05 17:27:47 +0530

    Issue #00 chore: update the migration scripts

--- deploy/sunbird_upgrade.sh
+++ deploy/sunbird_upgrade.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+export sunbird_cassandra_host=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
+export sunbird_cassandra_port=9042
+
 cd /tmp
 
 #Check cassandra migration jar file present or not inside /tmp
@@ -11,3 +14,4 @@ wget https://github.com/project-sunbird/sunbird-utils/releases/download/R1.5/cas
 
 # Start the java process to apply cassandra migration 
 nohup java -cp "cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar" com.contrastsecurity.cassandra.migration.utils.MigrationScriptEntryPoint &
+
