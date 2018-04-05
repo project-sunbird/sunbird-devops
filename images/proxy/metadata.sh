978f9a9d12714c9f847cea6cce962867dec193a1
manoj v <manojv@ilimi.in>
2018-04-05 11:32:45 +0530

    Issue #00 chore: change the proxy image version 0.0.1 to 1.5.0

--- images/proxy/metadata.sh
+++ images/proxy/metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"proxy","version":"0.0.1","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"proxy","version":"1.5.0","org":"sunbird","hubuser":"purplesunbird"}'
