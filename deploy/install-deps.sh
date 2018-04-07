86d6c55a4de190df13d5a441cd309f74c79a94c5
rjshrjndrn <rjshrjndrn@gmail.com>
2018-04-07 10:39:50 +0530

    Issue #39 fix: error routing to /dev/null

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -5,7 +5,7 @@
 ansible_version=2.4.1.0
 
 # Checking for ansible
-case "$(ansible --version | head -n1)" in 
+case "$(ansible --version 2> /dev/null | head -n1)" in 
     *2.4.1.0*)
         ;;
      *)
