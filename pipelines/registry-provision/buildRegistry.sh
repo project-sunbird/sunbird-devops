ee2dc7ee5e0de797ec1505eb0f4c9facf50f290c
Santhosh Gandham <santhosh.g@optit.in>
2018-04-10 14:57:37 +0530

    Issue #406 feat: updated jenkins pipeline

--- pipelines/registry-provision/buildRegistry.sh
+++ pipelines/registry-provision/buildRegistry.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-cd open-saber/java
-cp registry/src/main/resources/config-prod.properties.sample registry/src/main/resources/config-prod.properties 
-cp registry/src/main/resources/application.properties.sample registry/src/main/resources//application.properties 
-cp registry/src/main/resources/validations.shex.sample registry/src/main/resources//validations.shex 
-cp registry/src/main/resources/schema-configuration.jsonld.sample registry/src/main/resources/schema--configuration.jsonld 
-cp registry/src/main/resources/frame.json.sample registry/src/main/resources//frame.json 
-
+cd open-saber/
+./configure-dependencies.sh
+cd java
 mvn clean install
 
 cd registry 
