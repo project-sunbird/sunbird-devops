4edbbf1d7efecb40e0e907802e79f7ce5777da0a
rjshrjndrn <rajesh.r@optit.co>
2018-04-24 08:13:24 +0000

    Issue #109 feat: tasks

--- ansible/roles/stack-badger/files/user.sh
+++ ansible/roles/stack-badger/files/user.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+set -x
 
-# Checking for container
-container=$(docker ps | grep badger | awk '{print $1}')
-
-docker exec $container python /badger/code/manage.py migrage
-docker exec $container  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('{{badger_admin_user}}', '{{badger_admin_email}}', '{{badger_admin_password}}')" |  python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('{{badger_admin_user}}', '{{badger_admin_email}}', '{{badger_admin_password}}')" |  python /badger/code/manage.py shell
