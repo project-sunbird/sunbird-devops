34e0e8aa56d0a7d98f188e5812438745ae9b188e
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-07 14:57:07 +0530

    Issue #000 fix: ansible version

--- pipelines/sunbird-badger/installDeps.sh
+++ pipelines/sunbird-badger/installDeps.sh
@@ -4,4 +4,4 @@
 sudo apk add --no-cache gcc git libffi-dev musl-dev openssl-dev perl py-pip python python-dev sshpass
 apk -v --update --no-cache add jq
 # apk -v --no-cache add ansible=2.3.0.0-r1
-pip install ansible=2.4.1
+pip install ansible==2.4.1
