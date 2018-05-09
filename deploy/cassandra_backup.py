f9c6eee1c3efb4469b05ebc95191b379bea4155e
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-09 16:09:57 +0530

    Issue #SB-3229 feat: Created backup script for cassandra

--- deploy/cassandra_backup.py
+++ deploy/cassandra_backup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-
 '''
  Please run nodetool snapshot -t <snap_name> before running the script
-
  script to copy cassandra snaps to a remote dir
  usage script  --datadir /path/to/datadir \
                --snapshotname snapshot name
@@ -11,32 +9,45 @@
                                 --snapshotname my_snapshot
                                 --targetdir /backups/cassandra/$(date +%Y-%m-%d)
 '''
-
-from os import path, walk, sep
-import argparse
+from os import path, walk, sep, system
+from argparse import ArgumentParser 
 from shutil import rmtree, ignore_patterns, copytree
 from re import match, compile
+from sys import exit
 
-parser = argparse.ArgumentParser()
-parser.add_argument("datadir")
-parser.add_argument("snapshotname")
-parser.add_argument("targetdir")
+parser = ArgumentParser()
+parser.add_argument("--datadir")
+parser.add_argument("--snapshotname")
+parser.add_argument("--targetdir")
 args = parser.parse_args()
-
-# Removing target dir
 if path.exists(args.targetdir):
-    rmtree(args.targetdir)
-
-root_target_dir = args.targetdir.split(sep)[-1]
-root_levels = args.datadir.count(sep)
-ignore_list = compile(args.targetdir+sep+'(system|system|systemtauth|system_traces|system_schema|system_distributed)')
-
-try:
-    for root, dirs, files in walk(args.datadir):
-        root_target_dir=args.targetdir+sep+sep.join(root.split(sep)[root_levels+1:-2])
-        if match(ignore_list, root_target_dir):
-            continue
-        if root.split(sep)[-1] == args.snapshotname:
-            copytree(src=root, dst=root_target_dir, ignore=ignore_patterns('.*'))
-except Exception as e:
-    print(e)
+    print("\033[91m Target directory {} exists; exiting without backing up cassandra...".format(args.targetdir))
+    exit(1)
+def copy():
+    '''
+    Copying the data sanpshots to the target directory
+    '''
+    print("copying")
+# Removing target dir, if targer directory exists
+    root_target_dir = args.targetdir.split(sep)[-1]
+    print(root_target_dir)
+    root_levels = args.datadir.count(sep)
+    ignore_list = compile(args.targetdir+sep+'(system|system|systemtauth|system_traces|system_schema|system_distributed)')
+    try:
+        for root, dirs, files in walk(args.datadir):
+            root_target_dir=args.targetdir+sep+sep.join(root.split(sep)[root_levels+1:-2])
+            if match(ignore_list, root_target_dir):
+                continue
+            if root.split(sep)[-1] == args.snapshotname:
+                copytree(src=root, dst=root_target_dir, ignore=ignore_patterns('.*'))
+    except Exception as e:
+        print(e)
+# Creating snapshots
+command = "nodetool snapshot -t {}".format(args.snapshotname)
+rc = system(command)
+if rc == 0:
+    print("Snapshot taken. Copying to target dir")
+    copy()
+    print("Making a tarball")
+    command = "tar -czvf {}.tar.gz {}".format(args.targetdir,args.targetdir)
+    system(command)
