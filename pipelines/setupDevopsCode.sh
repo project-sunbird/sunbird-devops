e0a1c3753b07b68c1d83421c51a0e497c9b02408
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-22 18:00:21 +0530

    Issue #00 chore: setup devops code script updated

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -4,9 +4,8 @@ set -ex
 
 commit_ref=${1:-release-1.7}
 sunbird_devops_dir_name=sunbird-devops
-if [ -d $sunbird_devops_dir_name ];then
-    cd $sunbird_devops_dir_name
-    git clone https://github.com/project-sunbird/sunbird-devops -b $commit_ref
+if [ ! -d $sunbird_devops_dir_name ];then
+    git clone https://github.com/project-sunbird/$sunbird_devops_dir_name -b $commit_ref
     exit 0
 fi
 cd $sunbird_devops_dir_name
