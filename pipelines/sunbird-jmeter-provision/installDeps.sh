41213710afbf18aaf9c754f20cccbf918df1e23d
manoj v <manojv@ilimi.in>
2018-05-28 15:34:25 +0530

    Issue #3766 chore:  jmeter pipeline scripts are updated

--- pipelines/sunbird-jmeter-provision/installDeps.sh
+++ pipelines/sunbird-jmeter-provision/installDeps.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 # Build script
 # set -o errexit
-sudo apk add --no-cache gcc git libffi-dev musl-dev openssl-dev perl py-pip python python-dev sshpass
-apk -v --update --no-cache add jq
-# apk -v --no-cache add ansible=2.3.0.0-r1
-pip install ansible==2.4.1
