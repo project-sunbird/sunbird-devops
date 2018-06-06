4f28a5fab192a90d16f224d17bf2addbd21b556f
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-06 17:56:58 +0530

    Issue #000 fix: typo fix

--- pipelines/player-cdn-upload/clone.sh
+++ pipelines/player-cdn-upload/clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/project-sunbird/sunbird-portal.git -b angualar-migration
+git clone https://github.com/project-sunbird/sunbird-portal.git -b angular-migration
 
