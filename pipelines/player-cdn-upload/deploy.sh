7ad40e9ff2a3bb1fc8fb9a23c3b4664f06ad0a5f
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-06 23:44:31 +0530

    Issue #000 fix: extravars

--- pipelines/player-cdn-upload/deploy.sh
+++ pipelines/player-cdn-upload/deploy.sh
@@ -2,5 +2,5 @@
 home_dir=$(pwd)
 cd sunbird-devops/ansible
 ansible-playbook -i ../../ansible/inventories/dev -c local assets-upload.yml \
-    --vault-password-file /home/ops/vault
+    --vault-password-file /home/ops/vault \
     --extra-vars assets=${home_dir}/sunbird-portal/src/app/dist
