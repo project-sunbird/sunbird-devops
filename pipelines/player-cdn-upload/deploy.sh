9a716b867bda16e5c4d56ff9941324928a85a180
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-06 20:47:13 +0530

    Issue #000 fix: Updated vault pass

--- pipelines/player-cdn-upload/deploy.sh
+++ pipelines/player-cdn-upload/deploy.sh
@@ -2,5 +2,5 @@
 home_dir=$(pwd)
 cd sunbird-devops/ansible
 ansible-playbook -c local assets-upload.yml \
-    --vault-password-file /run/secrets/vault-pass \
+    --vault-password-file /home/ops/vault
     --extra-vars assets=${home_dir}/sunbird-portal/src/app/dist
