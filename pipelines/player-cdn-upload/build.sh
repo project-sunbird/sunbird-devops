03a87094c4a22db8ec3fadb99cadc5e90e54a865
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-08 02:44:42 +0530

    Issue #000 fix: Created container to build assets

--- pipelines/player-cdn-upload/build.sh
+++ pipelines/player-cdn-upload/build.sh
@@ -7,16 +7,6 @@ npm install -g gulp
 cd sunbird-portal/src/app
 gulp download:editors
 cd client
-pwd
-echo ==================
-printenv
-echo ==================
-source ~/.nvm/nvm.sh
-nvm install 8.11
-nvm use 8
-chmod -R 777 /home/ops/.npm
-whoami
-npm cache clean
 npm install
 npm run build-cdn -- --deployUrl $1
 mv ../dist/index.html ../dist/index.ejs
