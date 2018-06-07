43559c1f537d984ca85adac041bfe768227281e1
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-08 00:05:53 +0530

    Issue #000 fix: UPdated sunbird player

--- pipelines/player-cdn-upload/setupPlayer.sh
+++ pipelines/player-cdn-upload/setupPlayer.sh
@@ -5,6 +5,7 @@ set -ex
 commit_ref=${1:-release-1.7}
 player_dir=sunbird-portal
 [ -d $sunbird_devops_dir_name ] || git clone https://github.com/project-sunbird/$sunbird_devops_dir_name -b $commit_ref && exit 0
+[ -d sunbird-player ] || git clone https://github.com/project-sunbird/sunbird-player -b angular-migration && exit 0
 cd $sunbird_devops_dir_name
 git reset --hard
 git clean -fd
