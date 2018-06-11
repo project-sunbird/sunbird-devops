bb19a2614f1d2ec68abb243616944ed15cb6886f
S M Y <smy.altamash@gmail.com>
2018-06-11 12:27:16 +0000

    Issue #SB-3145 test: Added database installed versions

--- deploy/postInstallation.sh
+++ deploy/postInstallation.sh
@@ -34,11 +34,13 @@ check_es() {
     ip=${arr[0]}
 	local outpt=$(nssh $ssh_user@$ip "sudo netstat -ntpl | grep 9200 | awk 'NR==1{print $4}' | cut -d \":\" -f2" )
 	local outpt1=$(curl -XGET -s "$ip:9200/_cluster/health" | jq ".status")
+	local outpt2=$(curl -XGET -s "$ip:9200" | jq ".version.number")
 	if [ "$outpt1" == "\"green\"" ] || [ "$outpt1" == "\"yellow\"" ] && [ "$outpt" -eq 9200 ];then
         	echo "ELASTICSEARCH cluster is healthy"
 	else
 		echo "ELASTICSEARCH cluster is unhealthy"
         fi
+	echo -e "The Version of Elasticsearch we are using is \"$outpt2\"\n"
 }
 
 
@@ -46,13 +48,16 @@ check_postgres() {
     ips $1
     for ip in ${arr[@]}; do
         local outpt2=$(nssh $ssh_user@$ip "ps -ef | grep postgres | wc -l" )
+	local outpt4=$(nssh $ssh_user@$ip "/usr/lib/postgresql/9.5/bin/postgres --version")
         local outpt3=$(nc -z $ip 5432; echo $? ) 
 	if [ "$outpt2" -gt 1 ] && [ "$outpt3" -eq 0 ];then
                 echo "POSTGRES is working in $ip"
         else
                 echo "POSTGRES is Not Working in $ip"
         fi
+     echo  "The Postgres version which is being used is \"$outpt4\""
      done
+     echo -e "\n"
 }
 
 check_cassandra() {
@@ -60,12 +65,14 @@ check_cassandra() {
     for ip in ${arr[@]}; do
     local outpt4=$(nssh $ssh_user@$ip "ps -ef | grep cassandra | wc -l" )
     local outpt5=$(nc -z $ip 9042; echo $? )
+    local outpt6=$(nssh $ssh_user@$ip "cqlsh --version")
 	if [ "$outpt4" -gt 1 ] && [ "$outpt5" -eq 0 ];then
                 echo "CASSANDRA is working in $ip"
         else
                 echo "CASSANDRA is Not Working in $ip"
         fi
-     done
+    done
+    echo -e "The Cassandra version being used is \"$outpt6\" \n"
 }
 
 check_es_indices() {
