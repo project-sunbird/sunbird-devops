5222253b9d5e28f73a80ee190e651bdcc1c70fba
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-11 18:25:39 +0530

    Issue #000 fix: jenkins build is parameterized

--- pipelines/player-cdn-upload/deploy.sh
+++ pipelines/player-cdn-upload/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 home_dir=$(pwd)
+env=${1}
 cd sunbird-devops/ansible
-ansible-playbook -i ../../ansible/inventories/staging -c local assets-upload.yml \
+ansible-playbook -i ../../ansible/inventories/${env} -c local assets-upload.yml \
     --vault-password-file /home/ops/vault \
     --extra-vars assets=${home_dir}/sunbird-portal/src/app/dist -v
