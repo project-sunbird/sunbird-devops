4c2522f8ea72d7758c10fda7b8333a0d6c5e5183
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-13 19:34:32 +0530

    Issue #000 fix: Dialpage cdn updaload

---
+++ pipelines/dialpage-cdn-upload/setupPlayer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+commit_ref=${1:-release-1.7}
+player_dir=sunbird-portal
+if [ ! -d $player_dir ]; then
+    git clone https://github.com/project-sunbird/sunbird-portal -b $commit_ref
+    exit 0
+fi
+cd $player_dir
+git reset --hard
+git clean -fd
+git fetch
+git checkout "$commit_ref"
+git pull -X theirs
