301d2b736f6e883887499b0bc8620cc329249ef9
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-13 20:22:47 +0530

    Issue #000 fix: UPdate

--- pipelines/dialpage-cdn-upload/deploy.sh
+++ pipelines/dialpage-cdn-upload/deploy.sh
@@ -4,5 +4,5 @@ env=${1}
 cd sunbird-devops/ansible
 ansible-playbook -i ../../ansible/inventories/${env} -c local assets-upload.yml \
     --vault-password-file /home/ops/vault \
-    --extra-vars "player_cdn_container=dialpage assets=dist" \
-    --extra-vars assets=${home_dir}/sunbird-portal/src/app/dist -v
+    --extra-vars "player_cdn_container=dialpage \
+    --extra-vars assets=${home_dir}/sunbird-portal/dial-code-page/dist -v
