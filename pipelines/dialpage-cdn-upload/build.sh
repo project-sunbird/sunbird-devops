cad03c96e2b0e2cf34258db4d411f921963786b7
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-13 19:27:27 +0530

    Issue #000 feat: dialpage cdn Upload job

---
+++ pipelines/dialpage-cdn-upload/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Build script
+# set -o errexit
+env=$1
+apk add --no-cache git python make g++
+cd sunbird-portal/dial-code-page
+npm install
+npm run build
+# Created dist folder
+# Replace only staging
+[[ $env == 'staging' ]] && sed -i s#https://dev.open-sunbird.org#https://staging.open-sunbird.org#g dist/script.js
