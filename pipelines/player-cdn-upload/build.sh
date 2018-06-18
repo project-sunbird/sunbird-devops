00424fa856aac5afe7c1a20e95fc6063d2846a63
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-18 17:49:30 +0530

    Issue #000 fix: vars

--- pipelines/player-cdn-upload/build.sh
+++ pipelines/player-cdn-upload/build.sh
@@ -4,12 +4,12 @@
 
 apk add --no-cache git python make g++ jq
 cd sunbird-portal/src/app
+version=$(jq '.version' package.json | sed 's/\"//g')
+build_number=$(jq '.buildNumber' package.json | sed 's/\"//g')
 npm install
 ./node_modules/.bin/gulp download:editors
 cd client
 npm install
 npm run build-cdn -- --deployUrl $1
 # package.json in app folder
-version=$(jq '.version' package.json | sed 's/\"//g')
-build_number=$(jq '.buildNumber' package.json | sed 's/\"//g')
 mv ../dist/index.html ../dist/index_${version}.${build_number}.ejs
