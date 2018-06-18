af0326527a1b35ee03f5bb13f9e8221cbce6e8ff
rjshrjndrn <rjshrjndrn@gmail.com>
2018-06-18 17:24:49 +0530

    Issue #000 fix: variablized index.ejs

--- pipelines/player-cdn-upload/build.sh
+++ pipelines/player-cdn-upload/build.sh
@@ -2,11 +2,14 @@
 # Build script
 # set -o errexit
 
-apk add --no-cache git python make g++
+apk add --no-cache git python make g++ jq
 cd sunbird-portal/src/app
 npm install
 ./node_modules/.bin/gulp download:editors
 cd client
 npm install
 npm run build-cdn -- --deployUrl $1
-mv ../dist/index.html ../dist/index.ejs
+# package.json in app folder
+version=$(jq '.version' package.json | sed 's/\"//g')
+build_number=$(jq '.buildNumber' package.json | sed 's/\"//g')
+mv ../dist/index.html ../dist/index_${version}.${build_number}.ejs
