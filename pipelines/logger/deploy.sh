f137dfb9d29b7aaf52edb3152d67055ea4475e07
smy <30286162+SMYALTAMASH@users.noreply.github.com>
2018-07-06 23:45:16 +0530

    Issue #000 echo: getting env

--- pipelines/logger/deploy.sh
+++ pipelines/logger/deploy.sh
@@ -3,5 +3,5 @@
 # set -o errexit
 
 ansible-playbook --version
-
+echo $ENV
 ansible-playbook -i ansible/inventories/${ENV} sunbird-devops/ansible/logging.yml --vault-password-file  /run/secrets/vault-pass
