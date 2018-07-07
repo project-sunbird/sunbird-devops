8bbffa3adbf536b308e46bf6557809b96a0ad5e9
rjshrjndrn <rjshrjndrn@gmail.com>
2018-07-07 09:34:28 +0530

    Issue #000 feat: Dialpage Upload job removed

--- pipelines/dialpage-cdn-upload/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-home_dir=$(pwd)
-env=${1}
-cd sunbird-devops/ansible
-ansible-playbook -i ../../ansible/inventories/${env} -c local assets-upload.yml \
-    --vault-password-file /home/ops/vault \
-    --extra-vars "player_cdn_container=dialpage" \
-    --extra-vars assets=${home_dir}/sunbird-portal/dial-code-page/dist -v
