6883e491ce35b53e26719b49900d45258d8ca472
harshavardhnc <harshavardhanc95@gmail.com>
2018-07-27 20:46:09 +0530

    Issue #00 fix: added variable for tenant cdn

--- pipelines/tenant-cdn-upload/deploy.sh
+++ pipelines/tenant-cdn-upload/deploy.sh
@@ -4,4 +4,5 @@ env=${1}
 cd sunbird-devops/ansible
 ansible-playbook -i ../../ansible/inventories/${env} -c local assets-upload.yml \
     --vault-password-file /home/ops/vault \
+    --extra-vars "player_cdn_container=tenant" \
     --extra-vars assets=${home_dir}/sunbird-portal/src/app/tenants-build -v
