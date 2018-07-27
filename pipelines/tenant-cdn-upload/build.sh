b6b94239a2fa67efa9ca043e7535d117244ea1bc
harshavardhnc <harshavardhanc95@gmail.com>
2018-07-27 21:25:15 +0530

    Issue #00 fix: added path for tenant repo

--- pipelines/tenant-cdn-upload/build.sh
+++ pipelines/tenant-cdn-upload/build.sh
@@ -3,7 +3,8 @@
 # set -o errexit
 
 apk add --no-cache git python make g++
-
+git clone https://github.com/DIKSHA-NCTE/diksha-tenant sunbird-portal/src/app/
+cp -r diksha-tenant/tenants sunbird-portal/src/app/tenant
 cd sunbird-portal/src/app
 
 npm install gulp -g 
