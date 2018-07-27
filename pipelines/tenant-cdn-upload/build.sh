396763d35ddd4a3270409bba80278f22e4772a6d
harshavardhnc <harshavardhanc95@gmail.com>
2018-07-27 20:51:46 +0530

    Issue #00 fix: cdn variable args

--- pipelines/tenant-cdn-upload/build.sh
+++ pipelines/tenant-cdn-upload/build.sh
@@ -9,7 +9,7 @@ cd sunbird-portal/src/app
 npm install gulp -g 
 
 npm install
-gulp pushTenantsToCDN --gulpfile gulp-tenant.js --cdn-url=$1
+gulp pushTenantsToCDN --gulpfile gulp-tenant.js --cdnurl=$1
 #npm run build-cdn -- --deployUrl $1
 # package.json in app folder
 #mv ../dist/index.html ../dist/index_${version}.${build_number}.ejs
