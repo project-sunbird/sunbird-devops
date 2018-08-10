1f3bab05ad9c8c943a2045b3d95c437e011fadef
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-10 17:01:51 +0530

    Issue #SB-6252 feat: Uploading player assets

--- pipelines/player-all/deploy.sh
+++ pipelines/player-all/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 home_dir=$(pwd)
 env=${1}
+[[ -d app_dist ]] && rm -rf app_dist 
+tar -xvf player-build.tar.gz
+head app_dist/package.json
+ls app_dist/dist/index*
 cd sunbird-devops/ansible
 ansible-playbook -i ../../ansible/inventories/${env} -c local assets-upload.yml \
     --vault-password-file /home/ops/vault \
