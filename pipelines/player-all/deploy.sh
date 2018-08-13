6d093ba4bd744eaa9264062ecf270615bb475152
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-13 16:25:24 +0530

    Issue #6252 feat: renaming index.ejs to unique before uploading

--- pipelines/player-all/deploy.sh
+++ pipelines/player-all/deploy.sh
@@ -3,7 +3,11 @@ home_dir=$(pwd)
 env=${1}
 [[ -d app_dist ]] && rm -rf app_dist 
 tar -xvf player-dist.tar.gz
+buildHash=$(grep buildHash app_dist/package.json | cut -d '"' -f4)
+releaseNumber=$(grep version app_dist/package.json | cut -d '"' -f4)
 head app_dist/package.json
+# Renaming index.ejs to index.release-version.buildHash.ejs
+mv app_dist/dist/index.ejs app_dist/dist/index.${releaseNumber}.${buildHash}.ejs
 # Reading assets directory to upload
 assets_dir=$(readlink -f app_dist/dist)
 echo $assets_dir
