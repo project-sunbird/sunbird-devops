8e562d1554f8ced9b7438cdaa326e7770017831b
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-16 11:07:47 +0530

    Issue #DO-9 feat: pointing branch to new public branch

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-commit_ref=${1:-release-1.9}
+commit_ref=${1:-newenv}
 sunbird_devops_dir_name=sunbird-devops
 if [ ! -d $sunbird_devops_dir_name ];then
     git clone https://github.com/project-sunbird/$sunbird_devops_dir_name -b $commit_ref
