6a542f435b4e426e8dc8dbc908ee460828fda825
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-22 15:51:33 +0530

    Issue #SB-6252 feat: Gzipping of assets

--- pipelines/player-all/build.sh
+++ pipelines/player-all/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Build script
 # set -o errexit
-# set -x
-
-deployUrl=$1
+set -x
 
 apk add --no-cache git python make g++ jq
 cd sunbird-portal/src/app
 version=$(jq '.version' package.json | sed 's/\"//g')
-build_hash=$(jq '.buildHash' ../../../metadata.json | sed 's/\"//g')
+build_hash=$(jq '.commitHash' ../../../metadata.json | sed 's/\"//g')
 npm install
 ./node_modules/.bin/gulp download:editors
 cd client
 npm install
-npm run build-cdn -- --deployUrl ${deployUrl}
-# package.json in app folder
-mv ../dist/index.html ../dist/index_${version}.${build_number}.ejs
+npm run build-cdn -- --deployUrl $1
+cd ..
+# Gzipping of assets
+./node_modules/.bin/gulp gzip:editors client:gzip
+mv dist/index.html dist/index.${version}.${build_hash}.ejs
