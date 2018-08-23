5e3fbc16272cb6cf49592563ed09fd978806b12f
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-24 03:26:44 +0530

    Issue #SB-6252 feat: Building assets

--- pipelines/player-all/build.sh
+++ pipelines/player-all/build.sh
@@ -2,6 +2,8 @@
 # Build script
 # set -o errexit
 
+deployUrl=$1
+
 apk add --no-cache git python make g++ jq
 cd sunbird-portal/src/app
 version=$(jq '.version' package.json | sed 's/\"//g')
@@ -10,6 +12,6 @@ npm install
 ./node_modules/.bin/gulp download:editors
 cd client
 npm install
-npm run build-cdn -- --deployUrl $1
+npm run build-cdn -- --deployUrl ${deployUrl}
 # package.json in app folder
 mv ../dist/index.html ../dist/index_${version}.${build_number}.ejs
