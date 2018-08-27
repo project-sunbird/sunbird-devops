58340921757387b78f536c07c27b9e451722eb8f
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-27 11:29:33 +0530

    Issue #6252 feat: Assets Upload cdn

--- pipelines/player-all/build.sh
+++ pipelines/player-all/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Build script
 # set -o errexit
+# set -x
 
 apk add --no-cache git python make g++ jq
 cd sunbird-portal/src/app
 version=$(jq '.version' package.json | sed 's/\"//g')
-build_number=$(jq '.buildNumber' package.json | sed 's/\"//g')
+build_hash=$(jq '.buildHash' ../../../metadata.json | sed 's/\"//g')
 npm install
 ./node_modules/.bin/gulp download:editors
 cd client
@@ -14,4 +15,4 @@ npm run build-cdn -- --deployUrl $1
 cd ..
 # Gzipping of assets
 ./node_modules/.bin/gulp gzip:editors client:gzip
-mv dist/index.html dist/index_${version}.${build_number}.ejs
+mv dist/index.html dist/index.${version}.${build_hash}.ejs
