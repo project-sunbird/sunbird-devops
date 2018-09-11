727fe94e2941ffe312157c42b8a1ce012163de25
Jai <jaikumarsr@latitudefintech.com>
2018-09-11 14:18:13 +0530

    Issue #SB-6475 feat: Sunbird Initialisation Script - Upgrade

--- deploy/sunbird_upgrade.sh
+++ deploy/sunbird_upgrade.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+implementation_name=$(awk '/implementation_name: /{ if ($2 !~ /#.*/) {print $2}}' config)
+env_name=$(awk '/env: /{ if ($2 !~ /#.*/) {print $2}}' config)
+ansible_variable_path="${implementation_name}"-devops/ansible/inventories/"$env_name"
+ansible-playbook -i $ansible_variable_path/hosts ../ansible/system-init-upgrade.yml --extra-vars @config
+
 export sunbird_cassandra_host=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
 export sunbird_cassandra_port=9042
 
 cd /tmp
 
 #Check cassandra migration jar file present or not inside /tmp
-if [ -f cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar ] 
+if [ -f cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar ]
 then
   rm -rf cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 fi
 
 wget https://github.com/project-sunbird/sunbird-utils/releases/download/R1.5/cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-# Start the java process to apply cassandra migration 
+# Start the java process to apply cassandra migration
 nohup java -cp "cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar" com.contrastsecurity.cassandra.migration.utils.MigrationScriptEntryPoint &
 
