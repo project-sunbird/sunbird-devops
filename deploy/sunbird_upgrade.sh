c5ce6c9bb708507f66fd0b9a820a3723ab80e906
Jai <jaikumarsr@latitudefintech.com>
2018-09-14 11:03:29 +0530

    Issue #SB-6475 feat: Sunbird Initialisation Script - Upgrade

--- deploy/sunbird_upgrade.sh
+++ deploy/sunbird_upgrade.sh
@@ -10,13 +10,13 @@ export sunbird_cassandra_port=9042
 cd /tmp
 
 #Check cassandra migration jar file present or not inside /tmp
-if [ -f cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar ]
+if [ -f cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar ] 
 then
   rm -rf cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 fi
 
 wget https://github.com/project-sunbird/sunbird-utils/releases/download/R1.5/cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-# Start the java process to apply cassandra migration
+# Start the java process to apply cassandra migration 
 nohup java -cp "cassandra-migration-0.0.1-SNAPSHOT-jar-with-dependencies.jar" com.contrastsecurity.cassandra.migration.utils.MigrationScriptEntryPoint &
 
