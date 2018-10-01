ca7d9b8c2e4cf032de8e5998301ec218d7d4d939
S M Y <smy.altamash@gmail.com>
2018-10-01 15:41:39 +0530

    Issue #000 proxy: proxy changes

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-commit_ref=${1:-release-1.10.1}
+commit_ref=${1:-release-1.11}
 sunbird_devops_dir_name=sunbird-devops
 git -C $sunbird_devops_dir_name pull origin $commit_ref || git clone https://github.com/project-sunbird/sunbird-devops $sunbird_devops_dir_name
 # workaround for fatal: unable to auto-detect email address 
