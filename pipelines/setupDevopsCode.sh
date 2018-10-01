6461551c45df94aac8ef4aebc277bd37520ad6fd
rjshrjndrn <rjshrjndrn@gmail.com>
2018-10-01 13:37:19 +0530

    Issue #0000 feat: Unifying tags and branch

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -4,9 +4,6 @@ set -ex
 
 commit_ref=${1:-release-1.11}
 sunbird_devops_dir_name=sunbird-devops
-git -C $sunbird_devops_dir_name fetch --all --tags || git clone https://github.com/project-sunbird/$sunbird_devops_dir_name
+git -C $sunbird_devops_dir_name pull origin $commit_ref || git clone https://github.com/project-sunbird/sunbird-devops $sunbird_devops_dir_name
 cd $sunbird_devops_dir_name
-git reset --hard
-git clean -fd
 git checkout "$commit_ref"
-git symbolic-ref HEAD 2>&1 && git pull -X theirs 
