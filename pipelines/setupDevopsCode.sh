e325ec10153518bc40dd6a80e625ec65a9b11bc4
rjshrjndrn <rjshrjndrn@gmail.com>
2018-10-03 19:34:00 +0530

    Issue #0000 feat: Changed the git config so that it will be default
    for new and old repos
    
    release-1.11

--- pipelines/setupDevopsCode.sh
+++ pipelines/setupDevopsCode.sh
@@ -4,9 +4,9 @@ set -ex
 
 commit_ref=${1:-release-1.11}
 sunbird_devops_dir_name=sunbird-devops
-git -C $sunbird_devops_dir_name pull origin $commit_ref || git clone https://github.com/project-sunbird/sunbird-devops $sunbird_devops_dir_name
 # workaround for fatal: unable to auto-detect email address 
 git config --global user.email "jenkins@open-sunbird.org"
 git config --global user.name "jenkins"
+git -C $sunbird_devops_dir_name pull origin $commit_ref || git clone https://github.com/project-sunbird/sunbird-devops $sunbird_devops_dir_name
 cd $sunbird_devops_dir_name
 git checkout "$commit_ref"
