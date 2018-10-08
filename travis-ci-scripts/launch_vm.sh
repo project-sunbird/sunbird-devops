f67809c75f225250ca20ffca67a1e77c25a3639d
root <msknext@gmail.com>
2018-10-08 10:57:27 +0530

    fix: SB-7885 - Do not delete VM if build fails and build only tags

--- travis-ci-scripts/launch_vm.sh
+++ travis-ci-scripts/launch_vm.sh
@@ -2,17 +2,17 @@
 config_file=$TRAVIS_BUILD_DIR/deploy/config
 #aws ec2 run-instances --launch-template LaunchTemplateName=travis-ci-app
 #aws ec2 run-instances --launch-template LaunchTemplateName=travis-ci-db
-aws ec2 run-instances --image-id ami-0c510557369b14896 --instance-type t2.large --key-name adoption-keshav --subnet-id  subnet-0241076a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=travis-app-$TRAVIS_BUILD_NUMBER}]"
-aws ec2 run-instances --image-id ami-0c510557369b14896 --instance-type t2.large --key-name adoption-keshav --subnet-id  subnet-0241076a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=travis-db-$TRAVIS_BUILD_NUMBER}]"
+aws ec2 run-instances --image-id ami-0c510557369b14896 --instance-type t2.large --key-name adoption-keshav --subnet-id  subnet-0241076a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=travis-app-$TRAVIS_BUILD_NUMBER-$release}]"
+aws ec2 run-instances --image-id ami-0c510557369b14896 --instance-type t2.large --key-name adoption-keshav --subnet-id  subnet-0241076a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=travis-db-$TRAVIS_BUILD_NUMBER-$release}]"
 echo "Wait for app and db instances to launch"
 sleep 150
 
-aws_app_instance=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"  "Name=tag:Name,Values=travis-app-$TRAVIS_BUILD_NUMBER" --query "Reservations[*].Instances[*].{PrivateIP: PrivateIpAddress, PublicIP: PublicIpAddress, InstanceId: InstanceId}")
+aws_app_instance=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"  "Name=tag:Name,Values=travis-app-$TRAVIS_BUILD_NUMBER-$release" --query "Reservations[*].Instances[*].{PrivateIP: PrivateIpAddress, PublicIP: PublicIpAddress, InstanceId: InstanceId}")
 app_instance_id=$(echo $aws_app_instance | awk '{print $1}')
 app_private_ip=$(echo $aws_app_instance | awk '{print $2}')
 app_public_ip=$(echo $aws_app_instance | awk '{print $3}')
 
-aws_db_instance=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"  "Name=tag:Name,Values=travis-db-$TRAVIS_BUILD_NUMBER" --query "Reservations[*].Instances[*].{PrivateIP: PrivateIpAddress, PublicIP: PublicIpAddress, InstanceId: InstanceId}")
+aws_db_instance=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"  "Name=tag:Name,Values=travis-db-$TRAVIS_BUILD_NUMBER-$release" --query "Reservations[*].Instances[*].{PrivateIP: PrivateIpAddress, PublicIP: PublicIpAddress, InstanceId: InstanceId}")
 db_instance_id=$(echo $aws_db_instance | awk '{print $1}')
 db_private_ip=$(echo $aws_db_instance | awk '{print $2}')
 db_public_ip=$(echo $aws_db_instance | awk '{print $3}')
