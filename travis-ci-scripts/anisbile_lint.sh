377f9d49ad398593372fd89332d9cd5f97b57785
root <msknext@gmail.com>
2018-10-10 20:40:52 +0530

    chore: removing all travis releated scripts

--- travis-ci-scripts/anisbile_lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-virtualenv env;
-source env/bin/activate;
-pip -q install ansible;
-pip -q install ansible-lint
-ansible --version
-ansible-playbook  --version
-./test.sh
--- travis-ci-scripts/cli_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip3 -q install awscli --upgrade --user
-aws --version
-aws configure set aws_access_key_id $keyid
-aws configure set aws_secret_access_key $key
-aws configure set region ap-south-1
-aws configure set output text
