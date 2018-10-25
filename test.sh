e801bbc8dc74f9b8cd0fc1e86bce732dd9510112
Keshav Prasad <msknext@gmail.com>
2018-10-25 09:25:20 +0530

    Circle CI Script Updates (#264)
    * feat: SB-7716 - Travis CI Nightly builds for Sunbird installation
    
    * fix: Removing extra travis file
    
    * fix: SB-7885 - Do not delete VM if build fails and build only tags
    
    * fix: SB-7885 - Remove master branch checkout code as we will not build master's tip anymore
    
    * fix: Updating tags, root org json response retrieval fix
    
    * feat: Adding circle ci scripts
    
    * fix: remove extra file
    
    * fix: remove extra file
    
    * style: Remove echos, cleaner output messages, yaml anchors
    
    * chore: removing all travis releated scripts
    
    * fix: temporary run to disable other branch builds
    
    * feat: SB-7983, SB-7984 - Scheduling cron back
    
    * Revert "feat: SB-7983, SB-7984 - Scheduling cron back"
    
    This reverts commit 6164a41dd65742680888f5c951b7bef9e61ecfc0.
    
    * Revert "fix: temporary run to disable other branch builds"
    
    This reverts commit 7651af690e8c78baab64d73e178208ec4b169148.
    
    * feat: SB-7983, SB-7984 - Scheduling cron back
    
    * fix: SB-8102 - Updated code to use new config file, new logic to checkout tags and branch, code refactor"
    
    * fix: SB-8102 - Changed channel name

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-SCRIPT_BASE_DIR=$(dirname $0)
-SUNBIRD_DEVOPS_FOLDER=$SCRIPT_BASE_DIR
-
-bash $SUNBIRD_DEVOPS_FOLDER/ansible-syntax-check.sh $SUNBIRD_DEVOPS_FOLDER/ansible/inventories/sample
--- test/ci/copy_script.sh
+++ test/ci/copy_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-config_file=./deploy/config
-install_script=./circle-ci/install_sb.sh
-release_to_build=./circle-ci/release_to_build
+config_file=./deploy/config.yml.sample
+install_script=./test/ci/install_sb.sh
+release_to_build=./test/ci/release_to_build
 app_ip=$(awk '/dns_name:/{ if ($2 !~ /#.*/) {print $2}}' $config_file)
 user_name=$(awk '/ssh_ansible_user:/{if ($2 !~ /#.*/) {print $2}}' $config_file)
 printf "%s$private_key" > sunbird.pem
 chmod 600 sunbird.pem
-echo $release > $release_to_build
+echo $RELEASE > $release_to_build
 scp -i sunbird.pem -o StrictHostKeyChecking=no $config_file sunbird.pem $install_script $release_to_build $user_name@$app_ip:/tmp
 ssh -i sunbird.pem -o StrictHostKeyChecking=no $user_name@$app_ip "/tmp/install_sb.sh"
 if [[ $? -ne 0 ]]; then
-   echo -e "\nSunbird installation for $release failed - Please inspect the server - Instance not removed!"
+   echo -e "\nSunbird installation for $RELEASE failed - Please inspect the server - Instance not removed!"
    exit 1
 fi
