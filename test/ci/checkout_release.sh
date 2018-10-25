3878e660a2742a2dfd4344d94231750cb42010d6
Keshav Prasad <msknext@gmail.com>
2018-10-25 09:25:20 +0530

    Circle CI Script Updates (#264)
    * feat: SB-7716 - Travis CI Nightly builds for Sunbird installation
    
    * fix: Removing extra travis file
    
    * fix: SB-7885 - Do not delete VM if build fails and build only tags
    
    * fix: SB-7885 - Remove master branch checkout code as we will not build master's tip anymore
    
    * fix: Updating tags, root org json response retrieval fix
    
    * feat: Adding circle ci scripts
    
    * fix: remove extra file
    
    * fix: remove extra file
    
    * style: Remove echos, cleaner output messages, yaml anchors
    
    * chore: removing all travis releated scripts
    
    * fix: temporary run to disable other branch builds
    
    * feat: SB-7983, SB-7984 - Scheduling cron back
    
    * Revert "feat: SB-7983, SB-7984 - Scheduling cron back"
    
    This reverts commit 6164a41dd65742680888f5c951b7bef9e61ecfc0.
    
    * Revert "fix: temporary run to disable other branch builds"
    
    This reverts commit 7651af690e8c78baab64d73e178208ec4b169148.
    
    * feat: SB-7983, SB-7984 - Scheduling cron back
    
    * fix: SB-8102 - Updated code to use new config file, new logic to checkout tags and branch, code refactor"
    
    * fix: SB-8102 - Changed channel name

---
+++ test/ci/checkout_release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cp -r test .circleci ./deploy/config.yml.sample /tmp
+IFS="/" read -r var1 var2 <<< $RELEASE
+if [[ $var1 == "tags" ]]; then
+   git checkout $release -b $var2
+   echo -e "Checkout $var2"
+else
+   git checkout -b $var1 origin/$var1
+   echo -e "Checkout $var1"
+fi
+
+if ! [[ -f ./deploy/config.yml.sample ]]; then
+   cp /tmp/config.yml.sample ./deploy
+fi
+
+cp -r /tmp/test /tmp/.circleci .
