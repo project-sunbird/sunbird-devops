137333b8e38939334382a6c1847b6aa8e7e753b2
msknext <msknext@gmail.com>
2018-11-16 21:59:08 +0530

    feat: adding support for custom emails

--- test/ci/checkout_release.sh
+++ test/ci/checkout_release.sh
@@ -2,10 +2,10 @@
 cp -r test .circleci ./deploy/config.yml.sample /tmp
 IFS="/" read -r var1 var2 <<< $RELEASE
 if [[ $var1 == "tags" ]]; then
-   git checkout $release -b $var2
+   git checkout $release -b $var2 --force
    echo -e "Checkout $var2"
 else
-   git checkout -b $var1 origin/$var1
+   git checkout -b $var1 origin/$var1 --force
    echo -e "Checkout $var1"
 fi
 
@@ -14,3 +14,4 @@ if ! [[ -f ./deploy/config.yml.sample ]]; then
 fi
 
 cp -r /tmp/test /tmp/.circleci .
+
