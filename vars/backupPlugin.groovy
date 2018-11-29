dfe8a2166fc19843fcaa918320fdbdc7ec5863a4
rjshrjndrn <rjshrjndrn@gmail.com>
2018-11-29 06:05:30 +0530

    Issue #000 feat: Creating shared pipeline for backup
    DO-306

---
+++ vars/backupPlugin.groovy
@@ -0,0 +1,30 @@
+// common plugin to clone code and build
+def call(Map pipelineParams) {
+pipeline {
+        agent {
+            node {
+                label "${pipelineParams.agent}"
+            }
+        }
+        stages {
+            // cloning public sunbird-devops
+            stage('checkout git') {
+                steps {
+                    // checkout scm
+                    // This should be replaced with above step
+                    git branch: 'release-1.12', url: 'https://github.com/ekstep/sunbird-devops', credentialsId: 'f37ad21f-744a-4817-9f5e-02f8ec620b39'
+                    dir('sunbird-devops'){
+                    git branch: pipelineParams.branch, url: pipelineParams.scmUrl
+                    }
+                }
+            }
+
+            stage('Backup') {
+                steps {
+                    sh 'ls'
+                    sh "ansible-playbook -i ansible/inventories/${pipelineParams.env} sunbird-devops/ansible/${pipelineParams.playBook} ${pipelineParams.ansibleExtraArgs}"
+                }
+            }
+        }
+    }
+}
