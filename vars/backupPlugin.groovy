4dedd50ea6a88e0c0a361185e9a97fd1ae084848
rjshrjndrn <rjshrjndrn@gmail.com>
2018-11-30 12:40:59 +0530

    Issue #000 feat: removing private repo cloning from shared pipeline
    DO-306

--- vars/backupPlugin.groovy
+++ vars/backupPlugin.groovy
@@ -10,9 +10,6 @@ pipeline {
             // cloning public sunbird-devops
             stage('checkout git') {
                 steps {
-                    // checkout scm
-                    // This should be replaced with above step
-                    git branch: 'release-1.12', url: 'https://github.com/ekstep/sunbird-devops', credentialsId: 'f37ad21f-744a-4817-9f5e-02f8ec620b39'
                     dir('sunbird-devops'){
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                     }
