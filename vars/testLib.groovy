cef9cb0cca4fb794031a0a351bc1f2407c77407c
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-05 11:56:49 +0530

    Issue #000 feat: changed to scripted
    DO-306

--- vars/testLib.groovy
+++ vars/testLib.groovy
@@ -6,29 +6,24 @@ def call(body) {
     body.delegate = pipelineParams
     body()
 
-    pipeline {
-            node {
-                label "${pipelineParams.agent}"
-                env.METADATA_FILE = "${pipelineParams.artifactName}"
-                env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
-                env.ENV = "${pipelineParams.env}"
-                env.SERVICE_NAME = "${pipelineParams.serviceName}"
-                env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
-            }
-        stages {
+    node {
+            label "${pipelineParams.agent}"
+            env.METADATA_FILE = "${pipelineParams.artifactName}"
+            env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
+            env.ENV = "${pipelineParams.env}"
+            env.SERVICE_NAME = "${pipelineParams.serviceName}"
+            env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
+
             // cloning public sunbird-devops and private repo
             stage('checkout git') {
-                steps {
                     checkout scm
                     echo pipelineParams.branch
                     dir('sunbird-devops'){
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                     }
-                }
             }
 
             stage('Deploy') {
-                steps {
                     sh 'ls'
                     script{
                         step ([$class: 'CopyArtifact',
@@ -37,8 +32,6 @@ def call(body) {
                     }
                     sh deployScript
                     archiveArtifacts 'metadata.json'
-                }
             }
         }
     }
-}
