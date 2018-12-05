ac2fd47e2bc9f076bbbceb928fe6c3d20e3cbff0
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-05 12:09:55 +0530

    Issue #000 feat: updated deploy pipeline to scripted
    DO-306

--- vars/deployPlugin.groovy
+++ vars/deployPlugin.groovy
@@ -1,33 +1,29 @@
-// common plugin to clone code and build
-def call(Map pipelineParams) {
-def deployScript = libraryResource 'deploy.sh'
-pipeline {
-        agent {
-            node {
-                label "${pipelineParams.agent}"
-                env.METADATA_FILE = "${pipelineParams.artifactName}"
-                env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
-                env.ENV = "${pipelineParams.env}"
-                env.SERVICE_NAME = "${pipelineParams.serviceName}"
-                env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
-            }
-        }
-        stages {
-            // cloning public sunbird-devops
+def call(body) {
+
+    def deployScript = libraryResource 'deploy.sh'
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    node {
+            label "${pipelineParams.agent}"
+            env.METADATA_FILE = "${pipelineParams.artifactName}"
+            env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
+            env.ENV = "${pipelineParams.env}"
+            env.SERVICE_NAME = "${pipelineParams.serviceName}"
+            env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
+
+            // cloning public sunbird-devops and private repo
             stage('checkout git') {
-                steps {
-                    // checkout scm
-                    // This should be replaced with above step
-                    git branch: 'release-1.12', url: 'https://github.com/ekstep/sunbird-devops', credentialsId: 'f37ad21f-744a-4817-9f5e-02f8ec620b39'
+                    checkout scm
+                    echo pipelineParams.branch
                     dir('sunbird-devops'){
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                     }
-                }
             }
 
             stage('Deploy') {
-                steps {
-                    sh 'ls'
                     script{
                         step ([$class: 'CopyArtifact',
                         projectName: pipelineParams.parentProject,
@@ -35,8 +31,6 @@ pipeline {
                     }
                     sh deployScript
                     archiveArtifacts 'metadata.json'
-                }
             }
         }
     }
-}
