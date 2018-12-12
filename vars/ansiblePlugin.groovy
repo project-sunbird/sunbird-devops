3a1dd471f45f484695fc209ebfedaf333c05dd7c
kaliraja.ramasamy <kaliraja.ramasamy@tarento.com>
2018-12-12 11:56:35 +0530

    Issue #0000 fix: DO-327 added the vault passwd file

--- vars/ansiblePlugin.groovy
+++ vars/ansiblePlugin.groovy
@@ -9,15 +9,15 @@ def call(body) {
        // cloning public sunbird-devops and private repo
        stage('checkout git') {
            checkout scm
-           dir('sunbird-devops'){
+           dir('sunbird-devops-private'){
            git branch: pipelineParams.branch, url: pipelineParams.scmUrl, credentialsId: 'f37ad21f-744a-4817-9f5e-02f8ec620b39'
            }
        }
 
        stage('Push') {
            sh """
-           ansible-playbook -i ansible/inventories/${pipelineParams.env} \
-           ansible/${pipelineParams.playBook} ${pipelineParams.ansibleExtraArgs}
+           ansible-playbook -i $WORKSPACE/sunbird-devops-private/ansible/inventories/${pipelineParams.env} \
+           $WORKSPACE/ansible/${pipelineParams.playBook} ${pipelineParams.ansibleExtraArgs}
            """
        }
    }
