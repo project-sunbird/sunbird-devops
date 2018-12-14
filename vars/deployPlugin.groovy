d50fc958dbc0f6f351248dbddf8b2ef4ff1f4773
Keshav Prasad <msknext@gmail.com>
2018-12-14 14:20:03 +0530

    feat: DO-350 adding checkout credentials

---
+++ vars/deployPlugin.groovy
@@ -0,0 +1,35 @@
+def call(Map pipelineParams) {
+def deployScript = libraryResource 'deploy.sh'
+pipeline {
+        agent {
+            node {
+                label "${pipelineParams.agent}"
+                env.METADATA_FILE = "${pipelineParams.artifactName}"
+                env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
+                env.ENV = "${pipelineParams.env}"
+                env.SERVICE_NAME = "${pipelineParams.serviceName}"
+                env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
+            }
+        }
+        stages {
+           stage('checkout private repo') {
+                    dir('sunbird-devops-private'){
+                    git branch: pipelineParams.branch, url: pipelineParams.scmUrl, credentialsId: pipelineParams.credentials
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    sh 'ls'
+                    script{
+                        step ([$class: 'CopyArtifact',
+                        projectName: pipelineParams.parentProject,
+                        filter: pipelineParams.artifactName]);
+                    }
+                    sh deployScript
+                    archiveArtifacts 'metadata.json'
+                }
+            }
+        }
+    }
+}
