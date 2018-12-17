d4f350a9740320f7d442baba48acc612638c1c97
Keshav Prasad <msknext@gmail.com>
2018-12-17 10:55:11 +0530

    Issue # 000: Debug

--- vars/deployPlugin.groovy
+++ vars/deployPlugin.groovy
@@ -1,27 +1,18 @@
 def call(Map pipelineParams) {
 def deployScript = libraryResource 'deploy.sh'
-pipeline {
-        agent {
-            node {
-                label "${pipelineParams.agent}"
-                env.METADATA_FILE = "${pipelineParams.artifactName}"
-                env.ARTIFACT_LABEL = "${pipelineParams.artifactLabel}"
-                env.ENV = "${pipelineParams.env}"
-                env.SERVICE_NAME = "${pipelineParams.serviceName}"
-                env.DEPLOY_EXTRA_ARGS = "${pipelineParams.deployExtraArgs}"
+        
+ node(pipelineParams.agent){             
+                env.METADATA_FILE = pipelineParams.artifactName
+                env.ARTIFACT_LABEL = pipelineParams.artifactLabel
+                env.ENV = pipelineParams.env
+                env.SERVICE_NAME = pipelineParams.serviceName
+                env.DEPLOY_EXTRA_ARGS = pipelineParams.deployExtraArgs
+        
+         stage('checkout private repo') {
+            dir('sunbird-devops-private'){
+            git branch: pipelineParams.branch, url: pipelineParams.scmUrl, credentialsId: pipelineParams.credentials
             }
         }
-        stages {
-           stage('checkout') {
-                    dir('sunbird-devops-private'){
-                    sh 'pwd && ls -lrth'
-                    println pipelineParams.branch
-                    println        pipelineParams.scmUrl
-                     println       pipelineParams.credentials
-                    git branch: pipelineParams.branch, url: pipelineParams.scmUrl, credentialsId: pipelineParams.credentials
-                }
-            }
-
             stage('Deploy') {
                 steps {
                     sh 'ls'
@@ -35,6 +26,5 @@ pipeline {
                       println pipelineParams
                 }
             }
-        }
-    }
+       }
 }
