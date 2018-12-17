9b8cde7b8e72c9edc05328cc85b603c18fa303b3
Keshav Prasad <msknext@gmail.com>
2018-12-17 10:18:45 +0530

    feat: DO-350 Adding deploy script

---
+++ resources/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Build script
+set -o errexit
+set -x
+# installing prerequisites
+[[ $(which ansible) ]] || apk -v --update --no-cache add ansible
+[[ $(which jq) ]] || apk -v --update --no-cache add jq
+
+e () {
+    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
+}
+
+m=$(cat $METADATA_FILE)
+
+org=$(e "${m}" "org")
+name=$(e "${m}" "name")
+version=$(e "${m}" "version")
+
+artifactLabel=${ARTIFACT_LABEL:-bronze}
+env=${ENV:-null}
+
+echo "artifactLabel:  ${artifactLabel}"
+echo "env:            ${env}"
+echo "org:            ${org}"
+echo "name:           ${name}"
+echo "version:        ${version}"
+echo "ANSIBLE_PATH:   $ANSIBLE_PATH"
+
+ansible-playbook --version
+ansible-playbook -i ansible/inventories/$ENV sunbird-devops/ansible/deploy.yml --tags "stack-sunbird" --extra-vars "hub_org=${org} image_name=${name} image_tag=${version}-${artifactLabel} service_name=${SERVICE_NAME} ${DEPLOY_EXTRA_ARGS}" --vault-password-file /run/secrets/vault-pass
