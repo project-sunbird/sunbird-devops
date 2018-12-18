4421dce4a78b7ffd2ba6c2b2bdf6c49f69a58dba
Keshav Prasad <msknext@gmail.com>
2018-12-18 10:37:29 +0530

    Issue # 000: DO-350 temp commit

--- vars/deployPlugin.groovy
+++ vars/deployPlugin.groovy
@@ -6,27 +6,12 @@ def call(Map pipelineParams) {
             }
         }
             stage('Deploy') {
-                org = sh(returnStdout: true, script: 'jq -r .org metadata.json').trim()
-                name = sh(returnStdout: true, script: 'jq -r .name metadata.json').trim()
-                version= sh(returnStdout: true, script: 'jq -r .version metadata.json').trim()
-                artifactLabel = sh(returnStdout: true, script: '${pipelineParams.artifactLabel:-bronze}').trim()
-                environ = sh(returnStdout: true, script: '${pipelineParams.env:-null}').trim()
-
-                println "artifactLabel:  $artifactLabel"
-                println "env:            $environ"
-                println "org:            $org"
-                println "name:           $name"
-                println "version:        $version"
-                println "ANSIBLE_PATH:   $ANSIBLE_PATH"
-
-                println pipelineParams    
 //                sh """ 
-//                ansible-playbook -i $WORKSPACE/ansible/inventories/$environ \ 
+//                ansible-playbook -i $WORKSPACE/ansible/inventories/$pipelineParams.env \ 
 //                $WORKSPACE/sunbird-devops/ansible/$pipelineParams.ansiblePlaybook \
-//                --tags "stack-sunbird" --extra-vars "hub_org=$org image_name=$name image_tag=$version-$artifactLabel \
-//                service_name=$pipelineParams.serviceName $pipelineParams.deployExtraArgs" \
 //                --vault-password-file $pipelineParams.vaultFile
 //                """
+             println pipelineParams
             }
        }
 }
