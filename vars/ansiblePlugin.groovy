5cf0d6ce3c9069ea8adc214004298bd0bbf8dcb7
Keshav Prasad <msknext@gmail.com>
2018-12-18 15:03:09 +0530

    Issue # 000: DO-350 Adding git branch and creds

--- vars/ansiblePlugin.groovy
+++ vars/ansiblePlugin.groovy
@@ -4,8 +4,12 @@ def call(Map pipelineParams) {
     node(pipelineParams.agent){
         // cloning public sunbird-devops and private repo
         stage('checkout private repo') {
+            if (params.private_repo_branch != "")
+                values.put('privateBranch', params.private_repo_branch)
+            else
+                values.put('privateBranch', params.private_repo_branch)
             dir('sunbird-devops-private'){
-            git branch: pipelineParams.branch, url: pipelineParams.scmUrl, credentialsId: pipelineParams.credentials
+            git branch: pipelineParams.privateBranch, url: pipelineParams.scmUrl, credentialsId: private_repo_credentials
             }
         }
 
