e2ac4cdfd59b40950300cd9fd64d0d2cde9e7b01
Keshav Prasad <msknext@gmail.com>
2018-12-19 11:06:54 +0530

    feat: DO-350 variable exception handling

--- vars/ansiblePlugin.groovy
+++ vars/ansiblePlugin.groovy
@@ -5,14 +5,14 @@ def call(Map pipelineParams) {
             // cloning public sunbird-devops and private repo
             stage('checkout private repo') {
 
-                if (private_repo_branch == "" && params.private_repo_branch == "")
+                if (!env.private_repo_branch && (params.private_repo_branch == "" || params.private_repo_branch == null))
                     error 'Please specify private repo branch to checkout as a parameter to job or set it as a Jenkins environment variable'
 
-                if (params.private_repo_branch != "")
+                if (params.private_repo_branch != "" && params.private_repo_branch != null)
                     pipelineParams.put('privateBranch', params.private_repo_branch)
-                else {
+                else if (env.private_repo_branch) {
                     pipelineParams.put('privateBranch', private_repo_branch)
-                    println "Branch not specified as a parameter, checking out branch specified as environment variable"
+                    println "Branch not specified as a parameter, checking out branch specified as environment variable - $private_repo_branch"
                 }
                 dir('sunbird-devops-private') {
                     git branch: pipelineParams.privateBranch, url: pipelineParams.scmUrl, credentialsId: private_repo_credentials
