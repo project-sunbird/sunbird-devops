25ed46fbecc25ed0bdeae959e8b1536761d4e16a
Keshav Prasad <msknext@gmail.com>
2018-12-21 16:09:54 +0530

    feat: DO-350 Un-comment deploy step to test

--- vars/ansiblePlugin.groovy
+++ vars/ansiblePlugin.groovy
@@ -23,10 +23,10 @@ def call(Map pipelineParams) {
 
             stage('ansible-playbook') {
                 println pipelineParams
-//            sh """
-//            ansible-playbook -i $WORKSPACE/sunbird-devops-private/ansible/inventories/$pipelineParams.env \
-//            $WORKSPACE/ansible/$pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
-//            """
+            sh """
+            ansible-playbook -i $WORKSPACE/sunbird-devops-private/ansible/inventories/$pipelineParams.env \
+            $WORKSPACE/ansible/$pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
+            """
             }
         }
         catch (err){
