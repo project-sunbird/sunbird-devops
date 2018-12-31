9d785da1d7a573b0347093dff022adf14982c138
Keshav Prasad <msknext@gmail.com>
2018-12-31 13:12:46 +0530

    feat: DO-431 Changed to variables and populate params

--- vars/docker_service_variables.groovy
+++ vars/docker_service_variables.groovy
@@ -1,6 +1,48 @@
 def call(){
 
     try {
+        if (params.size() == 0){
+            jobname = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim()
+            properties([[$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+            parameters([[$class: 'WHideParameterDefinition', defaultValue: "$jobname", description: '', 
+            name: 'docker_service_name'], 
+            string(defaultValue: '', description: '', name: 'docker_service_version', trim: false), 
+            string(defaultValue: '', description: '', name: 'copy_metadata_from', trim: false), 
+            [$class: 'WHideParameterDefinition', defaultValue: 'sunbirded.azurecr.io/sunbirded', description: '', 
+            name: 'hub_org'], 
+            [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: '', 
+            filterLength: 1, filterable: false, name: 'inventory_source', randomName: 'choice-parameter-330141505859086', 
+            referencedParameters: '', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, 
+            script: ''], script: [classpath: [], sandbox: false, script: 'return [\'GitHub\', \'Local\']']]], 
+            [$class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', description: '', 
+            name: 'git_info', omitValueField: false, randomName: 'choice-parameter-330141508543294', 
+            referencedParameters: 'inventory_source', 
+           
+            script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 
+            '''def gitUrl = "", gitBranch = ""
+            if(inventory_source.equals(\'GitHub\')){
+            return "<b>Git URL</b><input name=\\"value\\" value=\\"${gitUrl}\\" class=\\"setting-input\\" type=\\"text\\"/> <b>Branch</b><input name=\\"value\\" value=\\"${gitBranch}\\" class=\\"setting-input\\" type=\\"text\\"/>"}
+            else
+            return "<b>Not Applicable</b>"'''], 
+                 
+            script: [classpath: [], sandbox: false, script: 
+            '''def gitUrl = "${private_repo_url}", gitBranch = "${private_repo_branch}"
+            if(inventory_source.equals(\'GitHub\')){
+            return "<b>Git URL</b><input name=\\"value\\" value=\\"${gitUrl}\\" class=\\"setting-input\\" type=\\"text\\"/> <b>Branch</b><input name=\\"value\\" value=\\"${gitBranch}\\"  class=\\"setting-input\\" type=\\"text\\"/>"}
+            else
+            return "<b>Not Applicable</b>"''']]], 
+            
+            string(defaultValue: '', description: '', name: 'inventory_path', trim: false)])])
+
+            ansiColor('xterm') {
+              println '''First run of the job. Parameters created. Stopping the current build. 
+                         Please trigger new build and provide parameters if required.'''
+            }
+            currentBuild.result = 'SUCCESS'
+            return
+        }
+        
+        
         // Check if the job was triggered by an upstream project
         // If yes, get the name of the upstream project else job was started manually
         stage('check upstream') {
@@ -9,73 +51,36 @@ def call(){
             triggerCause = upstream?.shortDescription
             if (triggerCause != null)
                 triggerCause = triggerCause.split()[4].replaceAll('"', '')
-            values.put('parent_project', triggerCause)
+            values.put('copy_metadata_from', triggerCause)
         }
         
         stage('parameter checks'){
-            // Set Jenkins environment variable hub_org to registry_hub/sunbirded
-            // If registry used is docker hub, registry_hub will be blank
-            // If using azure hub, set registry_hub to azure repo path
-            if (!env.hub_org)
-                error 'Please set a Jenkins environment variable named hub_org and value as registry_hub/sunbirded'
+            if (values.copy_metadata_from == null && params.copy_metadata_from == "")
+                error 'Please specify project name to copy metedata.json file.'
 
-            if (values.parent_project == null && (params.parent_project == "" || params.parent_project == null))
-                error 'Please specify project name to copy metedata.json file as a job parameter'
-
-            if (values.parent_project != null)
-                copyArtifacts filter: 'metadata.json', projectName: values.parent_project
+            if (values.copy_metadata_from != null)
+                copyArtifacts filter: 'metadata.json', projectName: values.copy_metadata_from
             else
-                copyArtifacts filter: 'metadata.json', projectName: params.parent_project
+                copyArtifacts filter: 'metadata.json', projectName: params.copy_metadata_from
             
-            if (params.image_name == "") {
-               println "Image name not specified, using the image name specified in metadata.json"
-               image_name = sh(returnStdout: true, script: 'jq -r .image_name metadata.json').trim()
-               println image_name
+            if (params.docker_service_name != jobname)
+                error 'Job name and hidden job parameter docker_service_name default value do not match.'
+                
+            if (params.docker_service_version == "") {
+               println """Version for docker service - $params.docker_service_version not specified, 
+                        using the version specified in metadata.json"""
+               docker_service_version = sh(returnStdout: true, script: 'jq -r .image_tag metadata.json').trim()
             }
             else
-               image_name = params.image_name
+               docker_service_version = params.docker_service_version
             
-            if (params.image_tag == "") {
-               println "Image tag not specified, using the version specified in metadata.json"
-               image_tag = sh(returnStdout: true, script: 'jq -r .image_tag metadata.json').trim()
-            }
-            else
-               image_tag = params.image_tag
-            
-            agent = sh(returnStdout: true, script: 'jq -r .nodeName metadata.json').trim()
-            values.put('image_name', image_name)
-            values.put('image_tag', image_tag)
-            values.put('hub_org', hub_org)
-            values.put('agent', agent)
-        }
-
-        stage('read properties') {
-            // Get the parent dir of where the job resides. This will be used to obtain the inventory file
-            envDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-3].trim()
-            env.jobname = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].split('_')[0].trim()
+            envDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-2].trim()
+            agent = sh(returnStdout: true, script: 'jq -r .node_name metadata.json').trim()
             values.put('env', envDir)
-
-            def file = new File("$WORKSPACE/properties.json")
-            if(!file.exists())
-                error 'properties.json file is missing in projects root directoy'
-
-            // Parse the json property file and append the values to the map. This will make use of the job name
-            // Job name should match the name provided in the json file
-            // Example - player_build, player_deploy
-            // Common for all services
-            scmUrl = sh(returnStdout: true, script: 'jq -r .common.scmUrl properties.json').trim()
-            vaultFile = sh(returnStdout: true, script: 'jq -r .common.vaultFile properties.json').trim()
-
-            // Job specific
-            ansiblePlaybook = sh(returnStdout: true, script: 'jq -r .$jobname.playbook properties.json').trim()
-            serviceName = sh(returnStdout: true, script: 'jq -r .$jobname.serviceName properties.json').trim()
-            deployExtraArgs = sh(returnStdout: true, script: 'jq -r .$jobname.deployExtraArgs properties.json').trim()
-
-            values.put('scmUrl', scmUrl)
-            values.put('vaultFile', vaultFile)
-            values.put('ansiblePlaybook', ansiblePlaybook)
-            values.put('serviceName', serviceName)
-            values.put('deployExtraArgs', deployExtraArgs)
+            values.put('agent', agent)
+            values.put('docker_service_name', params.docker_service_name)
+            values.put('docker_service_version', params.docker_service_version)
+            values.put('hub_org', params.hub_org)
             return values
         }
     }
