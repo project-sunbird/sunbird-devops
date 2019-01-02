b40bb7730c43b9056573b532429f7a3ae73c088c
Keshav Prasad <msknext@gmail.com>
2019-01-02 11:00:34 +0530

    feat: DO-431 Check ws allocation

--- vars/ansible_playbook_run.groovy
+++ vars/ansible_playbook_run.groovy
@@ -1,48 +1,49 @@
 // common plugin to general ansible tasks
 def call(Map pipelineParams) {
-//    node(pipelineParams.agent){
-        try {
-            // cloning public sunbird-devops and private repo
-            stage('checkout private repo') {
-                
+    node(pipelineParams.agent){
+    try {
+        // cloning public sunbird-devops and private repo
+        stage('checkout private repo') {
+            ws(pipelineParams.currentWs){
                 if(params.inventory_path == "")
                     error """\
-                           Please specify the absolute path to the inventory file directory.
-                           If option selected is GitHub, please specifiy inventory path as 
-                           $WORKSPACE/private/path_to_inventory
-                           """.stripIndent().replace("\n"," ")
-                    
+                                   Please specify the absolute path to the inventory file directory.
+                                   If option selected is GitHub, please specifiy inventory path as 
+                                   $WORKSPACE/private/path_to_inventory
+                                   """.stripIndent().replace("\n"," ")
+
                 if(params.inventory_source == 'GitHub'){
                     paramsSize = params.git_info.split(',').size()
                     if(paramsSize != 2)
-                      error '''\
-                              GitHub option selected during build. Please specify the GitHub URL and Branch to checkout
-                              You can also set these two values as environment variables with variable name as
-                              private_repo_url and private_repo_branch
-                              '''.stripIndent().replace("\n"," ")
+                        error '''\
+                                  GitHub option selected during build. Please specify the GitHub URL and Branch to checkout
+                                  You can also set these two values as environment variables with variable name as
+                                  private_repo_url and private_repo_branch
+                                  '''.stripIndent().replace("\n"," ")
                 }
-                    if(!env.private_repo_credentials)
-                       error '''\
+                if(!env.private_repo_credentials)
+                    error '''\
                                Please create a Jenkins environment variable named private_repo_credentials with
                                value being the credential id.
                                '''.stripIndent().replace("\n"," ")
-                    scmUrl = params.git_info.split(',')[0]
-                    scmBranch = params.git_info.split(',')[1]
+                scmUrl = params.git_info.split(',')[0]
+                scmBranch = params.git_info.split(',')[1]
                 dir('private') {
                     git branch: scmBranch, url: scmUrl, credentialsId: private_repo_credentials
                 }
             }
+        }
 
-            stage('ansible-playbook') {
-               println pipelineParams
+        stage('ansible-playbook') {
+            println pipelineParams
 //               sh """
 //               ansible-playbook -i $WORKSPACE/sunbird-devops-private/ansible/inventories/$pipelineParams.env \
 //               $WORKSPACE/ansible/$pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
 //               """
-            }
         }
-        catch (err){
-            throw err
-        }
-//    }
+    }
+    catch (err){
+        throw err
+    }
+    }
 }
