e31df8a9700ae4802d619583d984ce5baa0b6b14
msknext <msknext@gmail.com>
2019-01-29 14:00:58 +0530

    feat: DO-498 Restructure jenkinsfiles

--- pipelines/es-backup/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-set -e
-ansible-playbook --version
-ANSIBLE_FORCE_COLOR=true ansible-playbook -i ansible/inventory/$ENV ansible/es.yml --tags "es_backup" -v --vault-password-file /run/secrets/vault-pass
