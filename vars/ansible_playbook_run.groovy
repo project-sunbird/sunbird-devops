e7494bda920f7f08195a4f854a9ff7ed53751c9c
Keshav Prasad <msknext@gmail.com>
2019-01-29 13:24:01 +0530

    feat: DO-498 Removed local inventory option

--- vars/ansible_playbook_run.groovy
+++ vars/ansible_playbook_run.groovy
@@ -8,50 +8,40 @@ def call(Map pipelineParams) {
                 String ANSI_BOLD = "\u001B[1m"
                 String ANSI_RED = "\u001B[31m"
                 String ANSI_YELLOW = "\u001B[33m"
-                // cloning public sunbird-devops and private repo
+
+                // cloning private repo
                 stage('checkout private repo') {
                     dir("$pipelineParams.currentWs/private") {
-                        if (params.inventory_source == "GitHub") {
                             if (!env.private_repo_url || !env.private_repo_branch || !env.private_repo_credentials) {
                                 println(ANSI_BOLD + ANSI_RED + '''\
-                               Uh Oh! Option seleted is GitHub. Please create Jenkins environment variables named
+                               Uh Oh! Please create Jenkins environment variables named
                                private_repo_url, private_repo_branch, private_repo_credentials
                                '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
                                 error 'Please resolve errors and rerun..'
                             }
                             def checkDir = new File('../private')
                             if(!checkDir.exists())
-                               git branch: private_repo_branch, url: private_repo_url, credentialsId: private_repo_credentials
+                                git branch: private_repo_branch, url: private_repo_url, credentialsId: private_repo_credentials
                             else
-                               println(ANSI_BOLD + ANSI_YELLOW + 'Github repo already exists. Not cloning again' + ANSI_NORMAL)
-                        } else
-                            println(ANSI_BOLD + ANSI_YELLOW + '''\
-                            Option selected is Local. Using the local inventory specified in inventory_path
-                            '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
+                                println(ANSI_BOLD + ANSI_YELLOW + 'Github repo already exists. Not cloning again' + ANSI_NORMAL)
 
-                        if (params.inventory_path == "") {
-                            println(ANSI_BOLD + ANSI_RED + """\
-                                   Uh Oh! Please specify the absolute path to the inventory file directory.
-                                   If inventory is GitHub, please specifiy inventory path as 
-                                   $WORKSPACE/private/path_to_inventory
-                                   """.stripIndent().replace("\n", " ") + ANSI_NORMAL)
-                        }
                     }
                 }
 
                 stage('ansible-playbook') {
                     println pipelineParams
                     ansiColor('xterm') {
+                        inventory_path = "${WORKSPACE}/private/ansible/${pipelineParams.env}/${pipelineParams.module}"
                         sh """
-               ansible-playbook -i $params.inventory_path \
-               $pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
-               """
+                               ansible-playbook -i ${inventory_path} \
+                               $pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
+                           """
                     }
                 }
             }
         }
-            catch (err){
-                throw err
-            }
+        catch (err){
+            throw err
         }
     }
+}
