830f7c69b2246761101fed09195cc154f92e3512
msknext <msknext@gmail.com>
2019-02-09 09:39:29 +0000

    Issue #000: Updated config and setup files

--- deploy/jenkins/jenkins-config-setup.sh
+++ deploy/jenkins/jenkins-config-setup.sh
@@ -12,8 +12,8 @@ rsync -r jobs /var/lib/jenkins/
 chown -R jenkins:jenkins /var/lib/jenkins/jobs
 
 echo -e "\n\e[0;32m${bold}Downloading and copying m2 directory to Jenkins ${normal}"
-wget https://sunbirdpublic.blob.core.windows.net/installation/m2.zip
-unzip m2.zip
+wget https://sunbirdpublic.blob.core.windows.net/installation/m2_new.zip
+unzip m2_new.zip
 mv .m2 /var/lib/jenkins
 chown -R jenkins:jenkins /var/lib/jenkins/.m2
 
