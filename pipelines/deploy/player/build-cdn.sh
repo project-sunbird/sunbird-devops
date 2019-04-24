e6f73c5bc73f661900fc3308660ad4b583324854
Rajesh Rajendran <rjshrjndrn@users.noreply.github.com>
2019-04-24 08:00:36 +0530

    changing CDN Build image from alpine to slim (#427)
    * Issue #000 feat: changing apk to apt
    
    * Issue #000 feat: changing alpine to slim for portal cdn build
    
    * Issue #000 feat: disabling jessie updates
    
    * Issue #000 feat: auto accepting player builds

--- pipelines/deploy/player/build-cdn.sh
+++ pipelines/deploy/player/build-cdn.sh
@@ -3,7 +3,8 @@
 # set -o errexit
 set -x
 
-apk add --no-cache git python make g++ jq zip
+sed -i '/jessie-updates/d' /etc/apt/sources.list
+apt update && apt install git python make g++ jq zip -y
 su jenkins
 cd src/app
 version=$(jq '.version' package.json | sed 's/\"//g')
