861b7957eb6623e378d3312f85ee92b61d3b03f2
Keshav Prasad <msknext@gmail.com>
2019-05-03 20:16:08 +0530

    refactor: removed ansi color block

--- vars/docker_params.groovy
+++ vars/docker_params.groovy
@@ -10,48 +10,44 @@ def call(){
         module = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-2].trim()
         jobName = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim()
 
-        stage('parameter checks'){
-            ansiColor('xterm') {
-                if(!env.hub_org){
-                    println (ANSI_BOLD + ANSI_RED + '''\
+        stage('parameter checks') {
+            if (!env.hub_org) {
+                println(ANSI_BOLD + ANSI_RED + '''\
                     Uh oh! Please set a Jenkins environment variable named hub_org with value as registery/sunbidrded
                     '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
-                    error 'Please resolve errors and rerun..'
-                }
-                else
-                    println (ANSI_BOLD + ANSI_GREEN + '''\
+                error 'Please resolve errors and rerun..'
+            } else
+                println(ANSI_BOLD + ANSI_GREEN + '''\
                     Found environment variable named hub_org with value as:
                     '''.stripIndent().replace("\n", " ") + hub_org + ANSI_NORMAL)
 
-                if (params.absolute_job_path == ""){
-                    println (ANSI_BOLD + ANSI_RED + '''\
+            if (params.absolute_job_path == "") {
+                println(ANSI_BOLD + ANSI_RED + '''\
                     Uh oh! Please specify the full path of the job from where the metedata.json file should be copied
                     '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
-                    error 'Please resolve errors and rerun..'
-                }
-                values = [:]
-                copyArtifacts projectName: params.absolute_job_path, flatten: true
-                image_name = sh(returnStdout: true, script: 'jq -r .image_name metadata.json').trim()
-                agent = sh(returnStdout: true, script: 'jq -r .node_name metadata.json').trim()
+                error 'Please resolve errors and rerun..'
+            }
+            values = [:]
+            copyArtifacts projectName: params.absolute_job_path, flatten: true
+            image_name = sh(returnStdout: true, script: 'jq -r .image_name metadata.json').trim()
+            agent = sh(returnStdout: true, script: 'jq -r .node_name metadata.json').trim()
 
-                if (params.image_tag == "") {
-                    println (ANSI_BOLD + ANSI_YELLOW + '''\
+            if (params.image_tag == "") {
+                println(ANSI_BOLD + ANSI_YELLOW + '''\
                     image_tag not specified, using the image_tag specified in metadata.json.
                     '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
-                    image_tag = sh(returnStdout: true, script: 'jq -r .image_tag metadata.json').trim()
-                }
-                else
-                    image_tag = params.image_tag
-                
-                values.put('env', envDir)
-                values.put('module', module)
-                values.put('jobName', jobName)
-                values.put('absolute_job_path', params.absolute_job_path)
-                values.put('agent', agent)
-                values.put('image_name', image_name)
-                values.put('image_tag', image_tag)
-                return values
-            }
+                image_tag = sh(returnStdout: true, script: 'jq -r .image_tag metadata.json').trim()
+            } else
+                image_tag = params.image_tag
+
+            values.put('env', envDir)
+            values.put('module', module)
+            values.put('jobName', jobName)
+            values.put('absolute_job_path', params.absolute_job_path)
+            values.put('agent', agent)
+            values.put('image_name', image_name)
+            values.put('image_tag', image_tag)
+            return values
         }
     }
     catch (err){
