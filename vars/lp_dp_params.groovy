9f98fc8e311d4d52dca1bec95bf9cc77cfdc4e83
Keshav Prasad <msknext@gmail.com>
2019-05-03 19:58:19 +0530

    fix: removed upstream checks

--- vars/lp_dp_params.groovy
+++ vars/lp_dp_params.groovy
@@ -9,21 +9,10 @@ def call(){
         envDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-3].trim()
         module = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-2].trim()
         jobName = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim()
-
-        // Check if the job was triggered by an upstream project
-        // If yes, get the name of the upstream project else job was started manually
-        stage('check upstream') {
-            values = [:]
-            def upstream = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
-            triggerCause = upstream?.shortDescription
-            if (triggerCause != null)
-                triggerCause = triggerCause.split()[4].replaceAll('"', '')
-            values.put('absolute_job_path', triggerCause)
-        }
-
+        
         stage('parameter checks') {
             ansiColor('xterm') {
-                if (values.absolute_job_path == null && params.absolute_job_path == ""){
+                if (params.absolute_job_path == ""){
                     println (ANSI_BOLD + ANSI_RED + '''\
                     Uh oh! Please specify the full path of the job from where the metedata.json file should be copied
                     '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
@@ -39,15 +28,10 @@ def call(){
                 else
                     buildNumber = params.build_number
 
-                if (values.absolute_job_path != null)
-                    copyArtifacts projectName: values.absolute_job_path, fingerprintArtifacts: true, flatten: true, selector: specific(buildNumber)
-                else {
-                    copyArtifacts projectName: params.absolute_job_path, fingerprintArtifacts: true, flatten: true, selector: specific(buildNumber)
-                    values.put('absolute_job_path', params.absolute_job_path)
-                }
-
+                copyArtifacts projectName: params.absolute_job_path, fingerprintArtifacts: true, flatten: true, selector: specific(buildNumber)
                 artifact_name = sh(returnStdout: true, script: 'jq -r .artifact_name metadata.json').trim()
-
+                agent = sh(returnStdout: true, script: 'jq -r .node_name metadata.json').trim()
+                
                 if (params.artifact_version == "" || params.artifact_version == null) {
                     println (ANSI_BOLD + ANSI_YELLOW + '''\
                     artifact_version not specified, using the artifact_version specified in metadata.json.
@@ -57,10 +41,10 @@ def call(){
                 else
                     artifact_version = params.artifact_version
 
-                agent = sh(returnStdout: true, script: 'jq -r .node_name metadata.json').trim()
                 values.put('env', envDir)
                 values.put('module', module)
                 values.put('jobName', jobName)
+                values.put('absolute_job_path', params.absolute_job_path)
                 values.put('agent', agent)
                 values.put('artifact_name', artifact_name)
                 values.put('artifact_version', artifact_version)
