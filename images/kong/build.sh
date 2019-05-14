efbe01fb9dd72d9448337a4f71edcd1a1067e8ca
rjshrjndrn <rjshrjndrn@gmail.com>
2019-05-14 17:13:09 +0530

    Issue #000 feat: updating metadata and DF path for kong

--- images/kong/build.sh
+++ images/kong/build.sh
@@ -5,5 +5,5 @@ build_tag=$1
 node=$2
 hub_org=$3
 name=kong
-docker build -f ./images/kong/Dockerfile -t ${hub_org}/${name}:${build_tag} .
+docker build -f Dockerfile -t ${hub_org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
