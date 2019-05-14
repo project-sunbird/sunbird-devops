a2251683be9d13179b14af48b2f69318cf95e7b7
rjshrjndrn <rjshrjndrn@gmail.com>
2019-05-14 16:59:04 +0530

    Issue #000 chore: Updating build.sh

--- images/kong/build.sh
+++ images/kong/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Build script
 # set -o errexit
+build_tag=$1
+node=$2
+hub_org=$3
 name=kong
 docker build -f ./images/kong/Dockerfile -t ${hub_org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
