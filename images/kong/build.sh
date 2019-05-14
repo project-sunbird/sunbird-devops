bd2743ef710b88e272b28c6c06cca3fb6edd6b9e
rjshrjndrn <rjshrjndrn@gmail.com>
2019-05-14 16:39:50 +0530

    Issue #000 feat: Updating kong build

--- images/kong/build.sh
+++ images/kong/build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 # Build script
 # set -o errexit
-set -e
-e () {
-    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
-}
-m=$(./images/kong/metadata.sh)
-
-org=$(e "${m}" "org")
-name=$(e "${m}" "name")
-version=$(e "${m}" "version")
-
-docker build -f ./images/kong/Dockerfile -t ${org}/${name}:${version} .
+name=kong
+docker build -f ./images/kong/Dockerfile -t ${hub_org}/${name}:${build_tag} .
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
