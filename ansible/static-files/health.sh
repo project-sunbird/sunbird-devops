733d10ee2730877d82594084ed2ce9f06db02e99
G33tha <geetha.p@optit.co>
2019-07-15 14:07:39 +0530

    Issue #000 feat: added new files for lms-service

--- ansible/static-files/health.sh
+++ ansible/static-files/health.sh
@@ -7,6 +7,7 @@
 outpt1=$(curl -s content-service:5000/health | jq '.result.healthy')
 outpt2=$(curl -s player_player:3000/health| jq '.result.healthy')
 outpt3=$(curl -s learner-service:9000/health | jq '.result.response.checks[0].healthy')
+outpt4=$(curl -s lms-service:9005/health | jq '.result.response.checks[0].healthy')
 echo ""
 echo ""
 if [ "$outpt1" == "true" ];then
@@ -31,4 +32,13 @@ if [ "$outpt3" == "true" ];then
         echo "Learner Service is Healthy"
 else
         echo "Learner Service is unhealthy"
+fi
+
+echo ""
+echo ""
+
+if [ "$outpt4" == "true" ];then
+        echo "Lms Service is Healthy"
+else
+        echo "Lms Service is unhealthy"
 fi
