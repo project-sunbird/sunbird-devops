c5ee98c346f010042283f0879f78742f5958ebeb
harshavardhanc <harshavardhanc95@gmail.com>
2019-07-16 17:24:47 +0530

    Issue #00 feat: github api's scripts

---
+++ deploy/git/deleteBranches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "Enter Github Username: " user
+read -sp "Enter Github Password: " pass
+echo " "
+IFS=','
+grep -v -e '#' -e "^$" github.csv | while read -ra LINE
+do
+   repo_name="${LINE[0]}"
+   branch_name="${LINE[1]}"
+   echo "----------------------------------------------------"
+   echo -e '\033[0;32m'$repo_name' '$branch_name'\033[0m'
+   echo "----------------------------------------------------"
+   curl -u $user:$pass -XDELETE https://api.github.com/repos/project-sunbird/$repo_name/git/refs/heads/$branch_name
+done
