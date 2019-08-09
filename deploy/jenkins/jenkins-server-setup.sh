4586bb9b42a1a10565cbd0ba4079bdca4e52ebd2
S M Y <smy.altamash@gmail.com>
2019-08-09 14:48:12 +0530

    Installing tree as part of jenkins

--- deploy/jenkins/jenkins-server-setup.sh
+++ deploy/jenkins/jenkins-server-setup.sh
@@ -32,6 +32,9 @@ apt-get install -y jq
 echo -e "\n\e[0;32m${bold}Installating Simplejson${normal}"
 apt-get install -y python-simplejson
 
+echo -e "\n\e[0;32m${bold}Installating tree${normal}"
+apt install tree -y
+
 echo -e "\n\e[0;32m${bold}Installating Docker${normal}"
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
