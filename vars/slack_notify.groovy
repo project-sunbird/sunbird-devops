3759ad9a93032696626780bfe330a17582f8dcf1
Himanshu-optit <himanshu.j@optit.co>
2019-08-13 13:19:32 +0530

    created shared library slack_notify.groovy

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,4 +1,4 @@
-if(env.BUILD_NOTIFY_SLACK_CHANNEL != null)
+if(env.BACKUP_NOTIFY_SLACK_CHANNEL != null)
      slackSend (
             channel: "${env.BACKUP_NOTIFY_SLACK_CHANNEL}",
             color: 'danger',
@@ -6,10 +6,10 @@ if(env.BUILD_NOTIFY_SLACK_CHANNEL != null)
         )
     else if(env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
      slackSend (
-            channel: "${env.BACKUP_NOTIFY_SLACK_CHANNEL}",
+            channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
             color: 'danger',
             message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
         )
+     code to send slack notification to this slack channel
     else
       println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
-
