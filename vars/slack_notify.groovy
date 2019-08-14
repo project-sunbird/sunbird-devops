c8e5363d0d6d787f84e8d28175f9c8bc8aa161a6
Himanshu-optit <44564428+Himanshu-optit@users.noreply.github.com>
2019-08-14 19:05:06 +0530

    testing
    testing

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,15 +1,10 @@
-if(env.BACKUP_NOTIFY_SLACK_CHANNEL != null)
-     slackSend (
-            channel: "${env.BACKUP_NOTIFY_SLACK_CHANNEL}",
-            color: 'danger',
-            message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-        )
-    else if(env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
-     slackSend (
-            channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
-            color: 'danger',
-            message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-        )
-     code to send slack notification to this slack channel
-    else
-      println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
+def call() {
+       try {
+      
+            ansiColor('xterm') {
+            String ANSI_GREEN = "\u001B[32m"
+            String ANSI_NORMAL = "\u001B[0m"
+            String ANSI_BOLD = "\u001B[1m"
+       }
+       }
+}
