9849c30d7e1db820c4b13225b887f2495865d12b
Himanshu-optit <44564428+Himanshu-optit@users.noreply.github.com>
2019-08-16 20:17:47 +0530

    Update slack_notify.groovy

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,12 +1,6 @@
 def call() {
        try {
-            ansiColor('xterm') {
-                String ANSI_GREEN = "\u001B[32m"
-                String ANSI_NORMAL = "\u001B[0m"
-                String ANSI_BOLD = "\u001B[1m"
-                String ANSI_RED = "\u001B[31m"
-                String ANSI_YELLOW = "\u001B[33m"
-            
+              
                 stage('slack_notify') {
                      mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
                      slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
@@ -27,7 +21,6 @@ def call() {
       
             }
          }
-      }
       catch (err){
             throw err
         }
