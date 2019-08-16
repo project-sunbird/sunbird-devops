66b7793cd33daa0023cc83e7a99600a202160c5a
Himanshu-optit <44564428+Himanshu-optit@users.noreply.github.com>
2019-08-16 19:03:44 +0530

    checking mainDir

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -8,17 +8,20 @@ def call() {
                 String ANSI_YELLOW = "\u001B[33m"
             
                 stage('slack_notify') {
-                     if(env.BUILD_NOTIFY_SLACK_CHANNEL != null)
+                     mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
+                     slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
+                     if (slack_channel != null)
                       slackSend (
-                         channel: "${env.NOTIFY_SLACK_CHANNEL}",
+                         channel: slack_channel,
                          color: 'danger',
                          message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
                       )
                         else if(env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
                          slackSend (
-                            channel: "${env.NOTIFY_SLACK_CHANNEL}",
+                            channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
                             color: 'danger',
                             message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                       )
                         else
                          println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
       
