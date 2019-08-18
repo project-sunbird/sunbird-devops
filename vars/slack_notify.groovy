0aec427699a280b4e1a7198f9a71707beabbf2aa
Keshav Prasad <msknext@gmail.com>
2019-08-18 18:12:43 +0530

    fix: debug

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,27 +1,28 @@
 def call() {
-       try {
-              
-                stage('slack_notify') {
-                     mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
-                     slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
-                     if (slack_channel != null)
-                      slackSend (
-                         channel: slack_channel,
-                         color: 'danger',
-                         message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                      )
-                        else if(env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
-                         slackSend (
-                            channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
-                            color: 'danger',
-                            message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                       )
-                        else
-                         println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
-      
-            }
-         }
-      catch (err){
-            throw err
+    try {
+        println "In slack"
+        stage('slack_notify') {
+            mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
+            println mainDir
+            slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
+            println slack_channel
+            if (env.slack_channel != null)
+                slackSend(
+                        channel: slack_channel,
+                        color: 'danger',
+                        message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                )
+            else if (env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
+                slackSend(
+                        channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
+                        color: 'danger',
+                        message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                )
+            else
+                println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
         }
-   }
+    }
+    catch (err){
+        throw err
+    }
+}
