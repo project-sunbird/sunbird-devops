500f4c6fd609951b491cba5997b8f542a380e904
Himanshu-optit <44564428+Himanshu-optit@users.noreply.github.com>
2019-08-20 16:59:08 +0530

    Shared lib 2 (#751)
    * Update slack_notify.groovy

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,36 +1,41 @@
-def call() {
-    try {
-        ansiColor('xterm') {
-            String ANSI_GREEN = "\u001B[32m"
-            String ANSI_NORMAL = "\u001B[0m"
-            String ANSI_BOLD = "\u001B[1m"
-            String ANSI_RED = "\u001B[31m"
-            String ANSI_YELLOW = "\u001B[33m"
-            println "In slack"
-            stage('slack_notify') {
-                mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
-                println mainDir
-                slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
-                println slack_channel
-                println "$slack_channel"
-                if (slack_channel != null)
-                    slackSend(
-                            channel: slack_channel,
-                            color: 'danger',
-                            message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                    )
-                else if (env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
-                    slackSend(
+def call(String status) {
+       try {
+           ansiColor('xterm') {
+               String ANSI_GREEN = "\u001B[32m"
+               String ANSI_NORMAL = "\u001B[0m"
+               String ANSI_BOLD = "\u001B[1m"
+               String ANSI_RED = "\u001B[31m"
+               String ANSI_YELLOW = "\u001B[33m"
+               
+               stage('slack_notify') {
+                   mainDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
+                   slack_channel = mainDir.toUpperCase() + "_NOTIFY_SLACK_CHANNEL"
+                   if(status == "FAILURE"){
+                     slack_status = 'danger'
+                     build_status = "Failed"
+                  }
+                   else {
+                     slack_status = 'green'
+                     build_status = "Succeded"
+                  }
+                   if (slack_channel != null)
+                   slackSend (
+                         channel: slack_channel,
+                         color: slack_status,
+                          message: "Build ${build_status} - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                      )
+                        else if(env.GLOBAL_NOTIFY_SLACK_CHANNEL != null)
+                         slackSend (
                             channel: "${env.GLOBAL_NOTIFY_SLACK_CHANNEL}",
-                            color: 'danger',
-                            message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                    )
-                else
-                    println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
+                            color: slack_status,
+                            message: "Build ${build_status} - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                       )
+                        else
+                         println ANSI_YELLOW + ANSI_BOLD + "Could not find slack environment variable. Skipping slack notification.." + ANSI_NORMAL
+               }
             }
+         }
+      catch (err){
+            throw err
         }
-    }
-    catch (err){
-        throw err
-    }
-}
+   }
