7a3ac77692ef0c171bd71b08d8ea1f6d8d001687
Keshav Prasad <msknext@gmail.com>
2019-09-03 12:29:21 +0530

    feat: Adding email notifiy groovy class

---
+++ vars/email_notify.groovy
@@ -0,0 +1,48 @@
+def call(String buildStatus) {
+    try {
+        ansiColor('xterm') {
+            String ANSI_GREEN = "\u001B[32m"
+            String ANSI_NORMAL = "\u001B[0m"
+            String ANSI_BOLD = "\u001B[1m"
+            String ANSI_RED = "\u001B[31m"
+            String ANSI_YELLOW = "\u001B[33m"
+
+            stage('email_notify') {
+
+                if(buildStatus == "FAILURE"){
+                    slack_status = 'danger'
+                    build_status = "Failed"
+                }
+                else {
+                    slack_status = 'good'
+                    build_status = "Succeded"
+                }
+                envDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-3].trim()
+                email_group_name = envDir.toUpperCase() + "_EMAIL_GROUP"
+                try {
+                    email_group = evaluate "$email_group_name"
+                    step([$class: 'Mailer',
+                          notifyEveryUnstableBuild: true,
+                          recipients: email_group,
+                          sendToIndividuals: true])
+                    return
+                }
+                catch (MissingPropertyException ex) {
+                    println ANSI_YELLOW + ANSI_BOLD + "Could not find env specific email group. Check for global email group.." + ANSI_NORMAL
+                }
+
+                if(env.GLOBAL_EMAIL_GROUP != null)
+                    email_group = evaluate "$email_group_name"
+                step([$class: 'Mailer',
+                      notifyEveryUnstableBuild: true,
+                      recipients: email_group,
+                      sendToIndividuals: true])
+                else
+                println ANSI_YELLOW + ANSI_BOLD + "Could not find golbal email group variable. Skipping email notification.." + ANSI_NORMAL
+            }
+        }
+    }
+    catch (err){
+        throw err
+    }
+}
