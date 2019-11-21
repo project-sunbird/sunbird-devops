5f13fb9b3d5a3a126d5ac00c89324305879bac8a
rjshrjndrn <rjshrjndrn@gmail.com>
2019-11-21 16:15:58 +0530

    Issue #000 feat: removed badger

--- deploy/deploy-core.sh
+++ deploy/deploy-core.sh
@@ -16,18 +16,6 @@ source version.env
 [ -f ~/jwt_token_player.txt ] || echo "JWT token (~/jwt_token_player.txt) not found" || exit 1
 sunbird_api_auth_token=$(cat ~/jwt_token_player.txt|sed 's/ //g')
 
-# Bootstrap swarm
-echo "@@@@@@@@@ Bootstrap swarm"
-ansible-playbook -i $INVENTORY_PATH ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm --extra-vars @config.yml
-
-# Re-deploying badger service
-echo "Redeploy Badger service"
-ansible-playbook -i $INVENTORY_PATH ../ansible/deploy-badger.yml  --extra-vars=@config.yml --extra-vars "hub_org=${ORG} image_name=badger image_tag=${BADGER_SERVICE_VERSION}"
-
-[ -f ~/badger_token.txt ] ||  echo "badger auth token (~/badger_token.txt) not found" || exit 1
-badger_token=$(cat ~/badger_token.txt | cut -d '"' -f 4)
-
-
 # Re-deploy Player service
 echo "@@@@@@@@@ Redeploy player service"
 ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-sunbird" --extra-vars "hub_org=${ORG} image_name=player image_tag=${PLAYER_VERSION} service_name=player deploy_stack=True sunbird_api_auth_token=${sunbird_api_auth_token} vault_badging_authorization_key=${badger_token}" --extra-vars @config.yml
