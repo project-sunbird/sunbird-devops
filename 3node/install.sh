445ccea7a43b305ea06e2448ea51adea698b5c43
rjshrjndrn <rjshrjndrn@gmail.com>
2019-11-21 11:51:37 +0530

    Issue #000 feat: installing helm

--- 3node/install.sh
+++ 3node/install.sh
@@ -3,3 +3,8 @@ echo Installing k8s cluster
 
 curl -L https://get.k3s.io | INSTALL_K3S_VERSION=v0.9.0 INSTALL_K3S_EXEC="--no-deploy=traefik --no-deploy=servicelb" sh -
 sudo chown $(whoami) /etc/rancher/k3s/k3s.yaml
+echo 'Installing helm3'
+curl -LO https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz
+tar --strip 1 -xf  helm-v3.0.0-linux-amd64.tar.gz && rm helm-v3.0.0-linux-amd64.tar.gz
+sudo mv helm /usr/local/bin
+
