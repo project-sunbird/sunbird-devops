2bd8f14ecb9bd4be012ab9a21d2ccf9638d87feb
rjshrjndrn <rjshrjndrn@gmail.com>
2019-11-21 11:54:50 +0530

    Issue #000 feat: check kube already installed

--- 3node/install.sh
+++ 3node/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo Installing k8s cluster
 
-curl -L https://get.k3s.io | INSTALL_K3S_VERSION=v0.9.0 INSTALL_K3S_EXEC="--no-deploy=traefik --no-deploy=servicelb" sh -
-sudo chown $(whoami) /etc/rancher/k3s/k3s.yaml
+if [[ ! $(kubectl get po) ]]; then
+    curl -L https://get.k3s.io | INSTALL_K3S_VERSION=v0.9.0 INSTALL_K3S_EXEC="--no-deploy=traefik --no-deploy=servicelb" sh -
+    sudo chown $(whoami) /etc/rancher/k3s/k3s.yaml
+fi
+
 echo 'Installing helm3'
 curl -LO https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz
 tar --strip 1 -xf  helm-v3.0.0-linux-amd64.tar.gz && rm helm-v3.0.0-linux-amd64.tar.gz
