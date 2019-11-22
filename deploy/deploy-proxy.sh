e928d87c31013140abeabcb5bbd2fbbd1b695ec7
rjshrjndrn <rjshrjndrn@gmail.com>
2019-11-22 10:07:09 +0530

    Issue #000 feat: reomoving bootstraping swarm

--- deploy/deploy-proxy.sh
+++ deploy/deploy-proxy.sh
@@ -13,10 +13,6 @@ ORG=sunbird
 # Getting versions
 source version.env
 
-# Bootstrap swarm
-echo "@@@@@@@@@ Bootstrap swarm"
-ansible-playbook -i $INVENTORY_PATH ../ansible/bootstrap.yml  --extra-vars "hosts=swarm-manager" --tags bootstrap_swarm --extra-vars=@config.yml
-
 # Re-deploy Proxy
 echo "Redeploy Proxy"
 ansible-playbook -i $INVENTORY_PATH ../ansible/deploy.yml --tags "stack-proxy" --extra-vars "hub_org=${ORG} image_name=proxy image_tag=${PROXY_VERSION}" --extra-vars=@config.yml
