e39b862148f5692ecf8f2ab391c942eabc1a63db
Rajesh <rjshrjndrn@gmail.com>
2019-11-22 09:17:21 +0000

    Creating minimal bootstrapping for k8s without istio and others

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Installing deps
-sudo ./install-deps.sh
+# sudo ./install-deps.sh
 
 # installing dbs
 # Installing all dbs
 echo $INVENTORY_PATH
-[[ -z INVENTORY_PATH ]] && echo -e "ERROR: set environment variable \nexport INVENTORY_PATH=/path/to/ansible/inventory" && exit 100
-
+[[ $INVENTORY_PATH == "" ]] && echo -e "ERROR: set environment variable \nexport INVENTORY_PATH=/path/to/ansible/inventory" && exit 100
 module=Core
 # Creating inventory strucure
 cp $INVENTORY_PATH/$module/* ../ansible/inventory/env/
-ansible-playbook -i ../ansible/inventory/env/ ../ansible/provision.yml --skip-tags "postgresql-slave,log-es"
+# Installing dbs (es, cassandra, postgres)
+# ansible-playbook -i ../ansible/inventory/env/ ../ansible/provision.yml --skip-tags "postgresql-slave,log-es"
+
+# Bootstrapping k8s
+ansible-playbook -i ../ansible/inventory/env/ ../kubernetes/ansible/bootstrap_minimal.yaml
+# Installing API manager and onboarding apis
+ansible-playbook -i ../ansible/inventory/env/ ../kubernetes/ansible/deploy_core_service.yml -e chart_path=/home/ops/sunbird-devops/kubernetes/helm_charts/core/apimanager -e release_name=apimanager
