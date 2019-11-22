95446ca088fac71f13d94ba138fc8e1f888da43a
Rajesh <rjshrjndrn@gmail.com>
2019-11-22 05:50:17 +0000

    Error check for installation

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,8 +5,10 @@ sudo ./install-deps.sh
 
 # installing dbs
 # Installing all dbs
-[[ -z INVENTORY_PATH ]] || echo -e "set environment variable \nINVENTORY_PATH=/path/to/ansible/inventory" && exit 100
+echo $INVENTORY_PATH
+[[ -z INVENTORY_PATH ]] && echo -e "ERROR: set environment variable \nexport INVENTORY_PATH=/path/to/ansible/inventory" && exit 100
 
+module=Core
 # Creating inventory strucure
-
+cp $INVENTORY_PATH/$module/* ../ansible/inventory/env/
 ansible-playbook -i $INVENTORY_PATH ../ansible/provision.yml --skip-tags  postgresql-slave --extra-vars=@config.yml
