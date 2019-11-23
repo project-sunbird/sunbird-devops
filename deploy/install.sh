43416eae45b63227f8239d331a18def47b6f7c55
Rajesh <rjshrjndrn@gmail.com>
2019-11-23 08:28:01 +0000

    fixes for dbs and helm

--- deploy/install.sh
+++ deploy/install.sh
@@ -12,7 +12,7 @@ module=Core
 cp $INVENTORY_PATH/$module/* ../ansible/inventory/env/
 # Installing dbs (es, cassandra, postgres)
 ansible-playbook -i ../ansible/inventory/env/ ../ansible/provision.yml --tags postgresql-master #--skip-tags "postgresql-slave,log-es"
-ansible-playbook -i ../ansible/inventory/env/ ../ansible/postgresql-data-update.yml --tags postgresql-master #--skip-tags "postgresql-slave,log-es"
+ansible-playbook -i ../ansible/inventory/env/ ../ansible/postgresql-data-update.yml #--skip-tags "postgresql-slave,log-es"
 
 # # Bootstrapping k8s
 ansible-playbook -i ../ansible/inventory/env/ ../kubernetes/ansible/bootstrap_minimal.yaml
