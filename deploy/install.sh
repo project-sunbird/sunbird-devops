e352a41a1351040ffbb8198f104d09bc1651912e
rjshrjndrn <rjshrjndrn@gmail.com>
2019-12-19 06:48:53 +0530

    removing skip es tag

--- deploy/install.sh
+++ deploy/install.sh
@@ -31,7 +31,7 @@ done
 git checkout -- ../ansible/inventory/env/group_vars/all.yml # This is to make sure always the all.yaml is updated
 cp $INVENTORY_PATH/$module/* ../ansible/inventory/env/
 # Installing dbs (es, cassandra, postgres)
-ansible-playbook -i ../ansible/inventory/env/ ../ansible/provision.yml --skip-tags "es,postgresql-slave,log-es"
+ansible-playbook -i ../ansible/inventory/env/ ../ansible/provision.yml --skip-tags "postgresql-slave,log-es"
 ansible-playbook -i ../ansible/inventory/env/ ../ansible/postgresql-data-update.yml
 ansible-playbook -i ../ansible/inventory/env/ ../ansible/es-mapping.yml --extra-vars "indices_name=all ansible_tag=run_all_index_and_mapping"
 # Bootstrapping k8s
