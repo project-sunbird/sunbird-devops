9b73b6fe0806989aa703190b04788a0474b0e60b
rjshrjndrn <rjshrjndrn@gmail.com>
2020-01-07 09:10:02 +0530

    fix: run container as normal user

--- deploy/grafana/backup.sh
+++ deploy/grafana/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # How to run
-# docker run --rm -v$(pwd):/work -v $(pwd)/backup.sh:/work/backup.sh rjshrjndrn/wizzy /work/backup.sh https://grafna/url password
+# docker run --rm -v /etc/passwd:/etc/passwd -u $(id -u):$(id -g) -v$(pwd):/work -v $(pwd)/backup.sh:/work/backup.sh rjshrjndrn/wizzy /work/backup.sh https://grafna/url admin password
 url=$1
-password=$2
+username=$2
+password=$3
 script=$(echo $0)
 cd $(dirname "$script")
 echo Backing up grafafna in $(dirname "$script")
 wizzy set grafana url $url
-wizzy set grafana username admin
+wizzy set grafana username $username
 wizzy set grafana password $password
 wizzy import dashboards
-
