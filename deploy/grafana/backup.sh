2af38204fd641a475e8e32cbcfb8d6b79107a8c4
rjshrjndrn <rjshrjndrn@gmail.com>
2020-01-07 10:59:48 +0530

    Make import/export operation as option

--- deploy/grafana/backup.sh
+++ deploy/grafana/backup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # How to run
-# docker run --rm -v /etc/passwd:/etc/passwd -u $(id -u):$(id -g) -v$(pwd):/work -v $(pwd)/backup.sh:/work/backup.sh rjshrjndrn/wizzy /work/backup.sh https://grafna/url admin password
+# docker run --rm -v /etc/passwd:/etc/passwd -u $(id -u):$(id -g) -v$(pwd):/work -v $(pwd)/backup.sh:/work/backup.sh rjshrjndrn/wizzy /work/backup.sh https://grafna/url admin password <import or export>
 url=$1
 username=$2
 password=$3
+mode=${4:-import}
 script=$(echo $0)
 cd $(dirname "$script")
-echo Backing up grafafna in $(dirname "$script")
+echo ${mode}ing grafafna in $(dirname "$script")
 wizzy set grafana url $url
 wizzy set grafana username $username
 wizzy set grafana password $password
-wizzy import dashboards
+wizzy $mode dashboards
