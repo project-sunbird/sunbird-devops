ae4b374bcb2460d0720afe97b251b51ca0166d37
Harsha <harshavardhanc95@gmail.com>
2020-01-17 15:47:08 +0530

    Update copy_file_to_wksp.groovy

--- vars/copy_file_to_wksp.groovy
+++ vars/copy_file_to_wksp.groovy
@@ -30,9 +30,7 @@ def inputGetFile(Map pipelineParams, String savedfile = null) {
          inventory_path = "${pipelineParams.currentWs}/ansible/inventory/$pipelineParams.env"
           sh """
              cp ${pipelineParams.currentWs}/token.xlsx ${pipelineParams.currentWs}/data_input/
-                     ansible-playbook -i ${
-                 inventory_path -c local
-             } $pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
+                     ansible-playbook -i ${inventory_path} -c local $pipelineParams.ansiblePlaybook $pipelineParams.ansibleExtraArgs
              """
 
         }
