e2dd2b728c99ffcb0d951c8d724c5cc35e2e02d7
Rajesh Rajendran <rjshrjndrn@gmail.com>
2020-03-01 15:03:52 +0530

    fixed possible race condition.
    Added mutex to find last commited message
    Commiting only last message, making fetching message async task

--- exporters/Go/kafka-topic-exporter/main.go
+++ exporters/Go/kafka-topic-exporter/main.go
@@ -76,10 +76,38 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	kafka "github.com/rjshrjndrn/kafka-go"
 )
+
+// This is to get the last message served to prom endpoint.
+type lastReadMessage struct {
+	mu   sync.RWMutex
+	last kafka.Message
+}
+
+// Adding message
+func (lrm *lastReadMessage) Store(message kafka.Message) {
+	lrm.mu.Lock()
+	defer lrm.mu.Unlock()
+
+	lrm.last = message
+}
+
+// Return the last message read
+func (lrm *lastReadMessage) Get() kafka.Message {
+	lrm.mu.RLock()
+	defer lrm.mu.RUnlock()
+	return lrm.last
+}
+
+// Message format
+type metric struct {
+	message string
+	id      kafka.Message
+}
 
 // Metrics structure
 type Metrics struct {
@@ -112,41 +140,53 @@ func metricsNameValidator(names ...string) string {
 // This function will take the metrics input and create prometheus metrics
 // output and send it to metrics channel
 // So that http endpoint can serve the data
-func (metrics *Metrics) pushMetrics() (err error) {
+func (metrics *Metrics) pushMetrics(ctx context.Context, metricData *kafka.Message) (err error) {
 	label := fmt.Sprintf("system=%q,subsystem=%q,", metrics.System, metrics.SubSystem)
-	// Creating dictionary of labels
-	for _, labels := range metrics.Lables {
-		// Lables can't have '-' in it
-		label += fmt.Sprintf("%v=%q,", metricsNameValidator(labels.ID), labels.Value)
-	}
-	// Generating metrics
-	for _, metric := range metrics.Metrics {
-		retMetrics := ""
-		// Adding optional timestamp
-		switch metrics.MetricTS {
-		case "":
-			retMetrics = fmt.Sprintf("%s{%s} %.2f", metricsNameValidator(metrics.System, metrics.SubSystem, metric.ID), strings.TrimRight(label, ","), metric.Value)
-		default:
-			retMetrics = fmt.Sprintf("%s{%s} %.2f %s", metricsNameValidator(metrics.System, metrics.SubSystem, metric.ID), strings.TrimRight(label, ","), metric.Value, metrics.MetricTS)
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		// Creating dictionary of labels
+		for _, labels := range metrics.Lables {
+			// Lables can't have '-' in it
+			label += fmt.Sprintf("%v=%q,", metricsNameValidator(labels.ID), labels.Value)
 		}
-		fmt.Printf("%s\n", retMetrics)
-		promMetricsChannel <- retMetrics
+		// Generating metrics
+		for _, m := range metrics.Metrics {
+			metricStruct := metric{}
+			// Adding optional timestamp
+			switch metrics.MetricTS {
+			case "":
+				metricStruct.message = fmt.Sprintf("%s{%s} %.2f", metricsNameValidator(metrics.System, metrics.SubSystem, m.ID), strings.TrimRight(label, ","), m.Value)
+			default:
+				metricStruct.message = fmt.Sprintf("%s{%s} %.1f %s", metricsNameValidator(metrics.System, metrics.SubSystem, m.ID), strings.TrimRight(label, ","), m.Value, metrics.MetricTS)
+			}
+			fmt.Printf("%s\n", metricStruct.message)
+			metricStruct.id = *metricData
+			promMetricsChannel <- metricStruct
+		}
+		return nil
 	}
-	return nil
 }
 
 // Channel to keep metrics till prometheus scrape that
-var promMetricsChannel = make(chan string)
+var promMetricsChannel = make(chan metric)
 
-func metricsCreation(data []byte) error {
+func metricsCreation(ctx context.Context, m kafka.Message) error {
 	metrics := Metrics{}
+	data := m.Value
 	// Creating metrics struct
-	if err := json.Unmarshal(data, &metrics); err != nil {
-		fmt.Printf("Unmarshal error: %q\n", err)
-		return err
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		if err := json.Unmarshal(data, &metrics); err != nil {
+			fmt.Printf("Unmarshal error: %q\n", err)
+			return err
+		}
+		metrics.pushMetrics(ctx, &m)
+		return nil
 	}
-	metrics.pushMetrics()
-	return nil
 }
 
 // Http handler
@@ -154,17 +194,25 @@ func serve(w http.ResponseWriter, r *http.Request) {
 	// Channel to keep track of offset lag
 	lagChannel := make(chan int64, 1)
 	ctx := r.Context()
+	lastReadMessage := lastReadMessage{}
 	// Creating context
 	// Reading topic
 	go func(ctx context.Context, r *kafka.Reader) {
 		for {
-			m, err := r.ReadMessage(ctx)
+			// Only fetching the message, not commiting them
+			// It'll be commited once the transmission closes
+			m, err := r.FetchMessage(ctx)
 			if err != nil {
-				fmt.Printf("err reading message: %v", err)
+				fmt.Printf("err reading message: %v\n", err)
 				break
 			}
 			fmt.Printf("topic: %q partition: %v offset: %v lag: %d\n ", m.Topic, m.Partition, m.Offset, r.Lag())
-			go metricsCreation(m.Value)
+			go func(ctx context.Context) {
+				if err := metricsCreation(ctx, m); err != nil {
+					fmt.Printf("errored out metrics creation; err:  %s\n", err)
+					return
+				}
+			}(ctx)
 			fmt.Println(r.Lag())
 			lagChannel <- r.Lag()
 		}
@@ -172,13 +220,35 @@ func serve(w http.ResponseWriter, r *http.Request) {
 	for {
 		select {
 		case message := <-promMetricsChannel:
-			fmt.Fprintf(w, "%s\n", message)
+			lastReadMessage.Store(message.id)
+			fmt.Fprintf(w, "%s\n", message.message)
 			// Waiting for 1 ms before quitting
 		case <-ctx.Done():
-			fmt.Printf("done")
+			// explicitly commiting last read message
+			messageLastRead := lastReadMessage.Get()
+			// Don't commit if offset is zero
+			if messageLastRead.Offset != 0 {
+				fmt.Printf("Commiting message offset %d\n", messageLastRead.Offset)
+				// Using context.Background, Commit need to be successful.
+				if err := kafkaReader.CommitMessages(context.Background(), messageLastRead); err != nil {
+					fmt.Printf("Error commiting message, err: %q\n", err)
+				}
+
+			}
+			fmt.Printf("done\n")
 			return
 		case lag := <-lagChannel:
 			if lag == 0 {
+				// explicitly commiting last read message
+				messageLastRead := lastReadMessage.Get()
+				// Don't commit if offset is zero
+				if messageLastRead.Offset != 0 {
+					// explicitly commiting last read message
+					messageLastRead := lastReadMessage.Get()
+					fmt.Printf("Commiting message offset %d\n", messageLastRead.Offset)
+					kafkaReader.CommitMessages(ctx, messageLastRead)
+				}
+				fmt.Println("queue length: ", len(promMetricsChannel))
 				return
 			}
 		}
@@ -208,12 +278,15 @@ For example,
 	fmt.Println("kafka is accessible")
 	// Initializing kafka
 	kafkaReader = kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  []string{kafka_host},
-		GroupID:  "metrics-reader-test", // Consumer group ID
-		Topic:    kafka_topic,
-		MinBytes: 10e3, // 10KB
-		MaxBytes: 10e6, // 10MB
+		Brokers:          []string{kafka_host},
+		GroupID:          "metrics-reader-test", // Consumer group ID
+		Topic:            kafka_topic,
+		MinBytes:         1e3,  // 1KB
+		MaxBytes:         10e6, // 10MB
+		MaxWait:          200 * time.Millisecond,
+		RebalanceTimeout: time.Second * 5,
 	})
+	defer kafkaReader.Close()
 	http.HandleFunc("/metrics", serve)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
