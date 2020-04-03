97986f40ae5e41588eaed9c09bf9a48f9bc62210
Keshav Prasad <msknext@gmail.com>
2020-04-03 16:49:39 +0530

    feat: updating code for auto build compatibility

--- vars/auto_build_deploy.groovy
+++ vars/auto_build_deploy.groovy
@@ -6,7 +6,7 @@ def call(){
             String ANSI_BOLD = "\u001B[1m"
             String ANSI_RED = "\u001B[31m"
             String ANSI_YELLOW = "\u001B[33m"
-            
+
             tag_name = env.JOB_NAME.split("/")[-1]
             jobName = env.JOB_NAME.split("/")[-2]
             module = env.JOB_NAME.split("/")[-3]
@@ -14,22 +14,34 @@ def call(){
             envDir = "$auto_deploy_env"
 
             println ANSI_BOLD + ANSI_GREEN + "$jobName build succeeded. Triggering ArtifactUpload.." + ANSI_NORMAL
+            
             uploadStatus = build job: "ArtifactUpload/$envDir/$module/$jobName", parameters: [string(name: 'absolute_job_path', value: "$JOB_NAME")]
-            if (uploadStatus.result == "SUCCESS") {
+            
+            if (uploadStatus.currentResult == "SUCCESS") {
                 println ANSI_BOLD + ANSI_GREEN + "ArtifactUpload/$envDir/$module/$jobName succeeded. Triggering Deployment.." + ANSI_NORMAL
+                
+                slack_notify("SUCCESS", tag_name, uploadStatus.fullProjectName, uploadStatus.number, uploadStatus.absoluteUrl)
+                email_notify()
+                
                 deployStatus = build job: "Deploy/$envDir/$module/$jobName", parameters: [string(name: 'private_branch', value: "$private_repo_branch"), string(name: 'branch_or_tag', value: "$branch_or_tag")]
-                if (deployStatus.result == "SUCCESS") {
+                
+                if (deployStatus.currentResult == "SUCCESS") {
                     println ANSI_BOLD + ANSI_GREEN + "Deploy/$envDir/$module/$jobName succeeded. Notifying via email and slack.." + ANSI_NORMAL
-                    slack_notify()
+            
+                    slack_notify("SUCCESS", tag_name, deployStatus.fullProjectName, deployStatus.number, deployStatus.absoluteUrl)
                     email_notify()
+            
                 } else {
                     println ANSI_BOLD + ANSI_RED + "Deploy/$envDir/$module/$jobName failed. Notifying via email and slack.." + ANSI_NORMAL
-                    slack_notify()
+            
+                    slack_notify("FAILURE", tag_name, deployStatus.fullProjectName, deployStatus.number, deployStatus.absoluteUrl)
                     email_notify()
+            
                 }
             } else {
                 println ANSI_BOLD + ANSI_GREEN + "ArtifactUpload/$envDir/$module/$jobName failed. Notifying via email and slack.." + ANSI_NORMAL
-                slack_notify()
+            
+                slack_notify("FAILURE", tag_name, uploadStatus.fullProjectName, uploadStatus.number, uploadStatus.absoluteUrl)
                 email_notify()
             }
         }
