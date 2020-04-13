310c661bf60f2d4bfeddb30b029067da79a755b5
S M Y ALTAMASH smy.altamash@gmail.com <smy.altamash@gmail.com>
2020-04-13 18:40:46 +0530

    Remove old logic of calculating heap size

--- ansible/roles/cassandra/templates/cassandra-env.sh
+++ ansible/roles/cassandra/templates/cassandra-env.sh
@@ -33,24 +33,6 @@ calculate_heap_sizes()
         system_cpu_cores="1"
     fi
 
-    half_system_memory_in_mb=`expr $system_memory_in_mb / 2`
-    quarter_system_memory_in_mb=`expr $half_system_memory_in_mb / 2`
-    if [ "$half_system_memory_in_mb" -gt "1024" ]
-    then
-        half_system_memory_in_mb="1024"
-    fi
-    if [ "$quarter_system_memory_in_mb" -gt "8192" ]
-    then
-        quarter_system_memory_in_mb="8192"
-    fi
-    if [ "$half_system_memory_in_mb" -gt "$quarter_system_memory_in_mb" ]
-    then
-        max_heap_size_in_mb="$half_system_memory_in_mb"
-    else
-        max_heap_size_in_mb="$quarter_system_memory_in_mb"
-    fi
-    MAX_HEAP_SIZE="${max_heap_size_in_mb}M"
-
     # Young gen: min(max_sensible_per_modern_cpu_core * num_cores, 1/4 * heap size)
     max_sensible_yg_per_core_in_mb="100"
     max_sensible_yg_in_mb=`expr $max_sensible_yg_per_core_in_mb "*" $system_cpu_cores`
