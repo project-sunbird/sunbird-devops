7be93d0f722dc2d1de763c036a062d21810047ed
S M Y ALTAMASH smy.altamash@gmail.com <smy.altamash@gmail.com>
2020-04-13 18:42:46 +0530

    Adding new logic with proper documentation to upgrade cassandra heap size as per the best practice

--- ansible/roles/cassandra/templates/cassandra-env.sh
+++ ansible/roles/cassandra/templates/cassandra-env.sh
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 calculate_heap_sizes()
 {
     case "`uname`" in
@@ -33,6 +49,28 @@ calculate_heap_sizes()
         system_cpu_cores="1"
     fi
 
+    # New Logic to pick the heap size
+    # If resource_crunch == true then take quarter size of the Server
+    # else take half the size of the server and if half server Size exceeds 12GB then always take 12GB as the max heap size
+  
+    half_system_memory_in_mb=`expr $system_memory_in_mb / 2`
+    quarter_system_memory_in_mb=`expr $half_system_memory_in_mb / 2`
+    
+    resource_crunch="true"
+    if [ "$resource_crunch" == "true" ]
+    then
+      MAX_HEAP_SIZE="${quarter_system_memory_in_mb}M"
+    fi
+  
+    if [[ "$resource_crunch" != "true" ]] && [[ "$half_system_memory_in_mb" -gt "12288" ]]
+    then
+        MAX_HEAP_SIZE="12288M"
+    elif [ "$resource_crunch" != "true" ]
+    then
+        MAX_HEAP_SIZE="${half_system_memory_in_mb}M"
+    fi
+
+    HEAP_NEWSIZE=$MAX_HEAP_SIZE
 }
 
 # Determine the sort of JVM we'll be running on.
