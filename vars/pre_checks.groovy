1f7434f098d918876fa316494479b622c1aae88b
Keshav Prasad <msknext@gmail.com>
2020-04-15 17:09:01 +0530

    Update and rename check_slot.groovy to pre_checks.groovy

--- vars/pre_checks.groovy
+++ vars/pre_checks.groovy
@@ -7,7 +7,7 @@ def call() {
             String ANSI_BOLD = "\u001B[1m"
             String ANSI_RED = "\u001B[31m"
             String ANSI_YELLOW = "\u001B[33m"
-            
+
             def date = new Date()
             def sdf = new SimpleDateFormat("HH:mm:ss")
             current = sdf.format(date)
@@ -16,10 +16,12 @@ def call() {
             end = Date.parse("HH:mm:ss", env.END_TIME)
             current = Date.parse("HH:mm:ss", current)
 
-            if (current.after(start) && current.before(end))
+            if (current.after(start) && current.before(end)) {
+                println ANSI_BOLD + ANSI_GREEN + "Tigger is in the deployment window. Continuing build" + ANSI_NORMAL
                 return
+            }
             else {
-                println ANSI_BOLD + ANSI_RED + "Tigger is not in the deployment window. Skipping build" + ANSI_NORMAL
+                println ANSI_BOLD + ANSI_RED + "Tigger is NOT in the deployment window. Skipping build" + ANSI_NORMAL
                 error "Tigger is not in the deployment window. Skipping build"
             }
     }
