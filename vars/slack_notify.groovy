f6e39f0cbb5e62def21d9fccf1b24921cde54d91
Keshav Prasad <msknext@gmail.com>
2020-04-15 19:30:17 +0530

    fix: updated slack url

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -84,7 +84,7 @@ def call(String buildStatus, String release_tag=null, String jobName=null, int b
                                     color: slack_status,
                                     message: "Build ${build_status} for ${release_tag} - ${jobName} ${buildNumber} (<${jobUrl}|Open>)",
                                     notifyCommitters: true,
-                                    teamDomain: env.automated_slack_workspace,
+                                    baseUrl: env.automated_slack_workspace,
                                     tokenCredentialId: 'automated_slack_token'
                             )
 
