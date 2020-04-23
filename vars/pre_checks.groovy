e5ec704401955617888ae1fd0ed26f84653d5e46
Keshav Prasad <msknext@gmail.com>
2020-04-24 00:02:20 +0530

    fix: text spacing

--- vars/pre_checks.groovy
+++ vars/pre_checks.groovy
@@ -20,7 +20,7 @@ def call() {
                 println ANSI_BOLD + ANSI_GREEN + "Tigger is in the deployment window.. Check if tag matches our pattern.." + ANSI_NORMAL
                 tag_name = env.JOB_NAME.split("/")[-1]
                 if (!tag_name.contains(env.public_repo_branch) || !tag_name.contains("_RC")) {
-                    println(ANSI_BOLD + ANSI_RED + "Error.. Tag does not contain " + env.public_repo_branch + "or is not a RC tag" + ANSI_NORMAL)
+                    println(ANSI_BOLD + ANSI_RED + "Error.. Tag does not contain " + env.public_repo_branch + " or is not a RC tag" + ANSI_NORMAL)
                     error "Oh ho! Tag is not a release candidate.. Skipping build"
                 }
                 else {
