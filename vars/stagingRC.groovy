5728644df4d1849bfdf81edcc172fac0e10b0bed
Keshav Prasad <msknext@gmail.com>
2020-04-24 00:08:30 +0530

    fix: msg update and reduced variable usage

--- vars/stagingRC.groovy
+++ vars/stagingRC.groovy
@@ -17,11 +17,11 @@ def call() {
             current = Date.parse("HH:mm:ss", current)
 
             if (current.after(start) && current.before(end)) {
-                println (ANSI_BOLD + ANSI_GREEN + "Tigger is in the deployment window.. Check if the release branch matches pattren.." + ANSI_NORMAL)
-                branch_name = params.release_branch
-                if (!branch_name.contains(env.public_repo_branch)) {
-                    println(ANSI_BOLD + ANSI_RED + "Error.. release branch does not match " + env.public_repo_branch  + ANSI_NORMAL)
-                    error "Oh ho! branch is not a release candidate.. Skipping creation of tag"
+                println (ANSI_BOLD + ANSI_GREEN + "Tigger is in the deployment window.. Check if the release branch matches current release pattern.." + ANSI_NORMAL)
+//                branch_name = params.release_branch
+                if (params.release_branch != env.public_repo_branch) {
+                    println(ANSI_BOLD + ANSI_RED + "Sorry.. The branch your entered does not match the release branch: " + env.public_repo_branch  + ANSI_NORMAL)
+                    error "Oh ho! The branch your entered is not a staging release candidate.. Skipping creation of tag"
                 }
                 else {
                     println (ANSI_BOLD + ANSI_GREEN + "All checks passed - Continuing build.." + ANSI_NORMAL)
