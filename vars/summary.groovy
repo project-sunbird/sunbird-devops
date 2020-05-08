36eb839dfc4e0a41fd8c1404b6b8b1d79cdf3acc
Keshav Prasad <msknext@gmail.com>
2020-05-08 19:05:51 +0530

    fix: escape \ with another \

--- vars/summary.groovy
+++ vars/summary.groovy
@@ -5,14 +5,14 @@ def call() {
             String ANSI_NORMAL = "\u001B[0m"
             String ANSI_BOLD = "\u001B[1m"
 
-                
+
             jobName = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim()
             module = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-2].trim()
             envDir = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-3].trim()
             jobType = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-4].trim()
             privateBranch = params.private_branch
             publicBranch = params.branch_or_tag
-        
+
             stage('Write data') {
                 sh """
                     mkdir -p ${JENKINS_HOME}/summary/${envDir}
@@ -22,14 +22,14 @@ def call() {
                     if (module == "Core" || module == "Kubernetes") {
                         image_tag = sh(returnStdout: true, script: 'if [[ -f metadata.json ]]; then jq -r .image_tag metadata.json; else echo "NA"; fi').trim()
                         sh """
-                            sed -i "s/${jobType}\/${module}\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
+                            sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                             sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                             echo "${jobType}/${module}/${jobName},  imagetag: ${image_tag}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}">> $JENKINS_HOME/summary/${envDir}/summary.txt
                         """
                     } else {
                         artifact_version = sh(returnStdout: true, script: 'if [[ -f metadata.json ]]; then jq -r .artifact_version metadata.json; else echo "NA"; fi').trim()
                         sh """
-                            sed -i "s/${jobType}\/${module}\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
+                            sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                             sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                             echo "${jobType}/${module}/${jobName},  artifact_version: ${artifact_version}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}" >> $JENKINS_HOME/summary/${envDir}/summary.txt
                         """
@@ -37,14 +37,13 @@ def call() {
                 }
 
                 if (jobType == "Provision"  || jobType == "OpsAdministration") {
-                     sh """
-                        sed -i "s/${jobType}\/${module}\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
+                    sh """
+                        sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                         sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
                         echo "${jobType}/${module}/${jobName}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}" >> $JENKINS_HOME/summary/${envDir}/summary.txt
-                    """    
-                }  
+                    """
+                }
             }
-
         }
     }
 
