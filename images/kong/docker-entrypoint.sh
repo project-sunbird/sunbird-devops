c93d3d32b3ffc10d6795d076a24f0545fe3e89a5
keshavprasadms <msknext@gmail.com>
2020-05-22 20:22:55 +0530

    fix: updated to kong 0.10.4

--- images/kong/docker-entrypoint.sh
+++ images/kong/docker-entrypoint.sh
@@ -15,7 +15,7 @@ cat <> /tmp/logpipe 1>&1 &
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
 # Ensure kong listens on correct ip address https://github.com/Mashape/docker-kong/issues/93
-IP_ADDR=`ifconfig eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}'`
+IP_ADDR=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 export KONG_CLUSTER_LISTEN="$IP_ADDR:7946"
 
 echo "KONG_CLUSTER_LISTEN: $KONG_CLUSTER_LISTEN"
