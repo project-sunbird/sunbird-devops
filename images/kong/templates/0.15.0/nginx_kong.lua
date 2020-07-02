bda6f6623dbd83c2b5b8d00d28f8783c09450ca5
keshavprasadms <msknext@gmail.com>
2020-07-02 11:35:34 +0530

    fix: add env option for RL cache

--- images/kong/templates/0.15.0/nginx_kong.lua
+++ images/kong/templates/0.15.0/nginx_kong.lua
@@ -35,7 +35,7 @@ lua_shared_dict kong_locks          8m;
 lua_shared_dict kong_process_events 5m;
 lua_shared_dict kong_cluster_events 5m;
 lua_shared_dict kong_healthchecks   5m;
-lua_shared_dict kong_rate_limiting_counters 512m;
+lua_shared_dict kong_rate_limiting_counters ${{RATELIMIT_CACHE_SIZE}};
 > if database == "cassandra" then
 lua_shared_dict kong_cassandra      5m;
 > end
