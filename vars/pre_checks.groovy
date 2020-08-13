68ea7f8bfea346f2a5750792aeb0e5ea152b2e52
Keshav Prasad <msknext@gmail.com>
2020-08-13 10:38:16 +0530

    fix: temporary fix for creative branching strategy of Portal

--- vars/pre_checks.groovy
+++ vars/pre_checks.groovy
@@ -19,19 +19,27 @@ def call() {
             if (current.after(start) && current.before(end)) {
                 println ANSI_BOLD + ANSI_GREEN + "Tigger is in the deployment window.. Check if tag matches our pattern.." + ANSI_NORMAL
                 tag_name = env.JOB_NAME.split("/")[-1]
+                
+                // Temporary fix starts - For the creative branching strategy of Portal 
+                if (env.JOB_NAME.split("/")[-2] == "Player" && tag_name.contains(allowed_releases.split(",")[0])){
+                    println ANSI_BOLD + ANSI_RED + "Block this build. Its creating unnecessry havoc.." + ANSI_NORMAL
+                    error "This tag is blocked"
+                }
+                // Temporary fix ends - For the creative branching strategy of Portal 
+
                 allowed_releases = env.staging_allowed_releases
                 flag = 0
                 for(i = 0; i < allowed_releases.split(",").length; i++) {
-                    if (tag_name.contains(allowed_releases.split(",")[i]) && tag_name.contains("_RC")) {    
-                       println ANSI_BOLD + ANSI_GREEN + "All checks passed - Continuing build.." + ANSI_NORMAL
-                       flag = 1
-                       break;
+                    if (tag_name.contains(allowed_releases.split(",")[i]) && tag_name.contains("_RC")) {
+                        println ANSI_BOLD + ANSI_GREEN + "All checks passed - Continuing build.." + ANSI_NORMAL
+                        flag = 1
+                        break;
                     }
                 }
                 if (flag == 0) {
-                  println(ANSI_BOLD + ANSI_RED + "Error.. Tag does not contain " + env.automated_public_repo_branch + " or is not a RC tag" + ANSI_NORMAL)
-                  error "Oh ho! Tag is not a release candidate.. Skipping build"  
-                }    
+                    println(ANSI_BOLD + ANSI_RED + "Error.. Tag does not contain " + env.automated_public_repo_branch + " or is not a RC tag" + ANSI_NORMAL)
+                    error "Oh ho! Tag is not a release candidate.. Skipping build"
+                }
             }
             else {
                 println ANSI_BOLD + ANSI_RED + "Tigger is NOT in the deployment window. Skipping build" + ANSI_NORMAL
