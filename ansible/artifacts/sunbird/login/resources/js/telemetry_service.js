1b8cf22b6a9da42443e7caeebf24ee41e6f675c1
Ravinder Kumar <yravinderkumar33@gmail.com>
2021-01-15 13:31:26 +0530

    Issue #SB-22405 feat: updating the pdata version. (#2160)

--- ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
+++ ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
@@ -2506,7 +2506,7 @@ if(client_id.toLowerCase() === 'android'){
     "telemetry": {
       "pdata": {
         "id": pdataId,
-        "ver": "3.5.0",
+        "ver": "3.6.0",
         "pid": "sunbird-portal"
       }
     }
