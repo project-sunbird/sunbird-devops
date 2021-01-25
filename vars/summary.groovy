a6d7b7a0238f360f497130329a28aad7ab8a35b0
Keshav Prasad <msknext@gmail.com>
2021-01-25 14:01:16 +0530

    fix: write data in same format for every job
    Easier to sort and promote jobs to next env

--- vars/summary.groovy
+++ vars/summary.groovy
@@ -14,39 +14,13 @@ def call() {
             publicBranch = params.branch_or_tag
 
             stage('Write data') {
-                sh """
-                    mkdir -p ${JENKINS_HOME}/summary/${envDir}
-                    touch -a ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                """
-                if (jobType == "Deploy") {
-                    if (module == "Core" || module == "Kubernetes") {
-                        image_tag = sh(returnStdout: true, script: 'if [ -f metadata.json ]; then jq -r .image_tag metadata.json; else echo "null"; fi').trim()
-                        sh """
-                            #sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                            #sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                            echo "${jobType}/${envDir}/${module}/${jobName},  imagetag: ${image_tag}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}">> $JENKINS_HOME/summary/${envDir}/summary.txt
-                        """
-                    } else {
-                        artifact_version = sh(returnStdout: true, script: 'if [ -f metadata.json ]; then jq -r .artifact_version metadata.json; else echo "null"; fi').trim()
-                        sh """
-                            #sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                            #sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                            echo "${jobType}/${envDir}/${module}/${jobName},  artifact_version: ${artifact_version}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}" >> $JENKINS_HOME/summary/${envDir}/summary.txt
-                        """
-                    }
-                }
-
-                if (jobType == "Provision"  || jobType == "OpsAdministration") {
-                    sh """
-                        #sed -i "s/${jobType}\\/${module}\\/${jobName}.*//g" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                        #sed -i "/^\\\$/d" ${JENKINS_HOME}/summary/${envDir}/summary.txt
-                        echo "${jobType}/${envDir}/${module}/${jobName}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch}" >> $JENKINS_HOME/summary/${envDir}/summary.txt
-                    """
-                }
+                sh "mkdir -p ${JENKINS_HOME}/summary/${envDir} && touch -a ${JENKINS_HOME}/summary/${envDir}/summary.txt"
+                image_tag = sh(returnStdout: true, script: 'if [ -f metadata.json ]; then jq -r .image_tag metadata.json; else echo "null"; fi').trim()
+                artifact_version = sh(returnStdout: true, script: 'if [ -f metadata.json ]; then jq -r .artifact_version metadata.json; else echo "null"; fi').trim()
+                sh "echo ${jobType}/${envDir}/${module}/${jobName},  imagetag: ${image_tag}, artifact_version: ${artifact_version}, privatebranch: ${privateBranch}, publicbranch: ${publicBranch} >> $JENKINS_HOME/summary/${envDir}/summary.txt"
             }
         }
     }
-
     catch (err){
         throw err
     }
