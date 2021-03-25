cccbbbacb902cb885a8c7564fed110a11cab0b19
Keshav Prasad <keshavprasadms@gmail.com>
2021-03-25 16:28:00 +0530

    Revert "Issue #SB-23074 fix: Added logoff method. (#2355)"
    This reverts commit c59f8c34e4a99361fd2f9408824a2695a2f4f381.

--- ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
+++ ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
@@ -2618,22 +2618,13 @@ if(client_id.toLowerCase() === 'android'){
   }
 
   function doLogin(e) {
-    clearSession().done(function () {
-      e.preventDefault();
-      logInteractEvent("login");
-      logLoginImpressionEvent("pageexit");
-      setTimeout(function () {
-        $("#kc-form-login").submit();
-      }, 500);
-      return false;
-    });
-  }
-
-  function clearSession () {
-   return $.ajax({
-      method: "GET",
-      url: hostURL + "/logoff"
-    }).done(function (response) {});
+    e.preventDefault();
+    logInteractEvent("login");
+    logLoginImpressionEvent("pageexit");
+    setTimeout(function () {
+      $("#kc-form-login").submit();
+    }, 500);
+    return false;
   }
 
   $("body").ready(function ($) {
