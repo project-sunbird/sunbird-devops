10800755bf35ae8550d95eb59f7df33ec2a09a78
Ravinder Kumar <yravinderkumar33@gmail.com>
2021-06-08 14:05:04 +0530

    Issue #SB-25022 fix: pdata.ver updated to 4.0.0 (#2599)

--- ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
+++ ansible/artifacts/sunbird/login/resources/js/telemetry_service.js
@@ -2506,7 +2506,7 @@ if(client_id.toLowerCase() === 'android'){
     "telemetry": {
       "pdata": {
         "id": pdataId,
-        "ver": "3.9.0",
+        "ver": "4.0.0",
         "pid": "sunbird-portal"
       }
     }
