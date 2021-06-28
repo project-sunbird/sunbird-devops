af28387d36187aec805d4ce4e4f09c174d1406f2
keshavprasadms <keshavprasadms@gmail.com>
2021-06-28 16:36:28 +0530

    fix: color coding

--- deploy/post-install-script.sh
+++ deploy/post-install-script.sh
@@ -16,13 +16,16 @@ cassandra_forms(){
   echo "OK"
 }
 
-echo "User provided inputs"
-echo "proto: $proto"
-echo "domain_name: $domain_name"
-echo "core_vault_sunbird_api_auth_token: $core_vault_sunbird_api_auth_token"
-echo "private_ingressgateway_ip: $private_ingressgateway_ip"
-echo "learningservice_ip: $learningservice_ip"
-echo "core_vault_sunbird_sso_client_secret: $core_vault_sunbird_sso_client_secret"
-echo "core_vault_sunbird_google_captcha_site_key_portal: $core_vault_sunbird_google_captcha_site_key_portal"
-echo "sunbird_azure_public_storage_account_name: $sunbird_azure_public_storage_account_name"
-echo "cassandra-1: $cassandra"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo -e "\e[0;32m${bold}User provided inputs ${normal}"
+echo -e "\e[0;90m${bold}proto: $proto ${normal}"
+echo -e "\e[0;90m${bold}domain_name: $domain_name ${normal}"
+echo -e "\e[0;90m${bold}core_vault_sunbird_api_auth_token: $core_vault_sunbird_api_auth_token ${normal}"
+echo -e "\e[0;90m${bold}private_ingressgateway_ip: $private_ingressgateway_ip ${normal}"
+echo -e "\e[0;90m${bold}learningservice_ip: $learningservice_ip ${normal}"
+echo -e "\e[0;90m${bold}core_vault_sunbird_sso_client_secret: $core_vault_sunbird_sso_client_secret ${normal}"
+echo -e "\e[0;90m${bold}core_vault_sunbird_google_captcha_site_key_portal: $core_vault_sunbird_google_captcha_site_key_portal ${normal}"
+echo -e "\e[0;90m${bold}sunbird_azure_public_storage_account_name: $sunbird_azure_public_storage_account_name ${normal}"
+echo -e "\e[0;90m${bold}cassandra-1: $cassandra"
