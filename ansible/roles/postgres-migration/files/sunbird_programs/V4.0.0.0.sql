7d6aba488cfc3f8267557142d7531858dd7f532d
Rajesh Rajendran <rjshrjndrn@users.noreply.github.com>
2021-06-29 12:38:00 +0000

    Revert "Issue #SB-25360 fix: sms templates update (#2652)"
    This reverts commit 486960758353b61e3d6078f757d8969868e64176.

--- ansible/roles/postgres-migration/files/sunbird_programs/V4.0.0.0.sql
+++
@@ -1,2 +0,0 @@
-UPDATE "public"."configuration" SET value="VidyaDaan: Your Content $contentName for the project $projectName has been approved by the project owner. Please login to https://vdn.diksha.gov.in for details." WHERE key="smsContentAccept";
-UPDATE "public"."configuration" SET value="VidyaDaan: Your Content $contentName for the project $projectName has been approved by the project owner with few changes. Please login to https://vdn.diksha.gov.in for details." WHERE key="smsContentRequestedChanges";
