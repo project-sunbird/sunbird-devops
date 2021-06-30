a2cabd1fbf991474a15ffe357e9a0bcb1a045d9d
Kartheek Palla <palla.kartheekreddy@gmail.com>
2021-06-30 17:50:55 +0530

    Issue #SB-25360 fix: VDN SMS templates (#2657) (#2661)

--- ansible/roles/postgres-migration/files/sunbird_programs/V4.0.0.0.sql
+++
@@ -1,2 +0,0 @@
-UPDATE "public"."configuration" SET value="VidyaDaan: Your Content $contentName for the project $projectName has been approved by the project owner. Please login to $url for details." WHERE key="smsContentAccept";
-UPDATE "public"."configuration" SET value="VidyaDaan: Your Content $contentName for the project $projectName has been approved by the project owner with few changes. Please login to $url for details." WHERE key="smsContentRequestedChanges";
