a7c78321233b7a852d44dd72bea5e89bd1f0ba34
Amol Ghatol <amol_g@techjoomla.com>
2021-08-09 16:28:37 +0530

    Update v4.2.0.sql

--- ansible/roles/postgres-migration/files/sunbird_programs/v4.2.0.sql
+++ ansible/roles/postgres-migration/files/sunbird_programs/v4.2.0.sql
@@ -1,7 +1,1 @@
-alter type programtype rename to _programtype;
-CREATE TYPE programtype AS ENUM ('public', 'private', 'restricted');
-alter table program rename column type to _type;
-alter table program add type programtype;
-update program set type = _type::text::programtype;
-alter table program drop column _type;
-drop type _programtype;
+ALTER TYPE programtype ADD VALUE 'restricted';
