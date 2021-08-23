11b58eb9c547653ec78916c1178cfdc094ec37dc
JAGANNATH-OPTIT <73009211+JAGANNATH-OPTIT@users.noreply.github.com>
2021-08-23 23:50:34 +0530

    Updating the source path

--- ansible/roles/ml-analytics-service/files/faust.sh
+++ ansible/roles/ml-analytics-service/files/faust.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
-/opt/sparkjobs/spark_venv/bin/python /opt/sparkjobs/source/\$1.py --workdir /opt/sparkjobs/source/\$2 worker -l info
+/opt/sparkjobs/spark_venv/bin/python /opt/sparkjobs/ml-analytics-service/\$1.py --workdir /opt/sparkjobs/ml-analytics-service/\$2 worker -l info
