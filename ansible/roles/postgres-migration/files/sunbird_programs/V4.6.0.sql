1c914f9efa67271705ba1c6eb6bc5f359ca54274
vaishali <vaishali_k@techjoomla.com>
2021-12-21 17:03:16 +0530

    Issue #sb-26390 feat: form config table added

---
+++ ansible/roles/postgres-migration/files/sunbird_programs/V4.6.0.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.formconfig (
+    id character varying NOT NULL,
+    channel character varying,
+    objecttype character varying,
+    primarycategory character varying,
+    context character varying,
+    context_type character varying,
+    operation character varying,
+    data jsonb,
+    status public.formstatus,
+    createdon timestamp without time zone,
+    updatedon timestamp without time zone,
+    createdby character varying,
+    updatedby character varying,
+    CONSTRAINT unique_form_config PRIMARY KEY (channel, objecttype, primarycategory, context, context_type, operation)
+);
