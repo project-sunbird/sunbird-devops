dfc3d883877bc8e660b5ddb311d8d4b32a0f4d9d
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2023-04-05 10:27:09 +0530

    Oneclickinstaller final script (#3777)
    * Add one click script installer
    
    * Create common-lern.yaml
    
    * removed sensitive data
    
    * Add learn script
    
    * Script working .tgz file
    
    * Added cassandra and LMS chart path
    
    * Added charts
    
    * charts added
    
    * Added charts
    
    * chart csv file updated
    
    * Added final  values and script

--- oneclickinstaller/install-lern.sh
+++ oneclickinstaller/install-lern.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-set -x
-
+# set -x
 # Set the namespace for the Helm charts
 namespace="dry-run"
 kubeconfig_file=$1
@@ -50,21 +48,31 @@ if ! kubectl get namespace $namespace >/dev/null 2>&1; then
   echo -e "\e[92mCreated namespace $namespace\e[0m"
 fi
 
-while IFS=',' read -r chart_name  chart_repo; do
+# Check if the charts.csv file exists
+if [ ! -f "charts.csv" ]; then
+  echo "Error: charts.csv file not found"
+  exit 1
+fi
+
+
+while IFS=',' read -r chart_name chart_repo; do
+  # Check if the chart repository URL is empty
   if [ -z "$chart_repo" ]; then
     echo "Error: Repository URL not found for $chart_name in charts.csv"
     exit 1
   fi
 
-  # Check if the chart is already installed
-  if helm list -n $namespace | grep -q $chart_name; then
-    echo "$chart_name is already installed."
-  else
+  # # Check if the chart is already installed
+  # if helm list -n "$namespace" | grep -q "$chart_name"; then
+  #   echo -e "\e[92m$chart_name is already installed\e[0m"
+  # else
     # Install the chart with global variables
-    echo "helm upgrade --install $chart_name $chart_repo -n $namespace -f global-values.yaml"
-    helm upgrade --install $chart_name $chart_repo -n $namespace -f global-values.yaml
-    echo "$chart_name is installed successfully."
-    fi
+    if ! helm upgrade --install "$chart_name" "$chart_repo" -n "$namespace" -f global-values.yaml; then
+      echo -e "\e[91mError installing $chart_name\e[0m"
+   #   exit 1
+   fi
+    echo -e "\e[92m$chart_name is installed successfully\e[0m"
+  # fi
 done < charts.csv
 
 
