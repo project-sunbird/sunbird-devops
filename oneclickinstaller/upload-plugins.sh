d869da86fc888147f06ea3fd83e70ec4e44a984c
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2023-10-13 13:46:29 +0530

    Update upload-plugins.sh (#3910)

--- oneclickinstaller/upload-plugins.sh
+++ oneclickinstaller/upload-plugins.sh
@@ -8,21 +8,21 @@ AccountKey=$(grep "cloud_public_storage_secret" global-values.yaml | awk -F ":" 
 
 # Content-plugins
 rm -rf content-plugins
-wget https://sunbirdartifact.blob.core.windows.net/artifacts/content-plugins-artifacts.zip:release-5.2.0_RC1_c7c385c_6 -O content-plugins.zip 
+wget https://sunbirdartifact.blob.core.windows.net/artifacts/content-plugins.zip -O content-plugins.zip 
 unzip content-plugins.zip -d content-plugins >> /dev/null 
 unzip content-plugins/content-plugins.zip >> /dev/null
 az storage blob upload-batch -d sunbird-contents-dev/content-plugins --account-name $AccountName --account-key $AccountKey -s content-plugins --overwrite true
 
 # Content-editors
 rm -rf content-editor
-wget https://sunbirdartifact.blob.core.windows.net/artifacts/content-editor-artifacts.zip:release-5.2.0_RC1_fbbee60 -O content-editor.zip 
+wget https://sunbirdartifact.blob.core.windows.net/artifacts/content-editor.zip -O content-editor.zip 
 unzip content-editor.zip -d content-editor
 unzip content-editor/content-editor.zip -d content-editor >> /dev/null
 az storage blob upload-batch -d sunbird-contents-dev/content-editor --account-name $AccountName --account-key $AccountKey -s content-editor --overwrite true
 
 # Collection editor
 rm -rf collection-editor
-wget https://sunbirdartifact.blob.core.windows.net/artifacts/collection-editor-artifacts.zip:release-3.6.0_RC1 -O collection-editor.zip 
+wget https://sunbirdartifact.blob.core.windows.net/artifacts/collection-editor.zip -O collection-editor.zip 
 unzip collection-editor.zip -d collection-editor
 unzip collection-editor/collection-editor.zip -d collection-editor >> /dev/null
 az storage blob upload-batch -d sunbird-contents-dev/collection-editor --account-name $AccountName --account-key $AccountKey -s collection-editor --overwrite true
@@ -35,7 +35,7 @@ az storage blob upload-batch -d sunbird-contents-dev/v3/preview --account-name $
 
 #Generic Editor
 rm -rf generic-editor
-wget https://sunbirdartifact.blob.core.windows.net/artifacts/generic-editor-artifacts.zip:release-5.2.0_RC1_d77ea68 -O generic-editor.zip 
+wget https://sunbirdartifact.blob.core.windows.net/artifacts/generic-editor.zip -O generic-editor.zip 
 unzip generic-editor.zip -d generic-editor
 unzip generic-editor/generic-editor.zip -d generic-editor >> /dev/null
 az storage blob upload-batch -d sunbird-contents-dev/generic-editor --account-name $AccountName --account-key $AccountKey -s generic-editor --overwrite true
